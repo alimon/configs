b1d86571521faeaf837660f524125866e0e3130a
Paul Sokolovsky <paul.sokolovsky@linaro.org>
2015-03-05 22:48:18 +0200

    README: add information about jenkins-job-builder usage
    Change-Id: If070edc8cb3de28a3ceac1209bba94f30208d707
    Signed-off-by: Paul Sokolovsky <paul.sokolovsky@linaro.org>
    Signed-off-by: Fathi Boudra <fathi.boudra@linaro.org>

---
+++ run-jjb.py
@@ -0,0 +1,59 @@
+#!/usr/bin/python
+
+import os
+import shutil
+import signal
+import string
+import subprocess
+import sys
+from distutils.spawn import find_executable
+
+jjb_cmd = find_executable('jenkins-jobs') or sys.exit('jenkins-jobs is not found.')
+
+try:
+    arguments = ['git', 'diff-tree', '--no-commit-id', '--name-only',
+                 '--diff-filter=AM', '-r', os.environ.get('GIT_COMMIT')]
+    proc = subprocess.Popen(arguments,
+                            stdin=subprocess.PIPE,
+                            stdout=subprocess.PIPE,
+                            universal_newlines=False,
+                            preexec_fn=lambda:
+                            signal.signal(signal.SIGPIPE, signal.SIG_DFL))
+except (OSError, ValueError) as e:
+    raise ValueError("%s" % e)
+
+data = proc.communicate()[0]
+if proc.returncode != 0:
+    raise ValueError("command has failed with code '%s'" % proc.returncode)
+
+for conf_filename in data.splitlines():
+    if conf_filename.endswith('.yaml'):
+        with open(conf_filename) as f:
+            buffer = f.read()
+            template = string.Template(buffer)
+        buffer = template.safe_substitute(
+            PUBLISH_KEY=os.environ.get('PUBLISH_KEY') or
+                        sys.exit('Key is not defined.'),
+            LT_QCOM_KEY=os.environ.get('LT_QCOM_KEY'),
+            LAVA_USER=os.environ.get('LAVA_USER'),
+            LAVA_TOKEN=os.environ.get('LAVA_TOKEN'))
+        with open('template.yaml', 'w') as f:
+            f.write(buffer)
+        try:
+            arguments = [jjb_cmd, 'update', conf_filename]
+            # arguments = [jjb_cmd, 'test', conf_filename, '-o', 'out']
+            proc = subprocess.Popen(arguments,
+                                    stdin=subprocess.PIPE,
+                                    stdout=subprocess.PIPE,
+                                    universal_newlines=False,
+                                    preexec_fn=lambda:
+                                    signal.signal(signal.SIGPIPE, signal.SIG_DFL))
+        except (OSError, ValueError) as e:
+            raise ValueError("%s" % e)
+
+        data = proc.communicate()[0]
+        if proc.returncode != 0:
+            raise ValueError("command has failed with code '%s'" % proc.returncode)
+
+        os.remove('template.yaml')
+        #shutil.rmtree('out')
