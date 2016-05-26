7df7dbd7ae6ad7a8110ab69fe8ff56ef7b6b0c9d
Fathi Boudra <fathi.boudra@linaro.org>
2016-05-26 07:15:14 +0300

    run-jjb.py: add support ART_TOKEN and DB_TOKEN
    Change-Id: Iedc5d6181f9f88fe9b6bb7b1e2c929b6e3942c6b
    Signed-off-by: Fathi Boudra <fathi.boudra@linaro.org>

--- run-jjb.py
+++ run-jjb.py
@@ -35,7 +35,9 @@ for conf_filename in data.splitlines():
         buffer = template.safe_substitute(
             PUBLISH_KEY=os.environ.get('PUBLISH_KEY') or
                         sys.exit('Key is not defined.'),
+            ART_TOKEN=os.environ.get('ART_TOKEN'),
             AUTH_TOKEN=os.environ.get('AUTH_TOKEN'),
+            DB_TOKEN=os.environ.get('DB_TOKEN'),
             PRIVATE_KEY=os.environ.get('PRIVATE_KEY'),
             COVERITY_TOKEN_ODP=os.environ.get('COVERITY_TOKEN_ODP'),
             COVERITY_TOKEN_ODP_DPDK=os.environ.get('COVERITY_TOKEN_ODP_DPDK'),
