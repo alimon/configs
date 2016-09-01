7dceb004c99ecfb94e77b139ddba6b2c65fbbd4f
Fathi Boudra <fathi.boudra@linaro.org>
2016-09-01 17:05:57 +0300

    openembedded-armv8-luvos: build next branch
    Change-Id: I9b14cd675f7a285d2dfe06fe0031fce82d0c1c52
    Signed-off-by: Fathi Boudra <fathi.boudra@linaro.org>

---
+++ openembedded-armv8-luvos/builders.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+set -ex
+
+trap cleanup_exit INT TERM EXIT
+
+cleanup_exit()
+{
+  cd ${WORKSPACE}
+  rm -rf build/tmp/
+}
+
+# Build
+source oe-init-build-env
+echo "BBLAYERS += \" ${WORKSPACE}/meta-luv\"" >> conf/bblayers.conf
+bitbake luv-live-image
+
+rm -f ${WORKSPACE}/build/tmp/deploy/images/qemuarm64/*.txt
+find ${WORKSPACE}/build/tmp/deploy/images/qemuarm64 -type l -delete
+# Publish
+test -d ${HOME}/bin || mkdir ${HOME}/bin
+wget -q https://git.linaro.org/ci/publishing-api.git/blob_plain/HEAD:/linaro-cp.py -O ${HOME}/bin/linaro-cp.py
+time python ${HOME}/bin/linaro-cp.py \
+  --api_version 3 \
+  --link-latest \
+  ${WORKSPACE}/build/tmp/deploy/images/qemuarm64 openembedded/pre-built/luvos/${BRANCH}/${BUILD_NUMBER}
