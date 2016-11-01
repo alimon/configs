267d7284270b2a27a2e65d2a5ef67c8e970f397e
Vishal Bhoj <vishal.bhoj@linaro.org>
2016-11-01 14:39:02 +0530

    android-lcr: generic: support to include vendor tarballs
    Change-Id: I8a60577ee37416fd75de888ce6509972e30d10c5
    Signed-off-by: Vishal Bhoj <vishal.bhoj@linaro.org>

--- android-lcr/generic/builders.sh
+++ android-lcr/generic/builders.sh
@@ -1,11 +1,26 @@
 # Build Android
 repo init -u ${ANDROID_MANIFEST_URL} -b ${MANIFEST_BRANCH}
 repo sync -j"$(nproc)" -c
+
+if [ -n "${BLOBS_URL}" ]; then
+IFS='#'; for url in ${BLOBS_URL}; do
+  wget -q ${url}
+  tar -xvf $(basename ${url})
+  yes "I ACCEPT"|sh *.sh
+  rm *.sh
+done
+fi
+unset IFS
+
 source build/envsetup.sh
 lunch ${LUNCH_TARGET}
 make -j"$(nproc)"
 
-wget https://git.linaro.org/ci/job/configs.git/blob_plain/HEAD:/android-lcr/generic/build-info/template.txt -O ${ANDROID_PRODUCT_OUT}/BUILD-INFO.txt
+template="private-template.txt"
+if [ "${BUILD_TYPE}" = "public" ]; then
+  template="public-template.txt"
+fi
+wget https://git.linaro.org/ci/job/configs.git/blob_plain/HEAD:/android-lcr/generic/build-info/${template} -O ${ANDROID_PRODUCT_OUT}/BUILD-INFO.txt
 
 # Publish binaries
 PUB_DEST=/android/${JOB_NAME}/${BUILD_NUMBER}/${LUNCH_TARGET}
