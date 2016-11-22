d8baff222668c68313f5af508ad489c6b5693859
Lee Jones <lee.jones@linaro.org>
2016-11-22 09:59:53 +0000

    96boards-b2260: copy uImage file to <PREBUILT_IMAGE>/boot
    uImage is required by some of the configuations
    
    Change-Id: I833acb40549d69078d67a1300b7c1c98bf63f0f1
    Signed-off-by: Lee Jones <lee.jones@linaro.org>

--- 96boards-b2260/builders.sh
+++ 96boards-b2260/builders.sh
@@ -89,6 +89,7 @@ EOF
 
   sudo cp -a configs/96boards-b2260/boot/b2260 boot/ || true
   sudo cp -a configs/96boards-b2260/boot/update_default_boot.sh boot/ || true
+  sudo cp -a out/uImage boot/ || true
 
   sudo rm -rf rootfs/dev rootfs/boot rootfs/var/lib/apt/lists
   sudo mkdir rootfs/dev rootfs/boot rootfs/var/lib/apt/lists
