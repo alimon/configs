a0c3b8a4af99cb56fa9e0e5576df4d35a520686f
Vishal Bhoj <vishal.bhoj@linaro.org>
2016-11-22 11:14:00 +0530

    android-r-lcr-manifest: add a job to create r-lcr manifest
    Change-Id: I1b295af73ab5bf73ce35604012d3cba438bd0e74
    Signed-off-by: Vishal Bhoj <vishal.bhoj@linaro.org>

---
+++ android-r-lcr-manifest/tag-manifest.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+commit=$(git log -1 --format="%H")
+tags=$(git tag --points-at HEAD)
+tag=$(echo ${tags} | rev | cut -d' ' -f1 | rev)
+if [ ! ${tag} ]; then
+  git tag -a -m "${RELEASE_TAG} Release" ${RELEASE_TAG}
+  remote=$(git remote -v | grep -m1 fetch | cut -d$'\t' -f2 | cut -d' ' -f1 | sed -e "s/http:\/\/android-git.linaro.org/ssh:\/\/${GERRIT_USER}@android-review.linaro.org:29418/g" -e "s/\/git\//\//g")
+  git remote add upstream ${remote}
+  git push upstream ${RELEASE_TAG} -f
+  sed -i "s/${commit}/refs\/tags\/${RELEASE_TAG}/g" ${WORKSPACE}/R-LCR.xml
+else
+  sed -i "s/${commit}/refs\/tags\/${tag}/g" ${WORKSPACE}/R-LCR.xml
+fi
