39c902ceebaad01c121b9ca9eee4c847308e30b2
Vishal Bhoj <vishal.bhoj@linaro.org>
2016-11-22 13:14:01 +0530

    android-r-lcr-manifest: fix r-lcr.xml location
    Change-Id: I59f19719f2d1b3eec3bba337ffe3fd135925aa7f
    Signed-off-by: Vishal Bhoj <vishal.bhoj@linaro.org>

--- android-r-lcr-manifest/tag-manifest.sh
+++ android-r-lcr-manifest/tag-manifest.sh
@@ -8,7 +8,7 @@ if [ ! ${tag} ]; then
   remote=$(git remote -v | grep -m1 fetch | cut -d$'\t' -f2 | cut -d' ' -f1 | sed -e "s/http:\/\/android-git.linaro.org/ssh:\/\/${GERRIT_USER}@android-review.linaro.org:29418/g" -e "s/\/git\//\//g")
   git remote add upstream ${remote}
   git push upstream ${RELEASE_TAG} -f
-  sed -i "s/${commit}/refs\/tags\/${RELEASE_TAG}/g" ${WORKSPACE}/R-LCR.xml
+  sed -i "s/${commit}/refs\/tags\/${RELEASE_TAG}/g" ${WORKSPACE}/out/R-LCR.xml
 else
-  sed -i "s/${commit}/refs\/tags\/${tag}/g" ${WORKSPACE}/R-LCR.xml
+  sed -i "s/${commit}/refs\/tags\/${tag}/g" ${WORKSPACE}/out/R-LCR.xml
 fi
