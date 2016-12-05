635cd9c593f21012d436ce8bc9728b8e40c0392d
Andy Doan <andy.doan@linaro.org>
2016-12-05 10:56:33 -0600

    leg-devcloud-openstack: add a new CentOS build
    virtualenvs for Jessie won't run on CentOS. This allows us to build
    both.
    
    Change-Id: If13fdf1fb89aebc52df32566da182387f2072887

--- leg-devcloud-openstack-virtualenv/builders.sh
+++ leg-devcloud-openstack-virtualenv/builders.sh
@@ -21,9 +21,15 @@ sudo chown -R buildslave:buildslave out
 (cd out && sha256sum * > SHA256SUMS)
 
 # Publish
+DEST=snapshots/developer-cloud/openstack/centos-virtualenv/${BUILD_NUMBER}
+if grep Debian /etc/issue 2>&1 >/dev/null ; then
+  DEST=snapshots/developer-cloud/openstack/debian-virtualenv/${BUILD_NUMBER}
+fi
+
+
 test -d ${HOME}/bin || mkdir ${HOME}/bin
 wget -q https://git.linaro.org/ci/publishing-api.git/blob_plain/HEAD:/linaro-cp.py -O ${HOME}/bin/linaro-cp.py
 time python ${HOME}/bin/linaro-cp.py \
   --server ${PUBLISH_SERVER} \
   --link-latest \
-  out snapshots/developer-cloud/openstack/virtualenv/${BUILD_NUMBER}
+  out ${DEST}
