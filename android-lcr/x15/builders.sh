14e3112b5397ac8bc57540f182672ccbd7cc9b04
Yongqin Liu <yongqin.liu@linaro.org>
2016-12-07 01:32:41 +0800

    android-lcr/x15/builders.sh: remove kernel and uboot compiling part
    since kernel compilation is done via android build,
    and uboot is not building here.
    
    If necessary, we could add the uboot compiling again in the future,
    or add it as part of the android building
    
    Change-Id: Ibc2077a5771cf20aebcbed3e94c0423dc9fad5dc
    Signed-off-by: Yongqin Liu <yongqin.liu@linaro.org>

--- android-lcr/x15/builders.sh
+++ android-lcr/x15/builders.sh
@@ -1,28 +1,3 @@
-# Build u-boot, MLO and dtb
-if [ ! -e gcc-linaro-arm-none-eabi-4.8-2014.04_linux ]; then
-  wget -q https://releases.linaro.org/archive/14.04/components/toolchain/binaries/gcc-linaro-arm-none-eabi-4.8-2014.04_linux.tar.bz2
-  tar -xvf gcc-linaro-arm-none-eabi-4.8-2014.04_linux.tar.bz2
-fi
-export PATH=${PWD}/gcc-linaro-arm-none-eabi-4.8-2014.04_linux/bin/:${PATH}
-export CROSS_COMPILE="arm-none-eabi-"
-export ARCH=arm
-
-rm -rf u-boot linux
-git clone --depth=1 git://git.ti.com/android-sdk/u-boot.git u-boot -b p-ti-u-boot-2016.05
-cd u-boot
-make am57xx_evm_nodt_defconfig
-make -j"$(nproc)"
-cd -
-
-# Build Kernel
-git clone --depth=1 git://git.ti.com/android-sdk/kernel-omap.git linux -b p-ti-lsk-android-linux-4.4.y
-cd linux
-ti_config_fragments/defconfig_builder.sh -t ti_sdk_am57x_android_release
-make ti_sdk_am57x_android_release_defconfig
-make -j"$(nproc)" zImage
-make am57xx-evm-reva3.dtb
-cd -
-
 # Build Android
 build-tools/node/build us-east-1.ec2-git-mirror.linaro.org "${CONFIG}"
 cp -a /home/buildslave/srv/${BUILD_DIR}/build/out/*.json /home/buildslave/srv/${BUILD_DIR}/build/out/*.xml ${WORKSPACE}/
