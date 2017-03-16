b0ed07c049318a35a7c529a2be5eaa2c4e63804d
Fathi Boudra <fathi.boudra@linaro.org>
2017-03-16 11:00:05 +0200

    rpb-openembedded: Juno doesn't use fastboot to deploy images
    We can skip sparse image generation.
    
    Change-Id: I84c2a64400f20eb54db476dfc12b5cd8c2f36b33
    Signed-off-by: Fathi Boudra <fathi.boudra@linaro.org>

--- rpb-openembedded/builders.sh
+++ rpb-openembedded/builders.sh
@@ -102,7 +102,7 @@ cat ${DEPLOY_DIR_IMAGE}/pinned-manifest.xml
 # FIXME: am57xx-evm creates tar.xz rootfs image and
 #        meta-ti u-boot doesn't enable fastboot support
 case "${MACHINE}" in
-  am57xx-evm|stih410-b2260)
+  am57xx-evm|juno|stih410-b2260)
     ;;
   *)
     for rootfs in ${DEPLOY_DIR_IMAGE}/*.rootfs.ext4.gz; do
