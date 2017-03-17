63cc9734cbacdcfa541bfaf56fbab99c22233578
Fathi Boudra <fathi.boudra@linaro.org>
2017-03-17 14:18:31 +0200

    openembedded-armv8-luvos: add meta-oe layer
    meta-oe layer is now needed since the switch to plymouth.
    
    Change-Id: Iec1f02c0d0b59a724e10b7ddc55870bd505481fc
    Signed-off-by: Fathi Boudra <fathi.boudra@linaro.org>
    Signed-off-by: Naresh Bhat <naresh.bhat@linaro.org>

--- openembedded-armv8-luvos/builders.sh
+++ openembedded-armv8-luvos/builders.sh
@@ -13,6 +13,7 @@ cleanup_exit()
 # Build
 source oe-init-build-env
 echo "BBLAYERS += \" ${WORKSPACE}/meta-luv\"" >> conf/bblayers.conf
+echo "BBLAYERS += \" ${WORKSPACE}/meta-oe\"" >> conf/bblayers.conf
 bitbake luv-live-image
 
 rm -f ${WORKSPACE}/build/tmp/deploy/images/qemuarm64/*.txt
