6b50879174b27661c4514886506ea2914c753724
Fathi Boudra <fathi.boudra@linaro.org>
2017-03-21 11:39:23 +0200

    openembedded-lkft: add python depdencies for LAVA and test-runner
    Needed to run in LAVA but also to use test-runner tool.
    
    Change-Id: Ia501b39aaa0202db99078a35ed5955e49aa9c34e
    Signed-off-by: Fathi Boudra <fathi.boudra@linaro.org>

--- openembedded-lkft/builders.sh
+++ openembedded-lkft/builders.sh
@@ -83,6 +83,7 @@ EOF
 # Include additional recipes in the image
 cat << EOF >> conf/local.conf
 CORE_IMAGE_BASE_INSTALL_append = " kselftests libhugetlbfs-tests ltp"
+CORE_IMAGE_BASE_INSTALL_append = " python python-misc python-modules python-numpy python-pexpect python-pyyaml"
 EOF
 
 # Override cmdline
