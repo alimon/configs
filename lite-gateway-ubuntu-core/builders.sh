3430904f1dd8acffe72cbb66d2aac399639a2f14
Madper Xie <madper.xie@linaro.org>
2017-03-24 19:25:37 +0800

    lite-gateway-ubuntu-core: fix kernel snap output path.
    Change-Id: I5a613993d7759bb3a37510de17fa99bf6c5034a1

--- lite-gateway-ubuntu-core/builders.sh
+++ lite-gateway-ubuntu-core/builders.sh
@@ -17,7 +17,7 @@ cleanup_exit()
   cd ${WORKSPACE}
   sudo umount ${SNAP} || true
   sudo rm -rf ${SNAP} || true
-  rm -f ubuntu-image_*
+  rm -f ubuntu-image_* *.snap
 }
 
 # sbin isn't in the PATH by default and prevent to find mkfs.vfat
@@ -35,14 +35,14 @@ sudo mount -o loop -t squashfs ubuntu-image_*.snap ${SNAP}
 for machine in ${MACHINES}; do
   if [ "${machine}" == "hummingboard" ]; then
     git clone --depth 1 https://github.com/madper/hummingboard-kernel.git
-    (cd hummingboard-kernel && snapcraft --target-arch armhf snap --output hummingboard-kernel.snap)
+    (cd hummingboard-kernel && snapcraft --target-arch armhf snap --output ${WORKSPACE}/hummingboard-kernel.snap)
 
     git clone --depth 1 https://github.com/madper/hummingboard-gadget.git
-    snapcraft --target-arch armhf snap hummingboard-gadget --output hummingboard-gadget.snap
+    snapcraft --target-arch armhf snap hummingboard-gadget --output ${WORKSPACE}/hummingboard-gadget.snap
 
     ${SNAP}/command-ubuntu-image.wrapper -c beta \
-      --extra-snaps hummingboard-kernel.snap \
-      --extra-snaps hummingboard-gadget.snap \
+      --extra-snaps ${WORKSPACE}/hummingboard-kernel.snap \
+      --extra-snaps ${WORKSPACE}/hummingboard-gadget.snap \
       -o ubuntu-core-16-${machine}-lite.img ${machine}.model
   else
     ${SNAP}/command-ubuntu-image.wrapper -c beta \
