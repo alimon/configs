2ad079667e5bc7e64ce7460c36293e83512a2193
Fathi Boudra <fathi.boudra@linaro.org>
2017-03-30 12:49:08 +0300

    ci-dockerfiles-deployment: new job
    Job to build and publish our images to docker hub.
    It's triggered on gerrit merge event.
    
    Change-Id: I710d46c68082d5695538f40950360d09d6ee9ce9
    Signed-off-by: Fathi Boudra <fathi.boudra@linaro.org>
    Signed-off-by: Riku Voipio <riku.voipio@linaro.org>

---
+++ ci-dockerfiles-deployment/builders.sh
@@ -0,0 +1,73 @@
+#!/bin/bash
+
+set -ex
+
+trap cleanup_exit INT TERM EXIT
+
+cleanup_exit()
+{
+    rm -rf ${HOME}/.docker
+}
+
+mkdir -p ${HOME}/.docker
+sed -e "s|\${DOCKER_AUTH}|${DOCKER_AUTH}|" < ${WORKSPACE}/config.json > ${HOME}/.docker/config.json
+chmod 0600 ${HOME}/.docker/config.json
+
+echo ""
+echo "########################################################################"
+echo "    Gerrit Environment"
+env |grep '^GERRIT'
+echo "########################################################################"
+
+rm -rf ${WORKSPACE}/*
+
+git clone -b ${GERRIT_BRANCH} --depth 2 https://review.linaro.org/${GERRIT_PROJECT}
+cd *
+git fetch https://review.linaro.org/${GERRIT_PROJECT} ${GERRIT_REFSPEC}
+git checkout -q FETCH_HEAD
+
+git_previous_commit=$(git rev-parse HEAD~1)
+files=$(git diff --name-only ${git_previous_commit} ${GERRIT_PATCHSET_REVISION})
+echo Changes in: ${files}
+changed_dirs=$(dirname ${files})
+
+update_images=""
+for dir in ${changed_dirs}; do
+  # Find the closest directory with build.sh.  This is, primarily,
+  # to handle changes to tcwg-base/tcwg-build/tcwg-builslave/* directories.
+  while [ ! -e ${dir}/build.sh -a ! -e ${dir}/.git ]; do
+    dir=$(dirname ${dir})
+  done
+  # Add this and all dependant images in the update.
+  update_images="${update_images} $(dirname $(find ${dir} -name build.sh))"
+done
+
+host_arch=$(dpkg-architecture -qDEB_HOST_ARCH)
+
+for image in ${update_images}; do
+  (
+  set -e
+  cd ${image}
+  image_arch=$(basename ${PWD} | cut -f2 -d '-')
+  case "${image_arch}" in
+    amd64|i386)
+      if [ "${host_arch}" = "amd64" ]; then
+        echo "=== Start build: ${image} ==="
+        ./build.sh || echo "=== FAIL: ${image} ==="
+      fi
+      ;;
+    arm64|armhf)
+      if [ "${host_arch}" = "arm64" ]; then
+        echo "=== Start build: ${image} ==="
+        ./build.sh || echo "=== FAIL: ${image} ==="
+      fi
+      ;;
+    *)
+      echo "unknown arch: ${image_arch}"
+      ;;
+  esac
+  if [ -r .docker-tag ]; then
+    echo docker push $(cat .docker-tag)
+  fi
+  )
+done
