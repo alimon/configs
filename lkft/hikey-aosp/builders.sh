01867c5d68d09c5b54e5e80eb695bb7ccab6aa33
Vishal Bhoj <vishal.bhoj@linaro.org>
2017-04-14 10:21:41 +0530

    lkft-hikey-aosp: fix kernel_branch retrieval
    git branch doesn't reflect branch in jenkins
    
    Change-Id: Ia98306eeb506ac5a576f62bd8dbfbf2dda7e4759
    Signed-off-by: Vishal Bhoj <vishal.bhoj@linaro.org>

--- lkft/hikey-aosp/builders.sh
+++ lkft/hikey-aosp/builders.sh
@@ -23,7 +23,6 @@ python mkbootimg \
 wget -q ${REFERENCE_AOSP_BUILD}/build_config.json -O out/build_config.json
 remote=$(git remote -v | grep push | cut -d" " -f1 | cut -f2)
 commit=$(git log | grep commit | cut -d" " -f2)
-branch=$(git branch | cut -d" " -f2)
 sed -i "s|\"kernel_repo\" : \"|\"kernel_repo\" : \"$remote|g" out/build_config.json
 sed -i "s|\"kernel_commit_id\" : \"|\"kernel_commit_id\" : \"$commit|g" out/build_config.json
-sed -i "s|\"kernel_branch\" : \"|\"kernel_branch\" : \"$branch|g" out/build_config.json
+sed -i "s|\"kernel_branch\" : \"|\"kernel_branch\" : \"$KERNEL_BRANCH|g" out/build_config.json
