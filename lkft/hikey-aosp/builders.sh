23488312a7dc7486217a3d4a437470242218386d
Vishal Bhoj <vishal.bhoj@linaro.org>
2017-04-14 10:11:51 +0530

    lkft-hikey-aosp: publish build_config with kernel details
    Change-Id: I6560e4ddb9b58f350b079dde544845bf179e2928
    Signed-off-by: Vishal Bhoj <vishal.bhoj@linaro.org>

--- lkft/hikey-aosp/builders.sh
+++ lkft/hikey-aosp/builders.sh
@@ -10,6 +10,7 @@ make ARCH=arm64 CROSS_COMPILE=aarch64-linux-android- -j$(nproc) Image-dtb
 
 wget -q https://android-git.linaro.org/platform/system/core.git/plain/mkbootimg/mkbootimg -O mkbootimg
 wget -q ${REFERENCE_AOSP_BUILD}/ramdisk.img -O ramdisk.img
+
 mkdir -p out
 python mkbootimg \
   --kernel ${PWD}/arch/arm64/boot/Image-dtb \
@@ -18,3 +19,11 @@ python mkbootimg \
   --os_patch_level 2016-11-05 \
   --ramdisk ramdisk.img \
   --output out/boot.img
+
+wget -q ${REFERENCE_AOSP_BUILD}/build_config.json -O out/build_config.json
+remote=$(git remote -v | grep push | cut -d" " -f1 | cut -f2)
+commit=$(git log | grep commit | cut -d" " -f2)
+branch=$(git branch | cut -d" " -f2)
+sed -i "s|\"kernel_repo\" : \"|\"kernel_repo\" : \"$remote|g" out/build_config.json
+sed -i "s|\"kernel_commit_id\" : \"|\"kernel_commit_id\" : \"$commit|g" out/build_config.json
+sed -i "s|\"kernel_branch\" : \"|\"kernel_branch\" : \"$branch|g" out/build_config.json
