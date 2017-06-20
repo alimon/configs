30935eceae6969d77fa8eb3faaf7a4340ae7f7a9
Paul Sokolovsky <paul.sokolovsky@linaro.org>
2017-06-21 01:44:59 +0300

    pfalcon-aeolus-zephyr.js: Prototype of new LITE Aeolus job for Zephyr.js.
    New set of jobs for LITE Aeolus project will be per-component (separate
    for Zephyr.js, JerryScript, and MicroPython), will use upstream repositories
    directly instead of proxy repo with submodules requiring manual updates,
    and will submit artifacts to LAVA/QA Reports site for boot testing.
    
    Change-Id: Idf87941d18116084cbdde47056a92b26b9def967
    Signed-off-by: Paul Sokolovsky <paul.sokolovsky@linaro.org>

---
+++ lite-aeolus/builders.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+
+#sudo apt-get -q=2 update
+#sudo apt-get -q=2 -y install git g++ libc6-dev-i386 g++-multilib python3-ply python3-yaml gcc-arm-none-eabi python-requests rsync device-tree-compiler
+
+set -ex
+
+git clone --depth 1 https://chromium.googlesource.com/chromium/tools/depot_tools ${HOME}/depot_tools
+PATH=${HOME}/depot_tools:${PATH}
+git clone --depth 1 https://github.com/01org/zephyr.js ${WORKSPACE}
+git-retry submodule sync --recursive
+git-retry submodule update --init --recursive --checkout
+git clean -fdx
+echo "GIT_COMMIT=$(git rev-parse --short=8 HEAD)" > env_var_parameters
+
+# Toolchains are pre-installed and come from:
+# https://armkeil.blob.core.windows.net/developer/Files/downloads/gnu-rm/6_1-2017q1/gcc-arm-none-eabi-6-2017-q1-update-linux.tar.bz2
+# https://github.com/zephyrproject-rtos/meta-zephyr-sdk/releases/download/0.9.1/zephyr-sdk-0.9.1-setup.run
+# To install Zephyr SDK: ./zephyr-sdk-0.9.1-setup.run --quiet --nox11 -- <<< "${HOME}/srv/toolchain/zephyr-sdk-0.9.1"
+
+case "${ZEPHYR_GCC_VARIANT}" in
+  gccarmemb)
+    export GCCARMEMB_TOOLCHAIN_PATH="${HOME}/srv/toolchain/gcc-arm-none-eabi-6-2017-q1-update"
+  ;;
+  zephyr)
+    export ZEPHYR_SDK_INSTALL_DIR="${HOME}/srv/toolchain/zephyr-sdk-0.9.1"
+  ;;
+esac
+
+# Set build environment variables
+LANG=C
+ZEPHYR_BASE=${WORKSPACE}
+PATH=${ZEPHYR_BASE}/scripts:${PATH}
+OUTDIR=${HOME}/srv/zephyr/${ZEPHYR_GCC_VARIANT}/${PLATFORM}
+export LANG ZEPHYR_BASE PATH
+CCACHE_DIR="${HOME}/srv/ccache"
+CCACHE_UNIFY=1
+CCACHE_SLOPPINESS=file_macro,include_file_mtime,time_macros
+USE_CCACHE=1
+export CCACHE_DIR CCACHE_UNIFY CCACHE_SLOPPINESS USE_CCACHE
+env |grep '^ZEPHYR'
+
+echo ""
+echo "########################################################################"
+echo "    Build"
+echo "########################################################################"
+
+. deps/zephyr/zephyr-env.sh
+. ./zjs-env.sh
+make BOARD=${PLATFORM}
+
+cd ${WORKSPACE}
+mkdir -p out/${PLATFORM}
+cp outdir/${PLATFORM}/zephyr.bin out/${PLATFORM}/
+
+CCACHE_DIR=${CCACHE_DIR} ccache -M 30G
+CCACHE_DIR=${CCACHE_DIR} ccache -s
