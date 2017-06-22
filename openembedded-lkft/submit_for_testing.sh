c7bfe444a5682fd90a5294dc47168110dc478ffb
Milosz Wasilewski <milosz.wasilewski@linaro.org>
2017-06-22 17:24:33 +0100

    lkft: openembedded-lkft: fix incorrect condition
    Use git describe as version whenever it's available
    
    Change-Id: I7ae9572638b7f6d1d940caa0421a6051c3b615cf
    Signed-off-by: Milosz Wasilewski <milosz.wasilewski@linaro.org>

--- openembedded-lkft/submit_for_testing.sh
+++ openembedded-lkft/submit_for_testing.sh
@@ -4,7 +4,7 @@ set -e
 
 export KSELFTEST_SKIPLIST=""
 
-if [ -z "${KERNEL_DESCRIBE}" ]; then
+if [ ! -z "${KERNEL_DESCRIBE}" ]; then
     export QA_BUILD_VERSION=${KERNEL_DESCRIBE}
 else
     export QA_BUILD_VERSION=${KERNEL_COMMIT:0:12}
