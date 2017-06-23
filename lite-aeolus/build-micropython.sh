656c019a72fa5a24ede439c852e27190899d090a
Paul Sokolovsky <paul.sokolovsky@linaro.org>
2017-06-23 18:27:09 +0300

    pfalcon-aeolus-micropython: For selected boards, build minimal config.
    For boards with less than 256K ROM.
    
    Change-Id: I2999e090556362a91621af88b67daa8edf8245c7

--- lite-aeolus/build-micropython.sh
+++ lite-aeolus/build-micropython.sh
@@ -2,8 +2,19 @@ git clone --depth 1 https://git.linaro.org/lite/zephyr.git zephyr-rtos
 (cd zephyr-rtos; git clean -fdx)
 . zephyr-rtos/zephyr-env.sh
 
-make -C zephyr BOARD=${PLATFORM}
+
+small_rom() {
+    echo "arduino_101 foo_bar" | grep -F -w -q "$1"
+}
+
+cd zephyr
+if small_rom $1; then
+    ./make-minimal BOARD=${PLATFORM}
+else
+    make BOARD=${PLATFORM}
+fi
+
 
 cd ${WORKSPACE}
 mkdir -p out/${PLATFORM}
-cp outdir/${PLATFORM}/zephyr.bin out/${PLATFORM}/
+cp zephyr/outdir/${PLATFORM}/zephyr.bin out/${PLATFORM}/
