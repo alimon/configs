a773f63af7e1b5d5dd9c0c8eb5c9a404dfe97794
Paul Sokolovsky <paul.sokolovsky@linaro.org>
2017-06-23 12:20:37 +0300

    pfalcon-aeolus-jerryscript: Be sure to clone/init Zephyr.
    Change-Id: I9da7b9daf1166ecd3a8f5d8b550497bfbc5e3658

--- lite-aeolus/build-jerryscript.sh
+++ lite-aeolus/build-jerryscript.sh
@@ -1,3 +1,7 @@
+git clone --depth 1 https://git.linaro.org/lite/zephyr.git
+(cd zephyr; git clean -fdx)
+. zephyr/zephyr-env.sh
+
 make -f ./targets/zephyr/Makefile.zephyr BOARD=${PLATFORM}
 
 cd ${WORKSPACE}
