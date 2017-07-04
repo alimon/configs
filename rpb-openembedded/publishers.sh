a689451232486fcc7d4c5a8f2e8b046acc5cbfdd
Nicolas Dechesne <nicolas.dechesne@linaro.org>
2017-07-04 20:53:00 +0200

    rpb-openembedded: update build artifact locations
    * make sure that publish path ends with $BUILD_NUMBER/$DISTRO instead of
      $DISTRO/$BUILD_NUMBER as it makes it 'easier' to release a build later on.
      This is for rpb-openembedded-* jobs and the QC LT RPB build
    * move rpb-openembedded-* to snapshots.linaro.org instead of builds.96boards.org
    * use 2 steps to publish artifacts, one to upload and one to create
      "latest" link
    
    Change-Id: I43b199419db5c16adb919608f78a51213e8c6d61
    Signed-off-by: Nicolas Dechesne <nicolas.dechesne@linaro.org>

--- rpb-openembedded/publishers.sh
+++ rpb-openembedded/publishers.sh
@@ -4,6 +4,7 @@
 test -d ${HOME}/bin || mkdir ${HOME}/bin
 wget -q https://git.linaro.org/ci/publishing-api.git/blob_plain/HEAD:/linaro-cp.py -O ${HOME}/bin/linaro-cp.py
 time python ${HOME}/bin/linaro-cp.py \
-  --server ${PUBLISH_SERVER} \
-  --link-latest \
   ${DEPLOY_DIR_IMAGE}/ ${PUB_DEST}
+time python ${HOME}/bin/linaro-cp.py \
+  --make-link \
+  $(dirname ${PUB_DEST})
