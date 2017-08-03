15d962dab5d9f7c20a2290032d4b8ca75d06f963
Nicolas Dechesne <nicolas.dechesne@linaro.org>
2017-08-03 17:55:25 +0200

    rpb-openembedded/publishers.sh: fix publishing to 96boards.org
    in a689451232486fcc7d4c5a8f2e8b046acc5cbfdd, the publishing code was redesigned
    to allow publishing on linaro.org or 96boards.org, and publishing to
    96boards.org is broken since we are missing an argument with the server name
    (which default to linaro.org).
    
    Change-Id: I896f723e505312fcff47be79612b4cde680253e8
    Signed-off-by: Nicolas Dechesne <nicolas.dechesne@linaro.org>

--- rpb-openembedded/publishers.sh
+++ rpb-openembedded/publishers.sh
@@ -4,7 +4,9 @@
 test -d ${HOME}/bin || mkdir ${HOME}/bin
 wget -q https://git.linaro.org/ci/publishing-api.git/blob_plain/HEAD:/linaro-cp.py -O ${HOME}/bin/linaro-cp.py
 time python ${HOME}/bin/linaro-cp.py \
+  --server ${PUBLISH_SERVER} \
   ${DEPLOY_DIR_IMAGE}/ ${PUB_DEST}
 time python ${HOME}/bin/linaro-cp.py \
+  --server ${PUBLISH_SERVER} \
   --make-link \
   $(dirname ${PUB_DEST})
