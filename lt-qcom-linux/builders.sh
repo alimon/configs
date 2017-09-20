2251862303ee78417fe1ed3b01a62cfaf4b4af29
Nicolas Dechesne <nicolas.dechesne@linaro.org>
2017-09-20 16:15:17 +0200

    lt-qcom-linux: various updates and fixes
    * split the main job into a standalone builders.sh file
    * beautify variable names
    * remove variable mangling from the trigger job, which now only reports info
      from git into the main job
    * add KERNEL_CONFIGS
    * fixup package name and package version:
     * pkgname is now made as linux-<kernel version>-qcomlt-<abi>
     * pkgversion is mostly based on 'git describe', and -rc is replaced by ~rc, and
       includes BUILD_NUMBER
     * for now, the <abi> is set to be BUILD_NUMBER, this can potentially be
       optimzed a bit, later.
    * the kernel sources are checked out into 'linux' folder since packages are
      created in ../
    
    Change-Id: I306c9030eb1fe4bbac7ce9db58ade936fe33298c
    Signed-off-by: Nicolas Dechesne <nicolas.dechesne@linaro.org>

---
+++ lt-qcom-linux/builders.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+set -ex
+
+echo "Starting ${JOB_NAME} with the following parameters:"
+echo "KERNEL_DESCRIBE: ${KERNEL_DESCRIBE}"
+echo "KERNEL_VERSION: ${KERNEL_VERSION}"
+echo "KERNEL_BRANCH: ${KERNEL_BRANCH}"
+echo "GIT_COMMIT: ${GIT_COMMIT}"
+echo "GIT_BRANCH: ${GIT_BRANCH}"
+
+toolchain_url=http://releases.linaro.org/components/toolchain/binaries/6.3-2017.02/aarch64-linux-gnu/gcc-linaro-6.3.1-2017.02-x86_64_aarch64-linux-gnu.tar.xz
+tcdir=${HOME}/srv/toolchain
+tcbindir="${tcdir}/$(basename $toolchain_url .tar.xz)/bin"
+
+export ARCH=arm64
+export CROSS_COMPILE="ccache $(basename $(ls -1 ${tcbindir}/*-gcc) gcc)"
+export PATH=${tcbindir}:$PATH
+
+# SRCVERSION is the main kernel version, e.g. <version>.<patchlevel>.0.
+# PKGVERSION is similar to make kernelrelease, but reimplemented, since it requires setting up the build (and all tags).
+# e.g. SRCVERSION -> 4.9.0, PKGVERSION -> 4.9.47-530-g244b81e58a54, which leads to
+#      linux-4.9.0-qcomlt-99 (4.9.47-530-g244b81e58a54-99)
+SRCVERSION=$(echo ${KERNEL_VERSION} | sed 's/\(.*\)\..*/\1.0/')
+PKGVERSION=$(echo ${KERNEL_VERSION} | sed -e 's/\.0-rc/\.0~rc/')$(echo ${KERNEL_DESCRIBE} | awk -F- '{printf("-%05d-%s", $(NF-1),$(NF))}')
+
+cd ${WORKSPACE}/linux
+
+make ${KERNEL_CONFIGS}
+make savedefconfig
+cp defconfig arch/${ARCH}/configs
+
+make KERNELRELEASE=${SRCVERSION}-qcomlt-${BUILD_NUMBER} \
+     KDEB_PKGVERSION=${PKGVERSION}-${BUILD_NUMBER} \
+     KDEB_CHANGELOG_DIST=sid \
+     DEBEMAIL="dragonboard@lists.96boards.org" \
+     DEBFULLNAME="Linaro Qualcomm Landing Team" \
+     -j`nproc` deb-pkg
