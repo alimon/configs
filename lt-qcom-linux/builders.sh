107c7074632500f7900ba29b2ad7ed405d5854e4
Nicolas Dechesne <nicolas.dechesne@linaro.org>
2017-09-28 17:13:28 -0700

    lt-qcom-linux: do not use BUILD_NUMBER in source package name
    Let's step back and not use the BUILD_NUMBER as the package ABI. We already have
    the kernel version (x.y) in the source package. If we use BUILD_NUMBER in the
    source package name we are going to end up with a huge numbers of source
    packages in OBS.
    
    We can add -1, -2, if needed , when ABI breaks are detected.
    
    Change-Id: I1eb1986e95671ded133b5e3574890d725421b7af
    Signed-off-by: Nicolas Dechesne <nicolas.dechesne@linaro.org>

--- lt-qcom-linux/builders.sh
+++ lt-qcom-linux/builders.sh
@@ -19,7 +19,7 @@ export PATH=${tcbindir}:$PATH
 # SRCVERSION is the main kernel version, e.g. <version>.<patchlevel>.0.
 # PKGVERSION is similar to make kernelrelease, but reimplemented, since it requires setting up the build (and all tags).
 # e.g. SRCVERSION -> 4.9.0, PKGVERSION -> 4.9.47-530-g244b81e58a54, which leads to
-#      linux-4.9.0-qcomlt-99 (4.9.47-530-g244b81e58a54-99)
+#      linux-4.9.0-qcomlt (4.9.47-530-g244b81e58a54-99)
 SRCVERSION=$(echo ${KERNEL_VERSION} | sed 's/\(.*\)\..*/\1.0/')
 PKGVERSION=$(echo ${KERNEL_VERSION} | sed -e 's/\.0-rc/\.0~rc/')$(echo ${KERNEL_DESCRIBE} | awk -F- '{printf("-%05d-%s", $(NF-1),$(NF))}')
 
@@ -29,7 +29,7 @@ make ${KERNEL_CONFIGS}
 make savedefconfig
 cp defconfig arch/${ARCH}/configs
 
-make KERNELRELEASE=${SRCVERSION}-qcomlt-${BUILD_NUMBER} \
+make KERNELRELEASE=${SRCVERSION}-qcomlt \
      KDEB_PKGVERSION=${PKGVERSION}-${BUILD_NUMBER} \
      KDEB_CHANGELOG_DIST=sid \
      DEBEMAIL="dragonboard@lists.96boards.org" \
