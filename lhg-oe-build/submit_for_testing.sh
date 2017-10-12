ed05a0af54fe179f8242a602661a14066b37bf7f
Andrey Konovalov <andrey.konovalov@linaro.org>
2017-10-12 18:00:39 +0300

    lhg-oe-build: stop using uninitialized var for --git-commit argument
    Change-Id: Iadbc301f3665f72ce28e4dab9d1416808dd86ef6
    Signed-off-by: Andrey Konovalov <andrey.konovalov@linaro.org>

--- lhg-oe-build/submit_for_testing.sh
+++ lhg-oe-build/submit_for_testing.sh
@@ -2,7 +2,7 @@
 
 set -ex
 
-QA_BUILD_VERSION=${MANIFEST_COMMIT:0:12}
+MANIFEST_COMMIT=$(cd .repo/manifests && git rev-parse --short HEAD)
 
 rm -rf configs
 git clone --depth 1 http://git.linaro.org/ci/job/configs.git
@@ -15,6 +15,6 @@ python configs/openembedded-lkft/submit_for_testing.py \
   --qa-server ${QA_SERVER} \
   --qa-server-team lhg \
   --qa-server-project ${QA_SERVER_PROJECT} \
-  --git-commit ${QA_BUILD_VERSION} \
+  --git-commit ${MANIFEST_COMMIT:0:12} \
   --template-path configs/lhg-oe-build/lava-job-definitions \
   --template-names template-eme-clearkey.yaml
