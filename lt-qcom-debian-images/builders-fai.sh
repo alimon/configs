2780c3917511d78bde370105faba477c2d1be16e
Riku Voipio <riku.voipio@linaro.org>
2017-10-13 12:27:32 +0300

    lt-qcom-debian-images: fai: faster builds
    Base image creation:    15  min -> 6 min
    Images generated after: 9   min -> 1-2 min
    Image compression:      5-9 min -> 1-1.5min
    
    Change-Id: Ib1c7cb443e7d8a92f596e618ace984020ab7fa60
    Signed-off-by: Riku Voipio <riku.voipio@linaro.org>

--- lt-qcom-debian-images/builders-fai.sh
+++ lt-qcom-debian-images/builders-fai.sh
@@ -7,7 +7,7 @@ if ! sudo DEBIAN_FRONTEND=noninteractive apt-get -q=2 update; then
   sleep 15
   sudo DEBIAN_FRONTEND=noninteractive apt-get -q=2 update || true
 fi
-pkg_list="fai-server fai-setup-storage qemu-utils procps mtools zip android-tools-fsutils android-tools-mkbootimg"
+pkg_list="fai-server fai-setup-storage qemu-utils procps mtools pigz zip android-tools-fsutils android-tools-mkbootimg"
 if ! sudo DEBIAN_FRONTEND=noninteractive apt-get -q=2 install -y ${pkg_list}; then
   echo "INFO: apt install error - try again in a moment"
   sleep 15
@@ -32,6 +32,8 @@ Build description:
 * FAI commit: "$GIT_COMMIT":$GIT_URL/commit/?id=$GIT_COMMIT
 EOF
 
+sudo mount -o tmpfs tmpfs /tmp
+
 for rootfs in ${ROOTFS}; do
 
     rootfs_sz=$(echo $rootfs | cut -f2 -d,)
@@ -42,9 +44,9 @@ for rootfs in ${ROOTFS}; do
          --hostname linaro-${rootfs} \
          -S ${rootfs_sz} \
          --class $(echo SAVECACHE,${OS_FLAVOUR},DEBIAN,LINARO,QCOM,${rootfs},${FAI_BOARD_CLASS},RAW | tr '[:lower:]' '[:upper:]') \
-         out/${VENDOR}-${OS_FLAVOUR}-${rootfs}-${PLATFORM_NAME}-${VERSION}.img.raw
+         /tmp/${VENDOR}-${OS_FLAVOUR}-${rootfs}-${PLATFORM_NAME}-${VERSION}.img.raw
 
-    rootfs_sz_real=$(du -h out/${VENDOR}-${OS_FLAVOUR}-${rootfs}-${PLATFORM_NAME}-${VERSION}.img.raw | cut -f1)
+    rootfs_sz_real=$(du -h /tmp/${VENDOR}-${OS_FLAVOUR}-${rootfs}-${PLATFORM_NAME}-${VERSION}.img.raw | cut -f1)
 
     # make sure that there are the same for all images, in case we build more than 1 image
     if [ -f MD5SUM ]; then
@@ -53,11 +55,11 @@ for rootfs in ${ROOTFS}; do
         md5sum out/{vmlinuz-*,config-*,$(basename ${DTBS})} > MD5SUM
     fi
 
-    img2simg out/${VENDOR}-${OS_FLAVOUR}-${rootfs}-${PLATFORM_NAME}-${VERSION}.img.raw out/${VENDOR}-${OS_FLAVOUR}-${rootfs}-${PLATFORM_NAME}-${VERSION}.img
-    sudo rm -f out/${VENDOR}-${OS_FLAVOUR}-${rootfs}-${PLATFORM_NAME}-${VERSION}.img.raw
+    img2simg /tmp/${VENDOR}-${OS_FLAVOUR}-${rootfs}-${PLATFORM_NAME}-${VERSION}.img.raw out/${VENDOR}-${OS_FLAVOUR}-${rootfs}-${PLATFORM_NAME}-${VERSION}.img
+    sudo rm -f /tmp/${VENDOR}-${OS_FLAVOUR}-${rootfs}-${PLATFORM_NAME}-${VERSION}.img.raw
 
     # Compress image(s)
-    gzip -9 out/${VENDOR}-${OS_FLAVOUR}-${rootfs}-${PLATFORM_NAME}-${VERSION}.img
+    pigz -9 out/${VENDOR}-${OS_FLAVOUR}-${rootfs}-${PLATFORM_NAME}-${VERSION}.img
 
     # dpkg -l output
     mv out/packages.txt out/${VENDOR}-${OS_FLAVOUR}-${rootfs}-${PLATFORM_NAME}-${VERSION}.packages
@@ -76,4 +78,4 @@ mkbootimg \
     --pagesize "${BOOTIMG_PAGESIZE}" \
     --base "0x80000000" \
     --cmdline "root=/dev/disk/by-partlabel/${ROOTFS_PARTLABEL} rw rootwait console=tty0 console=${SERIAL_CONSOLE},115200n8"
-gzip -9 out/boot-${VENDOR}-${OS_FLAVOUR}-${PLATFORM_NAME}-${VERSION}.img
+pigz -9 out/boot-${VENDOR}-${OS_FLAVOUR}-${PLATFORM_NAME}-${VERSION}.img
