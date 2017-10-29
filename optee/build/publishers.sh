c9a76421423157e6c2ddb58d6ec9c5b5c0a1cf29
Fathi Boudra <fathi.boudra@linaro.org>
2017-10-29 09:47:42 +0200

    optee-build: juno: generate a recovery image to flash in NOR flash
    ftp-upload isn't supported by LAVA. In order to flash our own bl1/fip
    binaries, we need to use the recovery image to flash in NOR flash.
    
    Leave a commented section in case we need to adjust to give more space for
    a larger kernel or ramdisk. Right now, kernel is 15M and ramdisk is 10.1M.
    They fit in the default image configuration.
    
    Change-Id: I574d83f242ee3aefedabca6065edffac3f4c4ad3
    Signed-off-by: Fathi Boudra <fathi.boudra@linaro.org>

--- optee/build/publishers.sh
+++ optee/build/publishers.sh
@@ -11,6 +11,22 @@ cp -a \
   ${WORKSPACE}/${repo_proj}/vexpress-firmware/SOFTWARE/bl0.bin \
   ${WORKSPACE}/out-publish/
 
+# Create new recovery image
+wget -q http://releases.linaro.org/members/arm/platforms/17.04/juno-latest-oe-uboot.zip -O juno-latest-oe-uboot.zip
+unzip -d juno-oe-uboot juno-latest-oe-uboot.zip
+
+for file in bl1.bin fip.bin ramdisk.img Image juno.dtb juno-r1.dtb juno-r2.dtb; do
+  cp -a ${WORKSPACE}/out-publish/${file} ${WORKSPACE}/juno-oe-uboot/SOFTWARE/${file}
+done
+
+# Note: uncomment and adjust adresses to give more space for a larger kernel or ramdisk
+#sed -i -e 's/^NOR4ADDRESS:.*/NOR4ADDRESS: 0x02200000          ;Image Flash Address/g' \
+#  ${WORKSPACE}/juno-oe-uboot/SITE1/*/images.txt
+cat ${WORKSPACE}/juno-oe-uboot/SITE1/*/images.txt
+
+cd ${WORKSPACE}/juno-oe-uboot/
+zip -r ${WORKSPACE}/out-publish/juno-oe-uboot.zip .
+
 # Publish
 test -d ${HOME}/bin || mkdir ${HOME}/bin
 wget -q https://git.linaro.org/ci/publishing-api.git/blob_plain/HEAD:/linaro-cp.py -O ${HOME}/bin/linaro-cp.py
