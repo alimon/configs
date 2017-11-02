8071d67ab2a1ea71bd6162274faa8586ac32d93e
Yibo Cai <yibo.cai@linaro.org>
2017-11-02 16:11:34 +0800

    compass-deck: new job to build compass API server
    API server container for OPNFV Compass4NFV installer
    
    Change-Id: If8ccd6a676d9ec0554823c911c48b7d4a6d675e1
    Signed-off-by: Yibo Cai <yibo.cai@linaro.org>

---
+++ compass/compass-deck.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+set -ex
+
+trap cleanup_exit INT TERM EXIT
+
+cleanup_exit()
+{
+    rm -rf ${HOME}/.docker
+}
+
+mkdir -p ${HOME}/.docker
+sed -e "s|\${DOCKER_AUTH}|${DOCKER_AUTH}|" < ${WORKSPACE}/config.json > ${HOME}/.docker/config.json
+chmod 0600 ${HOME}/.docker/config.json
+
+docker build -t linaro/compass-deck:latest compass-deck
+docker push linaro/compass-deck:latest
