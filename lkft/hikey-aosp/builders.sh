0eace17fb0e026e7e7b61a0771507e4eb3f30fdd
Vishal Bhoj <vishal.bhoj@linaro.org>
2017-11-08 23:31:42 +0530

    lkft: android: remove build_config.json
    We don't use build_config to pass kernel commit
    and other metadata now.
    
    Change-Id: Idf7454f9861291eae24a29e166469f00f588bd9f
    Signed-off-by: Vishal Bhoj <vishal.bhoj@linaro.org>

--- lkft/hikey-aosp/builders.sh
+++ lkft/hikey-aosp/builders.sh
@@ -20,10 +20,3 @@ python mkbootimg \
   --ramdisk ramdisk.img \
   --output out/boot.img
 xz out/boot.img
-
-wget -q ${REFERENCE_BUILD_URL}/build_config.json -O out/build_config.json
-remote=$(git remote -v | grep push | cut -d" " -f1 | cut -f2)
-commit=$(git log | grep commit | cut -d" " -f2)
-sed -i "s|\"kernel_repo\" : \"|\"kernel_repo\" : \"$remote|g" out/build_config.json
-sed -i "s|\"kernel_commit_id\" : \"|\"kernel_commit_id\" : \"$commit|g" out/build_config.json
-sed -i "s|\"kernel_branch\" : \"|\"kernel_branch\" : \"$KERNEL_BRANCH|g" out/build_config.json
