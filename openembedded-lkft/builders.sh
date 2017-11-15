eadb9c70ea32ce47aa5e802937718640f799550c
Fathi Boudra <fathi.boudra@linaro.org>
2017-11-15 14:00:31 +0200

    openembedded-lkft: x86: add libelf-dev build dependency
    See https://bugs.linaro.org/show_bug.cgi?id=3459
    
    Mainline fails to build with:
      kernel-source/Makefile:938:
      *** "Cannot generate ORC metadata for CONFIG_UNWINDER_ORC=y,
      please install libelf-dev, libelf-devel or elfutils-libelf-devel".
      Stop.
    
    Workaround by adding the build dependency. However, it isn't the proper
    fix. The build dependency is supposed to be filled by adding elfutils
    dependency in the OE recipe. It's still not enough and requires more time
    to fix properly the issue.
    
    Change-Id: Id17eb1690202560105827f3ebc1912232f02294b
    Signed-off-by: Fathi Boudra <fathi.boudra@linaro.org>

--- openembedded-lkft/builders.sh
+++ openembedded-lkft/builders.sh
@@ -20,7 +20,7 @@ if ! sudo DEBIAN_FRONTEND=noninteractive apt-get -q=2 update; then
   sleep 15
   sudo DEBIAN_FRONTEND=noninteractive apt-get -q=2 update || true
 fi
-pkg_list="python-pip android-tools-fsutils chrpath cpio diffstat gawk libmagickwand-dev libmath-prime-util-perl libsdl1.2-dev libssl-dev python-requests texinfo vim-tiny whiptail"
+pkg_list="python-pip android-tools-fsutils chrpath cpio diffstat gawk libmagickwand-dev libmath-prime-util-perl libsdl1.2-dev libssl-dev python-requests texinfo vim-tiny whiptail libelf-dev"
 if ! sudo DEBIAN_FRONTEND=noninteractive apt-get -q=2 install -y ${pkg_list}; then
   echo "INFO: apt install error - try again in a moment"
   sleep 15
