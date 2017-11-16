ffff475c4df5c8ff3c5266318b20d2cadacf9fd0
Julien Duraj <julien.duraj@linaro.org>
2017-11-16 14:41:43 +0000

    ART: bring `device not found` issues to attention
    Change-Id: I9744a98c9013c537b08a3132e3e5a3c382ecc2f0

--- linaro-art/global.groovy
+++ linaro-art/global.groovy
@@ -2,3 +2,6 @@
 if (manager.logContains(".*Unexpected termination of the channel.*")) {
   manager.addShortText("Infrastructure problem", "black", "orange", "1px", "red")
 }
+if (manager.logContains(".*device .* not found.*")) {
+  manager.addShortText("device not found", "black", "orange", "1px", "red")
+}
