e277c2bcb90b3439e7482a8315bbd0f0dbfd32f9
Aníbal Limón <anibal.limon@linaro.org>
2017-11-23 16:15:47 -0600

    lt-qcom-debian-images/postbuild.groovy: Add new line for build location
    The build description for debian images consist on the build location
    and the qa reports job urls [1] without newline the info is mixed.
    
    [1] https://ci.linaro.org/job/lt-qcom-debian-images-dragonboard410c/300/
    
    Change-Id: Ia7f7e52242ed31656895881608e2a8425c0ff736
    Signed-off-by: Aníbal Limón <anibal.limon@linaro.org>

--- lt-qcom-debian-images/postbuild.groovy
+++ lt-qcom-debian-images/postbuild.groovy
@@ -2,7 +2,7 @@ if (manager.build.result == hudson.model.Result.SUCCESS) {
   def pub_dest = manager.build.buildVariables.get('PUB_DEST')
 
   def url = "http://builds.96boards.org/${pub_dest}/"
-  def desc = "&nbsp;<a href='${url}'>Build location<t/a>"
+  def desc = "&nbsp;<a href='${url}'>Build location<t/a><br />"
 
   manager.build.setDescription(desc)
 }
