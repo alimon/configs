acf31ec6d2098669d7a779d3844ba851dd618f17
Linaro CI <minseong.kim@linaro.org>
2017-12-15 06:00:01 +0000

    Clang master error is fixed upstream:
    Errorneous commit: 25b45aa81854313486df891985cdd7ef1ec09780
    Fix: https://reviews.llvm.org/rC320709
    --> Therefore the apply of the clang patch is removed
    
    Change-Id: I09c13a5bb8628753463a0bb3e985d33d69ec4ee6

--- android-clang-toolchain/builders.sh
+++ android-clang-toolchain/builders.sh
@@ -40,10 +40,10 @@ if [ ! -d clang+llvm-5.0.0-linux-x86_64-ubuntu14.04 ]; then
 fi
 
 # Temporary clang patch for 25b45aa81854313486df891985cdd7ef1ec09780
-cd llvm/tools/clang
-git clone https://git.linaro.org/people/minseong.kim/aosp_patches_for_upstream_clang.git
-patch -p1 < aosp_patches_for_upstream_clang/revert_25b45a.patch
-cd ../../../
+# cd ${HOME}/srv/aosp/${JOB_NAME}/llvm/tools/clang
+# git clone https://git.linaro.org/people/minseong.kim/aosp_patches_for_upstream_clang.git
+# patch -p1 < aosp_patches_for_upstream_clang/revert_25b45a.patch
+# cd ${HOME}/srv/aosp/${JOB_NAME}
 
 cd llvm
 mkdir -p build/clang-master
