523a9a754965e48f0ffa8774e36d0dc73b6cbd29
Riku Voipio <riku.voipio@linaro.org>
2017-12-18 11:14:42 +0200

    96boards-hikey-linux: fix parameters
    Fix parameter name and work path
    
    Change-Id: I50a78e2ec93863b983a7f6bf286d9cd66b644fa6

--- 96boards-hikey-linux/builders.sh
+++ 96boards-hikey-linux/builders.sh
@@ -2,6 +2,7 @@
 
 set -ex
 
+cd linux/
 KERNEL_DESCRIBE=`git describe --match 'v4*'| sed -e 's,^v,,'`
 KERNEL_VERSION=`echo ${KERNEL_DESCRIBE}|sed 's,-g.*,,' `
 
@@ -13,7 +14,7 @@ echo "GIT_COMMIT: ${GIT_COMMIT}"
 echo "GIT_BRANCH: ${GIT_BRANCH}"
 
 sudo apt-get update -q
-sudo apt-get install -y bc kmod cpio
+sudo apt-get install -q -y bc kmod cpio
 
 SRCVERSION=$(echo ${KERNEL_VERSION} |sed 's,-rc,,')
 PKGVERSION=$(echo ${KERNEL_DESCRIBE} |sed -e 's,-rc,~rc,')
