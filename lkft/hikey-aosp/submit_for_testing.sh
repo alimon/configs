aa6195ba0cd5e8c46c61404caa8236ce78f8768f
Vishal Bhoj <vishal.bhoj@linaro.org>
2018-01-08 19:14:19 +0530

    lkft: hikey: add job with android 8.1
    Change-Id: I1f8d2eb2ad18a26269f7eb8fd3d712244399d038
    Signed-off-by: Vishal Bhoj <vishal.bhoj@linaro.org>

---
+++ lkft/hikey-aosp/submit_for_testing.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+export DEVICE_TYPE=hi6220-hikey
+export LAVA_SERVER=https://lkft.validation.linaro.org/RPC2/
+export PUB_DEST=android/lkft/${JOB_NAME}/${BUILD_NUMBER}
+export DOWNLOAD_URL=http://snapshots.linaro.org/${PUB_DEST}
+export KERNEL_COMMIT=${SRCREV_kernel}
+export ANDROID_VERSION=$(echo $REFERENCE_BUILD_URL | awk -F"/" '{print$(NF-1)}')
+export VTS_VERSION=$(echo $VTS_URL | awk -F"/" '{print$(NF-1)}')
+
+if [ ! -z "${KERNEL_DESCRIBE}" ]; then
+    export QA_BUILD_VERSION=${KERNEL_DESCRIBE}
+else
+    export QA_BUILD_VERSION=${KERNEL_COMMIT:0:12}
+fi
+
+rm -rf configs
+git clone --depth 1 http://git.linaro.org/ci/job/configs.git
+
+python configs/openembedded-lkft/submit_for_testing.py \
+    --device-type ${DEVICE_TYPE} \
+    --build-number ${BUILD_NUMBER} \
+    --lava-server ${LAVA_SERVER} \
+    --qa-server ${QA_SERVER} \
+    --qa-server-team lkft \
+    --qa-server-project ${QA_SERVER_PROJECT} \
+    --git-commit ${QA_BUILD_VERSION} \
+    --template-path configs/lkft/lava-job-definitions \
+    --template-names template-boot.yaml template-vts-kernel-part1.yaml template-vts-kernel-part2.yaml template-vts-kernel-part3.yaml template-vts-kernel-part4.yaml \
+    --quiet
+
+python configs/openembedded-lkft/submit_for_testing.py \
+    --device-type ${DEVICE_TYPE} \
+    --build-number ${BUILD_NUMBER} \
+    --lava-server ${LAVA_SERVER} \
+    --qa-server ${QA_SERVER} \
+    --qa-server-team staging-lkft \
+    --qa-server-project ${QA_SERVER_PROJECT}-vts-staging \
+    --git-commit ${QA_BUILD_VERSION} \
+    --template-path configs/lkft/lava-job-definitions \
+    --template-names template-vts-staging-kernel.yaml \
+    --quiet
