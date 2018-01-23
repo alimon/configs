67f768d3fdf611265e560a10a704d786053133d2
Aníbal Limón <anibal.limon@linaro.org>
2018-01-23 16:51:33 -0600

    lt-qcom-linux-integration: Add support to publish into snapshots
    We need these artifacts published to be able send a LAVA test
    job with the URL's.
    
    Change-Id: Iac240a81aafa70b5afd6dbef9524b90ce78542bb
    Signed-off-by: Aníbal Limón <anibal.limon@linaro.org>

---
+++ lt-qcom-linux-integration/publishers.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+set -ex
+
+# Create MD5SUMS file
+(cd out && md5sum * > MD5SUMS.txt)
+
+wget -q ${BUILD_URL}consoleText -O out/build-log-$(echo ${JOB_NAME}|sed -e 's/[^A-Za-z0-9._-]/_/g')-${BUILD_NUMBER}.txt
+
+# Publish to snapshots
+test -d ${HOME}/bin || mkdir ${HOME}/bin
+wget https://git.linaro.org/ci/publishing-api.git/blob_plain/HEAD:/linaro-cp.py -O ${HOME}/bin/linaro-cp.py
+time python ${HOME}/bin/linaro-cp.py \
+     --server ${PUBLISH_SERVER} \
+     --link-latest \
+     out ${PUB_DEST}
