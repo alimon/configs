8638baa8799928857d26d7d608383cb3b3a82d8c
Aníbal Limón <anibal.limon@linaro.org>
2018-01-30 12:38:08 -0600

    lt-qcom-linux-integration: Add support for QA reports/LAVA testing
    For now only boots and runs some commands like uname, df, free and
    listing the contents of /dev, /proc and /sys.
    
    This job copies the content of LAVA test overlay inside the Android
    boot image ramdisk.
    
    For the LAVA testing [1] i created an Android boot image with Kernel
    CI last build [2] and the linaro-minimal-initramfs-image using
    builders.sh script.
    
    [1] https://lkft.validation.linaro.org/scheduler/job/106119
    [2] https://kernelci.org/boot/id/5a6b333259b514ee2cafa173/
    
    Change-Id: Iab1eae97faa25ebf1f13d37997b24a2a79cc6fc6
    Signed-off-by: Aníbal Limón <anibal.limon@linaro.org>

---
+++ lt-qcom-linux-integration/submit_for_testing.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+rm -rf configs
+git clone --depth 1 http://git.linaro.org/ci/job/configs.git
+
+# Install jinja2-cli and ruamel.yaml, required by submit_for_testing.py
+pip install --user --force-reinstall jinja2-cli ruamel.yaml
+
+export BOOT_URL=${PUBLISH_SERVER}${PUB_DEST}/${BOOT_FILE}
+export BOOT_URL_COMP=
+export LXC_BOOT_FILE=$(basename ${BOOT_URL})
+
+case "${MACHINE}" in
+  dragonboard410c)
+    export LAVA_DEVICE_TYPE="dragonboard-410c"
+
+    python configs/openembedded-lkft/submit_for_testing.py \
+        --device-type ${LAVA_DEVICE_TYPE} \
+        --build-number ${BUILD_NUMBER} \
+        --lava-server ${LAVA_SERVER} \
+        --qa-server ${QA_SERVER} \
+        --qa-server-team qcomlt \
+        --qa-server-project linux-integration \
+        --git-commit ${BUILD_NUMBER} \
+        --template-path configs/lt-qcom-linux-integration/lava-job-definitions \
+        --template-base-pre base_template.yaml \
+        --template-names template.yaml
+    ;;
+  *)
+    echo "Skip LAVA_DEVICE_TYPE for ${MACHINE}"
+    ;;
+esac
