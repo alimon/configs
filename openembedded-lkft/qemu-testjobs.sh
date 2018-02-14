07cb3e49c35b46380bced4ca94d326ef80a84dc9
Milosz Wasilewski <milosz.wasilewski@linaro.org>
2018-02-14 16:41:42 +0000

    openembedded-lkft: enable testing on QEMU
    Change-Id: I7c4c0eca3a393544da8b6f20fe19a0c0d8db19e7
    Signed-off-by: Milosz Wasilewski <milosz.wasilewski@linaro.org>

---
+++ openembedded-lkft/qemu-testjobs.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Submit QEMU jobs
+case "${MACHINE}" in
+  hikey)
+    DEVICE_TYPE=qemu_arm64
+    ;;
+  intel-core2-32)
+    DEVICE_TYPE=qemu_x86_64
+    ;;
+  *)
+    unset DEVICE_TYPE
+    ;;
+esac
+
+echo "DEVICE_TYPE=${DEVICE_TYPE}" > qemu_device_type_parameters
+
