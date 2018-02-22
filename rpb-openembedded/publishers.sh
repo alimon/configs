c288bcaa962301795794346931b562e7e1b3249a
Fathi Boudra <fathi.boudra@linaro.org>
2018-02-22 13:37:58 +0200

    Revert "rpb-openembedded/morty: debug publishing of artifacts"
    This reverts commit a28de0117853e257aca5ffd7c36fd34a350ab2b6.
    
    Change-Id: I80605a7704b4358e20dc3e686589ac7599fab5be

--- rpb-openembedded/publishers.sh
+++ rpb-openembedded/publishers.sh
@@ -1,12 +1,8 @@
 #!/bin/bash
 
-set -x
-
-PUBLISH_SCRIPT=${PUBLISH_SCRIPT:-https://git.linaro.org/ci/publishing-api.git/blob_plain/HEAD:/linaro-cp.py}
-
 # Publish
 test -d ${HOME}/bin || mkdir ${HOME}/bin
-wget -q ${PUBLISH_SCRIPT} -O ${HOME}/bin/linaro-cp.py
+wget -q https://git.linaro.org/ci/publishing-api.git/blob_plain/HEAD:/linaro-cp.py -O ${HOME}/bin/linaro-cp.py
 time python ${HOME}/bin/linaro-cp.py \
   --server ${PUBLISH_SERVER} \
   ${DEPLOY_DIR_IMAGE}/ ${PUB_DEST}
