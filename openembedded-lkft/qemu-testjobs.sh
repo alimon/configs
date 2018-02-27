a1be9c728972ec71f500bc7cf90df2daed7c6c7c
Dan Rue <dan.rue@linaro.org>
2018-02-27 16:23:14 -0600

    LKFT: Enable QEMU on x86-64
    Enable x86-64 qemu jobs, which boot reliably. Keep arm64 (hikey) qemu
    disabled until https://projects.linaro.org/browse/CTT-997 is resolved.
    
    Change-Id: Ia33cd550c4d63baaebd69c074ac596800854b118
    Signed-off-by: Dan Rue <dan.rue@linaro.org>

--- openembedded-lkft/qemu-testjobs.sh
+++ openembedded-lkft/qemu-testjobs.sh
@@ -2,9 +2,9 @@
 
 # Submit QEMU jobs
 case "${MACHINE}" in
-  hikey)
-    DEVICE_TYPE=qemu_arm64
-    ;;
+#  hikey)
+#    DEVICE_TYPE=qemu_arm64
+#    ;;
   intel-core2-32)
     DEVICE_TYPE=qemu_x86_64
     ;;
