70e1ee5cda58c7f7923d5de730f46617f41ef87a
Marcin Juszkiewicz <marcin.juszkiewicz@linaro.org>
2018-03-06 13:14:48 +0100

    leg-kolla: create new jobs according to LEG-698
    We need images from stable/queens branch, images from development branch
    and then we need images for Linaro Developer Cloud (stable/queens plus
    some extra changes maintained in separate overlay).
    
    leg-kolla will build rocky-YYMMDD images (development)
    leg-kolla-queens builds images from stable/queens branch
    leg-kolla-queens-ldc is like above plus some changes
    
    Change-Id: Icaeb32a0730154191844eb24b297ffd2438d0803

--- leg-kolla/build.sh
+++ leg-kolla/build.sh
@@ -2,6 +2,22 @@
 # build dependencies on Debian:
 # git build-essential virtualenv python-dev libffi-dev libssl-dev
 
+kolla_branch=${BRANCH}
+kolla_ldc=${DEVCLOUD}
+kolla_options=
+
+if [ -z "${kolla_branch}" ]; then
+    kolla_branch=master
+    kolla_tag=rocky-$(date +"%Y%m%d") # use YYYYMMDD for tags
+else
+    if [ -z "${kolla_ldc}" ]; then
+        kolla_tag=queens-$(date +"%Y%m%d") # use YYYYMMDD for tags
+    else
+        kolla_tag=ldc-queens-$(date +"%Y%m%d") # use YYYYMMDD for tags
+        kolla_options="--template-override ./Linaro-overlay/linaro-override.j2 --docker-dir ./Linaro-overlay/docker/"
+    fi
+fi
+
 set -ex
 
 trap cleanup_exit INT TERM EXIT
@@ -17,7 +33,11 @@ chmod 0600 ${HOME}/.docker/config.json
 
 rm -rf ${WORKSPACE}/*
 
-git clone --depth 1 https://git.openstack.org/openstack/kolla
+git clone --depth 1 --branch ${kolla_branch} https://git.openstack.org/openstack/kolla
+
+if [ -n ${kolla_ldc} ]; then
+    git clone --depth 1 https://git.linaro.org/leg/sdi/kolla/ldc-overlay.git Linaro-overlay
+fi
 
 virtualenv --python=/usr/bin/python2 venv-for-kolla
 . venv-for-kolla/bin/activate
@@ -28,11 +48,10 @@ pip install -r requirements.txt
 
 mkdir -p logs/debian-source
 
-kolla_tag=queens-$(date +"%Y%m%d") # use YYYYMMDD for tags
-
 kolla_namespace=linaro
 ./tools/build.py --base debian \
                  --format none \
+                 ${kolla_options} \
                  --logs-dir logs/debian-source \
                  --pull \
                  --push \
@@ -47,5 +66,5 @@ docker images | grep ${kolla_tag} | sort
 # do in a loop as we remove in random order and some have children images
 for run in 1 2 3 4 5
 do
-	docker images | grep ${kolla_tag} | awk '{print $3}' | xargs docker rmi -f 2>&1 >/dev/null || true
+    docker images | grep ${kolla_tag} | awk '{print $3}' | xargs docker rmi -f 2>&1 >/dev/null || true
 done
