dc61884897b5c7e890718e221c0a384657716e34
Marcin Juszkiewicz <marcin.juszkiewicz@linaro.org>
2018-04-16 14:01:43 +0200

    leg-docker-registry-image: remove image in case it exists (from previous jobs)
    Change-Id: I2088eb40bb472435803e05f3ff014e3018cee1fd

--- leg-docker-registry-image/build.sh
+++ leg-docker-registry-image/build.sh
@@ -7,6 +7,7 @@ trap cleanup_exit INT TERM EXIT
 cleanup_exit()
 {
     rm -rf ${HOME}/.docker
+    docker rmi linaro/registry:2
 }
 
 mkdir -p ${HOME}/.docker
@@ -15,9 +16,10 @@ chmod 0600 ${HOME}/.docker/config.json
 
 rm -rf ${WORKSPACE}/*
 
+# remove it just in case it exists from previous jobs
+docker rmi linaro/registry:2
+
 git clone --depth 1 https://git.linaro.org/leg/sdi/docker-registry-image.git
 cd docker-registry-image
 docker build --tag "linaro/registry:2" .
 docker push linaro/registry:2
-
-docker rmi linaro/registry:2
