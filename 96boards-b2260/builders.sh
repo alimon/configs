d0e4f52558ee4ca2406ee697e9bbdb70f95cb0c3
Riku Voipio <riku.voipio@linaro.org>
2018-05-16 11:29:06 +0300

    96boards-b2260: switch to debian stretch
    Move B2260 to use fai tooling and updared from debian jessie to
    debian stretch. Implements BB-479
    
    Change-Id: I8ac73ed7beba17b0754b0a0fa5ada8d4149e10a2

--- 96boards-b2260/builders.sh
+++ 96boards-b2260/builders.sh
@@ -6,125 +6,87 @@ trap cleanup_exit INT TERM EXIT
 
 cleanup_exit()
 {
-  cd ${WORKSPACE}
-  sudo umount boot rootfs || true
-  sudo kpartx -dv out/b2260-stretch_*.img || true
-  sudo rm -rf boot rootfs || true
-  rm -rf lci-build-tools
-  rm -rf builddir*
-  sudo git clean -fdx --exclude=out
+  sudo umount -f /mnt||true
+  sudo kpartx -dv /tmp/work.raw || true
+  sudo umount -f /tmp||true
 }
 
-#
-# Pull required tooling into Docker container
-#
-sudo apt-get -q=2 update
-sudo apt-get -q=2 install -y kpartx python-requests linaro-image-tools
+if ! sudo DEBIAN_FRONTEND=noninteractive apt-get -q=2 update; then
+  echo "INFO: apt update error - try again in a moment"
+  sleep 15
+  sudo DEBIAN_FRONTEND=noninteractive apt-get -q=2 update || true
+fi
+pkg_list="fai-server fai-setup-storage qemu-utils procps pigz kpartx "
+if ! sudo DEBIAN_FRONTEND=noninteractive apt-get -q=2 install -y ${pkg_list}; then
+  echo "INFO: apt install error - try again in a moment"
+  sleep 15
+  sudo DEBIAN_FRONTEND=noninteractive apt-get -q=2 install -y ${pkg_list}
+fi
 
-export LANG=C
-export make_bootwrapper=false
-export make_install=true
-export kernel_flavour=multi-v7
-export use_config_fragment=1
-export conf_filenames="arch/arm/configs/multi_v7_defconfig arch/arm/configs/bluetooth_6lowpan.conf"
-export MAKE_DTBS=true
-export tcbindir="${HOME}/srv/toolchain/arm-tc-16.02/bin"
-export toolchain_url=http://releases.linaro.org/components/toolchain/binaries/5.3-2016.02/arm-linux-gnueabihf/gcc-linaro-5.3-2016.02-x86_64_arm-linux-gnueabihf.tar.xz
+set -ex
 
-# Enable bluetooth 6lowpan
-cat << EOF > arch/arm/configs/bluetooth_6lowpan.conf
-CONFIG_6LOWPAN=m
-CONFIG_6LOWPAN_DEBUGFS=y
-CONFIG_IEEE802154=m
-CONFIG_IEEE802154_NL802154_EXPERIMENTAL=y
-CONFIG_IEEE802154_6LOWPAN=m
-CONFIG_MAC802154=m
-CONFIG_BT_BNEP=m
-CONFIG_BT_BNEP_MC_FILTER=y
-CONFIG_BT_BNEP_PROTO_FILTER=y
-CONFIG_BT_6LOWPAN=m
-CONFIG_IEEE802154_FAKELB=m
-CONFIG_IEEE802154_AT86RF230=m
-CONFIG_IEEE802154_AT86RF230_DEBUGFS=y
-CONFIG_IEEE802154_MRF24J40=m
-CONFIG_IEEE802154_CC2520=m
-CONFIG_IEEE802154_ATUSB=m
-EOF
+# Create version string
+echo "$(date +%Y%m%d)-${BUILD_NUMBER}" > build-version
 
-rm -rf configs lci-build-tools
-git clone --depth 1 http://git.linaro.org/ci/lci-build-tools.git
-git clone --depth 1 http://git.linaro.org/ci/job/configs.git
-wget -q http://snapshots.linaro.org/96boards/b2260/linaro/u-boot/latest/u-boot.bin \
-     -O configs/96boards-b2260/boot/b2260/u-boot.bin
-bash -x lci-build-tools/jenkins_kernel_build_inst
-rm -rf out/dtbs
-cp -a linux-*.deb out/
-cp -a configs/96boards-b2260/boot out/
-
-mkimage -A arm -O linux -C none -T kernel -a 0x40080000 -e 0x40080000 -n Linux -d out/zImage out/uImage
-
-#
-# Create the hardware pack
-#
-cp -a configs/96boards-b2260/hwpacks/linaro-b2260 .
-
-VERSION=$(date +%Y%m%d)-${BUILD_NUMBER}
-linaro-hwpack-create --debug linaro-b2260 ${VERSION}
-linaro-hwpack-replace -t `ls hwpack_linaro-b2260_*_armhf_supported.tar.gz` -p `ls linux-image-*-linaro-multi-v7_*.deb` -r linux-image -d -i
-linaro-hwpack-replace -t `ls hwpack_linaro-b2260_*_armhf_supported.tar.gz` -p `ls linux-headers-*-linaro-multi-v7_*.deb` -r linux-headers -d -i
-
-#
-# Generate build information
-#
+# Build information
+mkdir -p out
 cat > out/HEADER.textile << EOF
 
 h4. 96boards STiH410 B2260 - CE Debian
 
 Build description:
-* Build URL: "${BUILD_URL}":${BUILD_URL}
-* Kernel tree: "${GIT_URL}":${GIT_URL}
-* Kernel branch: ${GIT_BRANCH}
-* Kernel commit: "${GIT_COMMIT}":https://github.com/Linaro/rpk/commit/${GIT_COMMIT}
-* Kernel config: multi_v7_defconfig
+* Build URL: "$BUILD_URL":$BUILD_URL
+* OS flavour: $OS_FLAVOUR
+* FAI: "$GIT_URL":$GIT_URL
+* FAI commit: "$GIT_COMMIT":$GIT_URL/commit/?id=$GIT_COMMIT
 EOF
+
+sudo mount -t tmpfs tmpfs /tmp
+sudo cp tools/udevadm /sbin
 
 for rootfs in ${ROOTFS}; do
-  # Get rootfs
-  export ROOTFS_BUILD_NUMBER=`wget -q --no-check-certificate -O - https://ci.linaro.org/job/stretch-armhf-rootfs/label=docker-jessie-armhf,rootfs=${rootfs}/lastSuccessfulBuild/buildNumber`
-  export ROOTFS_BUILD_TIMESTAMP=`wget -q --no-check-certificate -O - https://ci.linaro.org/job/stretch-armhf-rootfs/label=docker-jessie-armhf,rootfs=${rootfs}/lastSuccessfulBuild/buildTimestamp?format=yyyyMMdd`
-  export ROOTFS_BUILD_URL="http://snapshots.linaro.org/debian/images/stretch/${rootfs}-armhf/${ROOTFS_BUILD_NUMBER}/linaro-stretch-${rootfs}-${ROOTFS_BUILD_TIMESTAMP}-${ROOTFS_BUILD_NUMBER}.tar.gz"
-  wget --progress=dot -e dotbytes=2M ${ROOTFS_BUILD_URL}
 
-  cat >> out/HEADER.textile << EOF
-* Rootfs (${rootfs}): "${rootfs}":http://snapshots.linaro.org/debian/images/stretch/${rootfs}-armhf/${ROOTFS_BUILD_NUMBER}
-EOF
+    rootfs_sz=$(echo $rootfs | cut -f2 -d,)
+    rootfs=$(echo $rootfs | cut -f1 -d,)
+    VERSION=$(cat build-version)
 
-  # Create pre-built image(s)
-  linaro-media-create --dev fastmodel --output-directory ${WORKSPACE}/out --image-file b2260-stretch_${rootfs}_${VERSION}.img --image-size 3G --binary linaro-stretch-${rootfs}-${ROOTFS_BUILD_TIMESTAMP}-${ROOTFS_BUILD_NUMBER}.tar.gz --hwpack hwpack_linaro-b2260_*.tar.gz --hwpack-force-yes --bootloader uefi
+    image_name=${VENDOR}-${OS_FLAVOUR}-${rootfs}-${PLATFORM_NAME}-${VERSION}
 
-  # Customize image(s)
-  mkdir -p boot rootfs
-  for device in $(sudo kpartx -avs out/b2260-stretch_${rootfs}_${VERSION}.img | cut -d' ' -f3); do
-    partition=$(echo ${device} | cut -d'p' -f3)
-    [ "${partition}" = "1" ] && sudo mount -o loop /dev/mapper/${device} boot
-    [ "${partition}" = "2" ] && sudo mount -o loop /dev/mapper/${device} rootfs
-  done
+    # make bootable sd card
+    sudo fai-diskimage -v --cspace $(pwd) \
+         --hostname linaro-${rootfs} \
+         -S ${rootfs_sz} \
+         --class $(echo SAVECACHE,${OS_FLAVOUR},DEBIAN,LINARO,${rootfs},${PLATFORM_NAME},UBOOT | tr '[:lower:]' '[:upper:]') \
+         /tmp/work.raw
 
-  sudo cp -a configs/96boards-b2260/boot/b2260 boot/ || true
-  sudo cp -a configs/96boards-b2260/boot/update_default_boot.sh boot/ || true
-  sudo cp -a out/uImage boot/ || true
+    sudo cp /var/log/fai/linaro-${rootfs}/last/fai.log fai-${rootfs}.log
+    if grep -E '^(ERROR:|WARNING: These unknown packages are removed from the installation list|Exit code task_)' fai-${rootfs}.log
+    then
+        rm -rf out/
+        echo "Errors during build"
+        exit 1
+    fi
 
-  sudo rm -rf rootfs/dev rootfs/boot rootfs/var/lib/apt/lists
-  sudo mkdir rootfs/dev rootfs/boot rootfs/var/lib/apt/lists
+    # snatch the rootfs and kernel/initrd for lava
+    for device in $(sudo kpartx -avs /tmp/work.raw | cut -d' ' -f3); do
+        partition=$(echo ${device} | cut -d'p' -f3)
+        sudo dd if=/dev/mapper/${device} of=/tmp/partition.raw bs=512
+        if [ "${partition}" = "2" ]; then
+            cp /tmp/partition.raw out/rootfs-${image_name}.img
+            sudo mount -o loop /tmp/partition.raw /mnt
+            kvers=$(ls /mnt/boot/vmlinuz-*|sed -e 's,.*vmlinuz-,,'|sort -rV|head -1)
+            cp /mnt/boot/vmlinuz-${kvers} out/vmlinuz
+            cp /mnt/boot/initrd.img-${kvers}  out/initrd
+            cp /mnt/usr/lib/linux-image-$kvers/stih410-b2260.dtb out/
+            sudo chroot /mnt dpkg -l > out/${image_name}.packages
+            sudo umount -f /mnt
+        fi
+        sudo rm -f /tmp/partition.raw
+    done
+    sudo kpartx -dv /tmp/work.raw
+    cp /tmp/work.raw out/${image_name}.sd
 
-  sudo umount boot rootfs
-  sudo kpartx -dv out/b2260-stretch_*.img
-
-  # Compress image(s)
-  gzip -9 out/b2260-stretch_${rootfs}_${VERSION}.img
+    # Compress image(s)
+    pigz -9 out/rootfs-${image_name}.img out/${image_name}.sd
 done
 
-# Create MD5SUMS file
-find out -type f | xargs md5sum > MD5SUMS.txt
-sed -i "s|out/||" MD5SUMS.txt
-mv MD5SUMS.txt out
