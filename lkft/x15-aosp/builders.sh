d21d24b12f68cca3cb94ad9297b52efd1797d53e
Vishal Bhoj <vishal.bhoj@linaro.org>
2018-05-29 18:18:27 +0530

    lkft-x15-aosp-4.14: add new job
    Change-Id: I04e851d91ca1b2df402d8234d305937558aa3ff7
    Signed-off-by: Vishal Bhoj <vishal.bhoj@linaro.org>

---
+++ lkft/x15-aosp/builders.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+
+export JAVA_TOOL_OPTIONS=-Dfile.encoding=UTF8
+export JENKINS_WORKSPACE=${WORKSPACE}
+
+java -version
+
+sudo apt-get -q=2 update
+sudo apt-get -q=2 install -y gcc-4.9-multilib bison git gperf libxml2-utils python-mako zip time python-requests genisoimage patch mtools python-pip pxz zlib1g-dev
+
+wget -q http://mirrors.kernel.org/ubuntu/pool/universe/o/openjdk-8/openjdk-8-jre-headless_8u45-b14-1_amd64.deb \
+  http://mirrors.kernel.org/ubuntu/pool/universe/o/openjdk-8/openjdk-8-jre_8u45-b14-1_amd64.deb \
+  http://mirrors.kernel.org/ubuntu/pool/universe/o/openjdk-8/openjdk-8-jdk_8u45-b14-1_amd64.deb
+sudo dpkg -i --force-all *.deb
+
+mkdir -p ${HOME}/bin ${WORKSPACE}/build/out
+curl https://storage.googleapis.com/git-repo-downloads/repo > ${HOME}/bin/repo
+chmod a+x ${HOME}/bin/*
+export PATH=${HOME}/bin:${PATH}
+
+repo init -u http://android.googlesource.com/platform/manifest -b master
+if [ ! -z ${REFERENCE_BUILD_URL} ]; then
+  cd .repo/manifest/
+  wget ${REFERENCE_BUILD_URL}/pinned-manifest.xml -O default.xml
+  cd -
+fi
+cd .repo/
+git clone git://android-git.linaro.org/platform/manifest.git -b x15-master local_manifests
+cd -
+
+set -e
+
+repo sync -j16 -c
+
+# build kernel
+export PATH=~/srv/toolchain/gcc-linaro-7.2.1-2017.11-x86_64_arm-eabi/bin:$PATH
+export CROSS_COMPILE=arm-eabi-
+export ARCH=arm
+cd -
+cd kernel/ti/x15/
+export KERNELDIR=${PWD}
+./ti_config_fragments/defconfig_builder.sh -t ti_sdk_dra7x_android_release
+make ti_sdk_dra7x_android_release_defconfig
+make -j$(nproc) zImage dtbs modules
+cd -
+
+./android-patchsets/x15-master-workarounds
+source build/envsetup.sh
+lunch am57xevm_full-userdebug
+make -j$(nproc)
+
+# Publish parameters
+cat << EOF > ${WORKSPACE}/publish_parameters
+PUB_DEST=lkft/x15-aosp-4.14/${BUILD_NUMBER}
+PUB_SRC=${PWD}/out/target/product/am57xevm/
+PUB_EXTRA_INC=^[^/]+zip
+EOF
+
---
+++ lkft/x15-aosp/submit_for_testing.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+export DEVICE_TYPE=x15
+export LAVA_SERVER=https://lkft.validation.linaro.org/RPC2/
+export PUB_DEST=android/lkft/${JOB_NAME}/${BUILD_NUMBER}
+export DOWNLOAD_URL=http://snapshots.linaro.org/${PUB_DEST}
+export KERNEL_COMMIT=${SRCREV_kernel}
+export ANDROID_VERSION=$(echo $REFERENCE_BUILD_URL | awk -F"/" '{print$(NF-1)}')
+export VTS_VERSION=$(echo $VTS_URL | awk -F"/" '{print$(NF-1)}')
+export CTS_VERSION=$(echo $CTS_URL | awk -F"/" '{print$(NF-1)}')
+[ -z "${TOOLCHAIN}" ] && export TOOLCHAIN="unknown"
+
+if [ ! -z "${KERNEL_DESCRIBE}" ]; then
+    export QA_BUILD_VERSION=${KERNEL_DESCRIBE}
+else
+    export QA_BUILD_VERSION=${KERNEL_COMMIT:0:12}
+fi
+
+rm -rf configs
+git clone --depth 1 http://git.linaro.org/ci/job/configs.git
+
+if curl --output /dev/null --silent --head --fail "${REFERENCE_BUILD_URL}/vendor.img.xz"; then
+    echo "This reference build comes with a vendor partition"
+else
+    echo "No vendor partition, so flashing cache partition from the job instead"
+    sed -i "s|vendor.img.xz|cache.img.xz|g" configs/lkft/lava-job-definitions/${DEVICE_TYPE}/*.yaml
+fi
+
+python configs/openembedded-lkft/submit_for_testing.py \
+    --device-type ${DEVICE_TYPE} \
+    --build-number ${BUILD_NUMBER} \
+    --lava-server ${LAVA_SERVER} \
+    --qa-server ${QA_SERVER} \
+    --qa-server-team android-lkft \
+    --qa-server-project ${QA_SERVER_PROJECT} \
+    --git-commit ${QA_BUILD_VERSION} \
+    --template-path configs/lkft/lava-job-definitions \
+    --template-names template-boot.yaml template-vts-kernel-syscalls.yaml template-cts-displaytestcases.yaml template-vts-kselftest.yaml template-cts.yaml template-vts-kernel-ltp.yaml
+    --quiet
+
+python configs/openembedded-lkft/submit_for_testing.py \
+    --device-type ${DEVICE_TYPE} \
+    --build-number ${BUILD_NUMBER} \
+    --lava-server ${LAVA_SERVER} \
+    --qa-server ${QA_SERVER} \
+    --qa-server-team staging-lkft \
+    --qa-server-project ${QA_SERVER_PROJECT}-vts-staging \
+    --git-commit ${QA_BUILD_VERSION} \
+    --template-path configs/lkft/lava-job-definitions \
+    --template-names template-vts-staging-kernel.yaml \
+    --quiet
