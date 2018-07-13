73415530bacd3aa02f067e98471b2a05429b8c9d
Aníbal Limón <anibal.limon@linaro.org>
2018-07-13 12:37:24 -0500

    lt-qcom-linux-integration/builders.sh: Improve sdm845 initramfs rootfs init
    - Execute rescue shell when fail to mount rootfs
    - Wait until device rootfs becaomes available
    - Umount dev, sys, proc before switch_root
    
    Change-Id: Ia3b10aa5ca5f8f87b632d00922ca5feb67c6a457
    Signed-off-by: Aníbal Limón <anibal.limon@linaro.org>

--- lt-qcom-linux-integration/builders.sh
+++ lt-qcom-linux-integration/builders.sh
@@ -18,18 +18,20 @@ do_mknod() {
 	test -e "\$1" || mknod "\$1" "\$2" "\$3" "\$4"
 }
 
+rescue_shell() {
+	echo "Failed to mount rootfs (__ROOTFS_PARTITION__), executing rescue shell..."
+	export PS1="linaro-test [rc=$(echo \$?)]# "
+	exec sh </dev/console >/dev/console 2>/dev/console
+}
+
 mkdir -p /proc
 mount -t proc proc /proc
 
 do_mount_fs sysfs /sys
-do_mount_fs debugfs /sys/kernel/debug
 do_mount_fs devtmpfs /dev
-do_mount_fs devpts /dev/pts
-do_mount_fs tmpfs /dev/shm
 
 mkdir -p /run
 mkdir -p /var/run
-
 /sbin/udevd --daemon
 /bin/udevadm trigger
 
@@ -37,8 +39,19 @@ do_mknod /dev/console c 5 1
 do_mknod /dev/null c 1 3
 do_mknod /dev/zero c 1 5
 
+if [ ! -b "__ROOTFS_PARTITION__" ]; then
+	echo "Waiting for root device __ROOTFS_PARTITION__ ..."
+	while [ ! -b "__ROOTFS_PARTITION__" ]; do
+		sleep 1s
+	done;
+fi
+
 mkdir -p /rootfs
-mount __ROOTFS_PARTITION__ /rootfs
+mount -o ro __ROOTFS_PARTITION__ /rootfs || rescue_shell
+
+umount /proc
+umount /sys
+umount /dev
 
 echo "All done. Switching to real root."
 exec switch_root /rootfs /sbin/init
