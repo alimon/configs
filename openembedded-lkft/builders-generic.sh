abc84a5362775f96e76331b32b1f1828bde1a3c9
Daniel Díaz <daniel.diaz@linaro.org>
2018-07-19 12:26:19 -0500

    openembedded-lkft: generic: sync with regular job
    Change image to build: rpb-console-image{,-lkft}.
    
    Sync other build procedures from builders.sh.
    
    Change-Id: I01b60d8b9e279a88009e0113d20c2f0bd3e7b7e6
    Signed-off-by: Daniel Díaz <daniel.diaz@linaro.org>

--- openembedded-lkft/builders-generic.sh
+++ openembedded-lkft/builders-generic.sh
@@ -20,15 +20,15 @@ if ! sudo DEBIAN_FRONTEND=noninteractive apt-get -q=2 update; then
   sleep 15
   sudo DEBIAN_FRONTEND=noninteractive apt-get -q=2 update || true
 fi
-pkg_list="python-pip android-tools-fsutils chrpath cpio diffstat gawk libmagickwand-dev libmath-prime-util-perl libsdl1.2-dev libssl-dev python-requests texinfo vim-tiny whiptail pxz"
+pkg_list="virtualenv python-pip android-tools-fsutils chrpath cpio diffstat gawk libmagickwand-dev libmath-prime-util-perl libsdl1.2-dev libssl-dev python-requests texinfo vim-tiny whiptail libelf-dev pxz"
 if ! sudo DEBIAN_FRONTEND=noninteractive apt-get -q=2 install -y ${pkg_list}; then
   echo "INFO: apt install error - try again in a moment"
   sleep 15
   sudo DEBIAN_FRONTEND=noninteractive apt-get -q=2 install -y ${pkg_list}
 fi
 
-# Install ruamel.yaml
-pip install --user --force-reinstall ruamel.yaml
+# Install jinja2-cli and ruamel.yaml
+pip install --user --force-reinstall jinja2-cli ruamel.yaml
 
 set -ex
 
@@ -39,7 +39,7 @@ export PATH=${HOME}/bin:${PATH}
 
 # initialize repo if not done already
 if [ ! -e ".repo/manifest.xml" ]; then
-   repo init -u ${MANIFEST_URL} -b ${MANIFEST_BRANCH_PREFIX}${MANIFEST_BRANCH}
+   repo init -u ${MANIFEST_URL} -b ${MANIFEST_BRANCH}
 
    # link to shared downloads on persistent disk
    # our builds config is expecting downloads and sstate-cache, here.
@@ -72,7 +72,6 @@ rm -rf conf build/conf build/tmp-*glibc/
 
 # Accept EULA if/when needed
 export EULA_dragonboard410c=1
-export EULA_stih410b2260=1
 source setup-environment build
 
 # Add job BUILD_NUMBER to output files names
@@ -102,7 +101,7 @@ IMAGE_FSTYPES_remove = "ext4 iso wic"
 EOF
 
 # Include additional recipes in the image
-[ "${MACHINE}" = "am57xx-evm" -o "${MACHINE}" = "stih410-b2260" ] || extra_pkgs="numactl"
+[ "${MACHINE}" = "am57xx-evm" -o "${MACHINE}" = "beaglebone" ] || extra_pkgs="numactl"
 cat << EOF >> conf/local.conf
 CORE_IMAGE_BASE_INSTALL_append = " kernel-selftests kselftests-mainline kselftests-next libhugetlbfs-tests ltp ${extra_pkgs}"
 CORE_IMAGE_BASE_INSTALL_append = " python python-misc python-modules python-numpy python-pexpect python-pyyaml"
@@ -131,14 +130,22 @@ custom_kernel_conf=$(find ../layers/meta-96boards/recipes-kernel -name custom-ke
 mv ${WORKSPACE}/custom-kernel-info.inc.tmp ${custom_kernel_conf}
 ###
 
+# Enable lkft-metadata class
+cat << EOF >> conf/local.conf
+INHERIT += "lkft-metadata"
+LKFTMETADATA_COMMIT = "1"
+EOF
+
+# FIXME: Remove when upstream has resolved what to do with wordsize.h recursion
+sed -i "s|bits/wordsize.h||" ../layers/openembedded-core/meta/recipes-core/glibc/glibc-package.inc
+
 # add useful debug info
 cat conf/{site,auto,local}.conf
 cat ${distro_conf}
 cat ${custom_kernel_conf}
 
-[ "${DISTRO}" = "rpb" ] && IMAGES+=" ${IMAGES_RPB}"
-[ "${DISTRO}" = "rpb-wayland" ] && IMAGES+=" ${IMAGES_RPB_WAYLAND}"
-[ "${MACHINE}" = "am57xx-evm" ] && IMAGES="rpb-console-image"
+# Temporary sstate cleanup to get lkft metadata generated
+bitbake -c cleansstate kselftests-mainline kselftests-next ltp libhugetlbfs
 
 time bitbake ${IMAGES}
 
@@ -153,13 +160,13 @@ cat ${DEPLOY_DIR_IMAGE}/pinned-manifest.xml
 # FIXME: IMAGE_FSTYPES_remove doesn't work
 rm -f ${DEPLOY_DIR_IMAGE}/*.rootfs.ext4 \
       ${DEPLOY_DIR_IMAGE}/*.rootfs.iso \
-      ${DEPLOY_DIR_IMAGE}/*.rootfs.wic \
+      ${DEPLOY_DIR_IMAGE}/*.rootfs.wic* \
       ${DEPLOY_DIR_IMAGE}/*.iso \
       ${DEPLOY_DIR_IMAGE}/*.stimg
 
 # FIXME: Sparse and converted images here, until it gets done by OE
 case "${MACHINE}" in
-  juno|stih410-b2260|orangepi-i96)
+  juno)
     ;;
   intel-core2-32)
     for rootfs in ${DEPLOY_DIR_IMAGE}/*.hddimg; do
@@ -189,7 +196,7 @@ h4. LKFT Generic Linux - OpenEmbedded
 Build description:
 * Build URL: "$BUILD_URL":$BUILD_URL
 * Manifest URL: "${MANIFEST_URL}":${MANIFEST_URL}
-* Manifest branch: ${MANIFEST_BRANCH_PREFIX}${MANIFEST_BRANCH}
+* Manifest branch: ${MANIFEST_BRANCH}
 * Manifest commit: "${MANIFEST_COMMIT}":${MANIFEST_URL/.git/\/commit}/${MANIFEST_COMMIT}
 EOF
 
@@ -205,24 +212,30 @@ EOF
   mv /srv/oe/manifest-changes.txt ${DEPLOY_DIR_IMAGE}
 fi
 
-# Need different files for each machine
+GCCVERSION=$(bitbake -e | grep "^GCCVERSION="| cut -d'=' -f2 | tr -d '"')
+TARGET_SYS=$(bitbake -e | grep "^TARGET_SYS="| cut -d'=' -f2 | tr -d '"')
+TUNE_FEATURES=$(bitbake -e | grep "^TUNE_FEATURES="| cut -d'=' -f2 | tr -d '"')
+STAGING_KERNEL_DIR=$(bitbake -e | grep "^STAGING_KERNEL_DIR="| cut -d'=' -f2 | tr -d '"')
+
+# lkft-metadata class generates metadata file, which can be sourced
+for recipe in kselftests-mainline kselftests-next ltp libhugetlbfs; do
+  source lkftmetadata/packages/*/${recipe}/metadata
+done
+
 BOOT_IMG=$(find ${DEPLOY_DIR_IMAGE} -type f -name "boot-*-${MACHINE}-*-${BUILD_NUMBER}*.img" | sort | xargs -r basename)
-ROOTFS_EXT4_IMG=$(find ${DEPLOY_DIR_IMAGE} -type f -name "rpb-console-image-${MACHINE}-*-${BUILD_NUMBER}.rootfs.ext4.gz" | xargs -r basename)
-ROOTFS_TARXZ_IMG=$(find ${DEPLOY_DIR_IMAGE} -type f -name "rpb-console-image-${MACHINE}-*-${BUILD_NUMBER}.rootfs.tar.xz" | xargs -r basename)
-ROOTFS_IMG=$(find ${DEPLOY_DIR_IMAGE} -type f -name "rpb-console-image-${MACHINE}-*-${BUILD_NUMBER}.rootfs.img.gz" | xargs -r basename)
-ROOTFS_DESKTOP_IMG=$(find ${DEPLOY_DIR_IMAGE} -type f -name "rpb-desktop-image-${MACHINE}-*-${BUILD_NUMBER}.rootfs.img.gz" | xargs -r basename)
-KERNEL_IMG=$(find ${DEPLOY_DIR_IMAGE} -type f -name "uImage-*-${MACHINE}-*-${BUILD_NUMBER}.bin" | xargs -r basename)
+KERNEL_IMG=$(find ${DEPLOY_DIR_IMAGE} -type f -name "*Image-*-${MACHINE}-*-${BUILD_NUMBER}.bin" | xargs -r basename)
+ROOTFS_IMG=$(find ${DEPLOY_DIR_IMAGE} -type f -name "rpb-console-image-lkft-${MACHINE}-*-${BUILD_NUMBER}.rootfs.img.gz" | xargs -r basename)
+ROOTFS_EXT4=$(find ${DEPLOY_DIR_IMAGE} -type f -name "rpb-console-image-lkft-${MACHINE}-*-${BUILD_NUMBER}.rootfs.ext4.gz" | xargs -r basename)
+ROOTFS_TARXZ_IMG=$(find ${DEPLOY_DIR_IMAGE} -type f -name "rpb-console-image-lkft-${MACHINE}-*-${BUILD_NUMBER}.rootfs.tar.xz" | xargs -r basename)
+HDD_IMG=$(find ${DEPLOY_DIR_IMAGE} -type f -name "rpb-console-image-lkft-${MACHINE}-*-${BUILD_NUMBER}.hddimg.xz" | xargs -r basename)
 case "${MACHINE}" in
   am57xx-evm)
-    # LAVA image is too big for am57xx-evm
-    ROOTFS_IMG=$(find ${DEPLOY_DIR_IMAGE} -type f -name "rpb-console-image-${MACHINE}-*-${BUILD_NUMBER}.rootfs.img.gz" | xargs -r basename)
-    # FIXME: several dtb files case
+    # QEMU arm 32bit needs the zImage file, not the uImage file.
+    # KERNEL_IMG is not used for the real hardware itself.
+    KERNEL_IMG=$(find ${DEPLOY_DIR_IMAGE} -type f -name "zImage-*-${MACHINE}-*-${BUILD_NUMBER}.bin" | xargs -r basename)
     ;;
   juno)
-    # FIXME: several dtb files case
-    ;;
-  *)
-    DTB_IMG=$(find ${DEPLOY_DIR_IMAGE} -type f -name "*-${MACHINE}-*-${BUILD_NUMBER}.dtb" | xargs -r basename)
+    DTB_IMG=$(find ${DEPLOY_DIR_IMAGE} -type f -name "*Image-*-${MACHINE}-r2-*-${BUILD_NUMBER}.dtb" | xargs -r basename)
     ;;
 esac
 
@@ -234,6 +247,18 @@ cat > ${DEPLOY_DIR_IMAGE}/build_config.json <<EOF
   "kernel_branch" : "${KERNEL_BRANCH}",
   "kernel_describe" : "${KERNEL_DESCRIBE}",
   "build_location" : "${BASE_URL}/${PUB_DEST}"
+  "kselftest_mainline_url" : "${KSELFTESTS_MAINLINE_URL}",
+  "kselftest_mainline_version" : "${KSELFTESTS_MAINLINE_VERSION}",
+  "kselftest_next_url" : "${KSELFTESTS_NEXT_URL}",
+  "kselftest_next_version" : "${KSELFTESTS_NEXT_VERSION}",
+  "ltp_url" : "${LTP_URL}",
+  "ltp_version" : "${LTP_VERSION}",
+  "ltp_revision" : "${LTP_REVISION}",
+  "libhugetlbfs_url" : "${LIBHUGETLBFS_URL}",
+  "libhugetlbfs_version" : "${LIBHUGETLBFS_VERSION}",
+  "libhugetlbfs_revision" : "${LIBHUGETLBFS_REVISION}",
+  "build_arch" : "${TUNE_FEATURES}",
+  "compiler" : "${TARGET_SYS} ${GCCVERSION}",
 }
 EOF
 
@@ -241,23 +266,28 @@ EOF
 #       BASE_URL and PUB_DEST, typically used for OE RPB builds
 cat << EOF > ${WORKSPACE}/post_build_lava_parameters
 DEPLOY_DIR_IMAGE=${DEPLOY_DIR_IMAGE}
-MANIFEST_COMMIT=${MANIFEST_COMMIT}
 BASE_URL=${BASE_URL}
 BOOT_URL=${BASE_URL}/${PUB_DEST}/${BOOT_IMG}
-ROOTFS_BUILD_URL=${BASE_URL}/${PUB_DEST}/${ROOTFS_EXT4_IMG}
-ROOTFS_SPARSE_BUILD_URL=${BASE_URL}/${PUB_DEST}/${ROOTFS_IMG}
-ROOTFS_DESKTOP_SPARSE_BUILD_URL=${BASE_URL}/${PUB_DEST}/${ROOTFS_DESKTOP_IMG}
 SYSTEM_URL=${BASE_URL}/${PUB_DEST}/${ROOTFS_IMG}
 KERNEL_URL=${BASE_URL}/${PUB_DEST}/${KERNEL_IMG}
 DTB_URL=${BASE_URL}/${PUB_DEST}/${DTB_IMG}
-NFSROOTFS_URL=${BASE_URL}/${PUB_DEST}/${ROOTFS_TARXZ_IMG}
 RECOVERY_IMAGE_URL=${BASE_URL}/${PUB_DEST}/juno-oe-uboot.zip
+NFSROOTFS_URL=${BASE_URL}/${PUB_DEST}/${ROOTFS_TARXZ_IMG}
 EXT4_IMAGE_URL=${BASE_URL}/${PUB_DEST}/${ROOTFS_EXT4}
 HDD_URL=${BASE_URL}/${PUB_DEST}/${HDD_IMG}
-LXC_BOOT_IMG=${BOOT_IMG}
-LXC_ROOTFS_IMG=$(basename ${ROOTFS_IMG} .gz)
 KERNEL_COMMIT=${KERNEL_COMMIT}
 KERNEL_CONFIG_URL=${BASE_URL}/${PUB_DEST}/config
 KERNEL_DEFCONFIG_URL=${BASE_URL}/${PUB_DEST}/defconfig
+KSELFTESTS_MAINLINE_URL=${KSELFTESTS_MAINLINE_URL}
+KSELFTESTS_MAINLINE_VERSION=${KSELFTESTS_MAINLINE_VERSION}
+KSELFTESTS_NEXT_URL=${KSELFTESTS_NEXT_URL}
+KSELFTESTS_NEXT_VERSION=${KSELFTESTS_NEXT_VERSION}
+LTP_URL=${LTP_URL}
+LTP_VERSION=${LTP_VERSION}
+LTP_REVISION=${LTP_REVISION}
+LIBHUGETLBFS_URL=${LIBHUGETLBFS_URL}
+LIBHUGETLBFS_VERSION=${LIBHUGETLBFS_VERSION}
+LIBHUGETLBFS_REVISION=${LIBHUGETLBFS_REVISION}
 MAKE_KERNELVERSION=${MAKE_KERNELVERSION}
+TOOLCHAIN="${TARGET_SYS} ${GCCVERSION}"
 EOF
