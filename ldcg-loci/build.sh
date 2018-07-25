e98302ae29bb3351d491e2f44b917e0562b133d5
Marcin Juszkiewicz <marcin.juszkiewicz@linaro.org>
2018-07-25 13:49:11 +0200

    ldcg-loci: handle Docker authorization stuff
    Change-Id: Ib0a8b950bb645ebeb18f4e0ced2ffc90e7b3925c

--- ldcg-loci/build.sh
+++ ldcg-loci/build.sh
@@ -1,5 +1,22 @@
 #!/bin/bash
 
+set -ex
+
+trap cleanup_exit INT TERM EXIT
+
+cleanup_exit()
+{
+    rm -rf ${HOME}/.docker
+}
+
+mkdir -p ${HOME}/.docker
+sed -e "s|\${DOCKER_AUTH}|${DOCKER_AUTH}|" < ${WORKSPACE}/config.json > ${HOME}/.docker/config.json
+chmod 0600 ${HOME}/.docker/config.json
+
+rm -rf ${WORKSPACE}/*
+
+# ---------------------------------------------------------------------------------------------------------
+
 HUB_USERNAME=linaro
 IMG_TAG=build-${BUILD_NUMBER}
 BASE_IMG=${HUB_USERNAME}/loci-base:${IMG_TAG}
