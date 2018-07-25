a7f3ddabbfffa79c1eb460132bcee6d9762c9456
Marcin Juszkiewicz <marcin.juszkiewicz@linaro.org>
2018-07-25 13:17:50 +0200

    ldcg-loci: when you build stuff from git clone it first
    Also apply required not-yet-merged changes to get it buildable on
    AArch64.
    
    Change-Id: I3b59e8cc8c897ee22cdf5194795c162d647d05c2

--- ldcg-loci/build.sh
+++ ldcg-loci/build.sh
@@ -16,6 +16,16 @@ done
 docker build . --tag ${BASE_IMG}
 docker push ${BASE_IMG}
 
+git clone --depth 1 https://git.openstack.org/openstack/loci
+
+cd loci/
+
+# 583726 # bindep: install blas and lapack headers to build scipy on aarch64
+# 583727 # requirements.sh: install numpy and scipy on AArch64
+
+git pull https://git.openstack.org/openstack/loci refs/changes/26/583726/3
+git pull https://git.openstack.org/openstack/loci refs/changes/27/583727/5
+
 # requirements needs to be built first
 for project in requirements cinder glance heat horizon ironic keystone neutron nova octavia
 do 
