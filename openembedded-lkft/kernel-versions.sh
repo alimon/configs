e49875bf3aa8461fba7eddd1879e6ca6a95f170d
Daniel Díaz <daniel.diaz@linaro.org>
2018-07-31 13:25:49 -0500

    openembedded-lkft-linux-developer: Clone kernel and determine versions
    Change-Id: Ie4e2b845256b75d12a1971d8e298697bf752d96d
    Signed-off-by: Daniel Díaz <daniel.diaz@linaro.org>

---
+++ openembedded-lkft/kernel-versions.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+# Variables needed here:
+#  KERNEL_REPO         A clonable Git repository.
+#  KERNEL_BRANCH       The branch to checkout.
+#  KERNEL_COMMIT       (Optional) A commit to go to. This is
+#                      assumed to be part of the KERNE_BRANCH.
+#
+# This will create a file named ${WORKSPACE}/linux_versions
+# that can then be injected into the environment. The contents
+# will be:
+#  KERNEL_DESCRIBE     The output of `git describe --always`.
+#  KERNEL_SRCREV       The actual commit id that is the result
+#                      of the checkout or reset.
+#  MAKE_KERNELVERSION  The output of running `make
+#                      kernelversion` in the Linux tree.
+
+set -xe
+
+linux_reference=""
+if [ -d "${HOME}/srv/linux.git" ]; then
+  linux_reference="--reference ${HOME}/srv/linux.git"
+fi
+
+git clone "${linux_reference}" -o origin "${KERNEL_REPO}" "${WORKSPACE}/linux"
+cd "${WORKSPACE}/linux"
+git checkout "origin/${KERNEL_BRANCH}"
+if [ "${KERNEL_COMMIT}" ]; then
+  git reset --hard "${KERNEL_COMMIT}"
+fi
+
+GIT_COMMIT="$(git rev-parse HEAD)"
+
+echo "KERNEL_DESCRIBE=$(git describe --always)" >> "${WORKSPACE}/linux_versions"
+echo "KERNEL_SRCREV=${GIT_COMMIT}" >> "${WORKSPACE}/linux_versions"
+echo "MAKE_KERNELVERSION=$(make kernelversion)" >> "${WORKSPACE}/linux_versions"
+cat "${WORKSPACE}/linux_versions"
