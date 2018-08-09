71fc0219dbd7dee98e577681a041f42f97d29335
Daniel Díaz <daniel.diaz@linaro.org>
2018-08-09 11:51:27 -0500

    openembedded-lkft: groovy: read optional job name
    When supported by submit_for_testing.py, the job name will be
    printed along with the URL of the test job. The pattern
    matcher needeed a slight change to correctly detect that line.
    
    If a job name is printed, it will be added to the link to the
    test job. Exempli gratia:
    
    Whereas before links (within <a/> tags) looked like this:
      QA Reports: 266596
    now they will look like this:
      QA Reports: 266596 lkft-ltp-syscalls-master-40
    
    If no job name is printed, nothing changes in the link
    compared to the existing situation.
    
    Change-Id: Iee4b0393f1fba5d5043f299503df85e43524203d
    Signed-off-by: Daniel Díaz <daniel.diaz@linaro.org>

--- openembedded-lkft/postbuild.groovy
+++ openembedded-lkft/postbuild.groovy
@@ -4,13 +4,14 @@ if (manager.build.result == hudson.model.Result.SUCCESS) {
   if (desc == null) {
     desc = ""
   }
-  pattern = ~"${qa_server}/testjob/(\\d+)"
+  pattern = ~"(${qa_server}/testjob/(\\d+))(.*)"
   manager.build.logFile.eachLine { line ->
     matcher = pattern.matcher(line)
     if(matcher.matches()) {
-      def url = matcher.group(0)
-      def testjob_id = matcher.group(1)
-      desc += "&nbsp;<a href='${url}'>QA Reports: ${testjob_id}</a><br/>"
+      def url = matcher.group(1)
+      def testjob_id = matcher.group(2)
+      def job_name = matcher.group(3)
+      desc += "&nbsp;<a href='${url}'>QA Reports: ${testjob_id}${job_name}</a><br/>"
     }
   }
   manager.build.setDescription(desc)
