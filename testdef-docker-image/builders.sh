5608dd0785a0c2175052f6240e4a8b58a4027894
Chase Qi <chase.qi@linaro.org>
2018-08-21 14:55:49 +0100

    testdef-docker-image: setup new jenkins job
    Change-Id: Icb1628cef08537f6cc3083075590383ceba8098c
    Signed-off-by: Chase Qi <chase.qi@linaro.org>

---
+++ testdef-docker-image/builders.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+set -ex
+
+trap cleanup_exit INT TERM EXIT
+
+cleanup_exit()
+{
+    rm -rf "${HOME}/.docker"
+    rm -rf "${WORKSPACE}/dockerfiles"
+}
+
+mkdir -p "${HOME}/.docker"
+sed -e "s|\${DOCKER_AUTH}|${DOCKER_AUTH}|" < "${WORKSPACE}/config.json" > "${HOME}/.docker/config.json"
+chmod 0600 "${HOME}/.docker/config.json"
+
+testdef_tag="$(git describe --tags --abbrev=0)"
+
+rm -rf "${WORKSPACE}/dockerfiles"
+git clone https://git.linaro.org/ci/dockerfiles.git
+
+build_img() {
+    docker_img="$1"
+    cd "${WORKSPACE}/dockerfiles/${docker_img}"
+    ./build.sh "${testdef_tag}"
+
+    # Push to linaro/testdef-*
+    docker push "$(cat .docker-tag)"
+}
+
+build_img "stretch-arm64-testdef"
+build_img "stretch-armhf-testdef"
