cb2fa35ef1db50d3bd0df30a6266d9d4dfa42937
Daniel Díaz <daniel.diaz@linaro.org>
2018-09-04 10:27:33 -0500

    openembedded-lkft-linux-developer: Add QA Reports link
    A link to QA Reports in the build can help find where the
    results will end up without too much clicking.
    
    Change-Id: I3cfcefc33b9fc4e986af7089a236364809b526ac
    Signed-off-by: Daniel Díaz <daniel.diaz@linaro.org>

---
+++ openembedded-lkft/qareports.groovy
@@ -0,0 +1,20 @@
+if (manager.build.result == hudson.model.Result.SUCCESS) {
+  qa_server = manager.envVars["QA_SERVER"]
+  qa_server_team = manager.envVars["QA_SERVER_TEAM"]
+  if (qa_server_team == null) {
+    qa_server_team = "lkft"
+  }
+  qa_server_project = manager.envVars["QA_SERVER_PROJECT"]
+  kernel_describe = manager.envVars["KERNEL_DESCRIBE"]
+  test_suites = manager.envVars["TEST_SUITES"]
+
+  if (test_suites != "")
+  {
+    def qa_desc = manager.build.getDescription()
+    if (qa_desc == null) {
+      qa_desc = ""
+    }
+    qa_desc += "&nbsp;<a href='${qa_server}/${qa_server_team}/${qa_server_project}/build/${kernel_describe}'>QA Reports</a><br/>"
+    manager.build.setDescription(qa_desc)
+  }
+}
