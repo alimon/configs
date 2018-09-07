0f75c41c468e564c490d448045dffb26c661cc80
Daniel Díaz <daniel.diaz@linaro.org>
2018-09-07 13:09:36 -0500

    openembedded-lkft-linux-developer: Add sufix to QA Reports link
    As of ff50e61e, the build number is suffixed to the QA Reports
    build when submitting the tests, so the URL has to be fixed in
    the build description to point to the right place.
    
    Change-Id: I6911de8d1af2a9ef3f856a936271b4c08533b4af
    Signed-off-by: Daniel Díaz <daniel.diaz@linaro.org>

--- openembedded-lkft/qareports.groovy
+++ openembedded-lkft/qareports.groovy
@@ -6,6 +6,8 @@ if (manager.build.result == hudson.model.Result.SUCCESS) {
   }
   qa_server_project = manager.envVars["QA_SERVER_PROJECT"]
   kernel_describe = manager.envVars["KERNEL_DESCRIBE"]
+  build_number = manager.envVars["BUILD_NUMBER"]
+  qa_build = kernel_describe + "-b" + build_number
   test_suites = manager.envVars["TEST_SUITES"]
 
   if (test_suites != "")
@@ -14,7 +16,7 @@ if (manager.build.result == hudson.model.Result.SUCCESS) {
     if (qa_desc == null) {
       qa_desc = ""
     }
-    qa_desc += "&nbsp;<a href='${qa_server}/${qa_server_team}/${qa_server_project}/build/${kernel_describe}'>QA Reports</a><br/>"
+    qa_desc += "&nbsp;<a href='${qa_server}/${qa_server_team}/${qa_server_project}/build/${qa_build}'>QA Reports</a><br/>"
     manager.build.setDescription(qa_desc)
   }
 }
