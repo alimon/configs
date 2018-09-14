a42ed46ae58f198bbefbc3e2b95c7a2e6bb23faf
Todor Tomov <todor.tomov@linaro.org>
2018-09-14 16:16:04 +0300

    lt-qcom-linux-aosp: Update location of ramdisk for DB410c bootimage
    Use the ramdisk image generated by job
    "96boards - AOSP Master - dragonboard410c".
    
    Change-Id: Ifd08b88e6d0a639cbec141ad6ff69781f9af9609
    Signed-off-by: Todor Tomov <todor.tomov@linaro.org>

--- lt-qcom-linux-aosp/package-aosp-bootimage.sh
+++ lt-qcom-linux-aosp/package-aosp-bootimage.sh
@@ -2,5 +2,5 @@
 
 cat ${WORKSPACE}/linux/arch/${ARCH}/boot/Image.gz ${WORKSPACE}/linux/arch/${ARCH}/boot/dts/qcom/apq8016-sbc.dtb > Image.gz-dtb
 wget -q https://android-git.linaro.org/platform/system/core.git/plain/mkbootimg/mkbootimg -O mkbootimg
-wget -q ${REFERENCE_BUILD_URL}/ramdisk.img -O ramdisk.img
+wget -q ${AOSP_BUILD_DB410C}/ramdisk.img -O ramdisk.img
 python mkbootimg --kernel Image.gz-dtb --ramdisk ramdisk.img --output boot-db410c.img --pagesize 2048 --base 0x80000000 --cmdline 'androidboot.selinux=permissive firmware_class.path=/system/vendor/firmware/'
