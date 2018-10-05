6ca451058189d1f3260d07fd1c2414a1305eb2bc
Todor Tomov <todor.tomov@linaro.org>
2018-10-05 16:27:28 +0300

    lt-qcom-linux-aosp: Publish build files to both db410c and db820c directories
    Publish build files to both db410c and db820c directories.
    For each publish only the relevent dtb file.
    
    Change-Id: I990b97a8db2641b8d91218f14f91ee20e228d05c
    Signed-off-by: Todor Tomov <todor.tomov@linaro.org>

---
+++ lt-qcom-linux-aosp/publisher_vars_db410c.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+echo "KERNEL_DTBS=qcom/apq8016-sbc.dtb" >publisher_vars_db410c
+echo "PUB_DEST=96boards/dragonboard410c/linaro/aosp/kernel/${BUILD_NUMBER}" >>publisher_vars_db410c
---
+++ lt-qcom-linux-aosp/publisher_vars_db820c.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+echo "KERNEL_DTBS=qcom/apq8096-db820c.dtb" >publisher_vars_db820c
+echo "PUB_DEST=96boards/dragonboard820c/linaro/aosp/kernel/${BUILD_NUMBER}" >>publisher_vars_db820c
