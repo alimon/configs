5ccbc2858b8c8e536dfbdb9bce939666e6881870
Paul Sokolovsky <paul.sokolovsky@linaro.org>
2018-11-08 12:27:15 +0300

    lite-aeolus: Update to Zephyr SDK 0.9.5.
    Change-Id: I280d1d64c6be22de4671da1f09df65f8fa68039e

--- lite-aeolus/build-init.sh
+++ lite-aeolus/build-init.sh
@@ -17,15 +17,15 @@ echo "GIT_COMMIT=$(git rev-parse --short=8 HEAD)" > env_var_parameters
 
 # Toolchains are pre-installed and come from:
 # https://armkeil.blob.core.windows.net/developer/Files/downloads/gnu-rm/7-2018q2/gcc-arm-none-eabi-7-2018-q2-update-linux.tar.bz2
-# https://github.com/zephyrproject-rtos/meta-zephyr-sdk/releases/download/0.9.3/zephyr-sdk-0.9.3-setup.run
-# To install Zephyr SDK: ./zephyr-sdk-0.9.3-setup.run --quiet --nox11 -- <<< "${HOME}/srv/toolchain/zephyr-sdk-0.9.3"
+# https://github.com/zephyrproject-rtos/meta-zephyr-sdk/releases/download/0.9.5/zephyr-sdk-0.9.5-setup.run
+# To install Zephyr SDK: ./zephyr-sdk-0.9.5-setup.run --quiet --nox11 -- <<< "${HOME}/srv/toolchain/zephyr-sdk-0.9.5"
 
 case "${ZEPHYR_GCC_VARIANT}" in
   gccarmemb)
     export GCCARMEMB_TOOLCHAIN_PATH="${HOME}/srv/toolchain/gcc-arm-none-eabi-7-2018-q2-update"
   ;;
   zephyr)
-    export ZEPHYR_SDK_INSTALL_DIR="${HOME}/srv/toolchain/zephyr-sdk-0.9.3"
+    export ZEPHYR_SDK_INSTALL_DIR="${HOME}/srv/toolchain/zephyr-sdk-0.9.5"
   ;;
 esac
 
