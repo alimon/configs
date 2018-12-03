bee021af3e478a681abbbec2ed4012bec5276bfd
Vishal Bhoj <vishal.bhoj@linaro.org>
2018-12-03 08:39:59 +0530

    rpb-openembedded: set GERRIT_URL for premerge ci
    Change-Id: Ib2f27ce4d1ced1b8687cc45fe146b63a5a3937c2
    Signed-off-by: Vishal Bhoj <vishal.bhoj@linaro.org>

--- rpb-openembedded/builders.sh
+++ rpb-openembedded/builders.sh
@@ -70,6 +70,7 @@ else
 fi
 
 if [ -n "$GERRIT_PROJECT" ] && [ $GERRIT_EVENT_TYPE == "patchset-created" ]; then
+    GERRIT_URL="http://${GERRIT_HOST}/${GERRIT_PROJECT}"
     cd `grep -rni $GERRIT_PROJECT\" .repo/manifest.xml | grep -Po 'path="\K[^"]*'`
     if git pull ${GERRIT_URL} ${GERRIT_REFSPEC} | grep -q "Automatic merge failed"; then
         git reset --hard
