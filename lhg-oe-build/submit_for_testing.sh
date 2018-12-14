87fce5c96f98db92944218afcb06faebdf79a38b
Arthur She <arthur.she@linaro.org>
2018-12-13 23:13:59 -0800

    lhg-oe-build: Add v4l2-compliance test to dragonboard-410c
    Add v4l2-compliance test to dragonboard-410c
    
    Change-Id: I34077abd139485f25b980b4e15ed7cf52be3deb4

--- lhg-oe-build/submit_for_testing.sh
+++ lhg-oe-build/submit_for_testing.sh
@@ -5,6 +5,9 @@ set -ex
 rm -rf configs
 git clone --depth 1 http://git.linaro.org/ci/job/configs.git
 
+TEST_JOBS="template-tests-${MANIFEST_BRANCH}.yaml template-wifi-tests.yaml"
+[ "${DEVICE_TYPE}" == "dragonboard-410c" ] && TEST_JOBS="${TEST_JOBS} template-v4l2-compliance-test.yaml"
+
 [ -z "${DEVICE_TYPE}" ] || \
 python configs/openembedded-lkft/submit_for_testing.py \
   --device-type ${DEVICE_TYPE} \
@@ -15,4 +18,4 @@ python configs/openembedded-lkft/submit_for_testing.py \
   --qa-server-project ${QA_SERVER_PROJECT} \
   --git-commit ${BUILD_NUMBER} \
   --template-path configs/lhg-oe-build/lava-job-definitions \
-  --template-names template-tests-${MANIFEST_BRANCH}.yaml template-wifi-tests.yaml
+  --template-names ${TEST_JOBS}
