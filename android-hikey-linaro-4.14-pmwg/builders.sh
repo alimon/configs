74cc5c28e6fadb49a0c26a453038f71d6c8a4b17
Lisa Nguyen <lisa.nguyen@linaro.org>
2019-01-07 12:58:37 -0800

    android-hikey-linaro-4.14-pmwg: Add files to setup 4.14 CI loops
    Add new files needed to setup the PMWG Android 4.14 CI loops
    on hikey and hikey960.
    
    Change-Id: I0a855e574b97858ff97c0e529fc9e9fd7ed0fd47
    Signed-off-by: Lisa Nguyen <lisa.nguyen@linaro.org>

---
+++ android-hikey-linaro-4.14-pmwg/builders.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+set -ex
+
+git clone --depth=1 https://android.googlesource.com/platform/prebuilts/gcc/linux-x86/aarch64/aarch64-linux-android-4.9
+export PATH=${PATH}:${PWD}/aarch64-linux-android-4.9/bin/
+
+if ! sudo DEBIAN_FRONTEND=noninteractive apt-get -q=2 update; then
+  echo "INFO: apt update error - try again in a moment"
+  sleep 15
+  sudo DEBIAN_FRONTEND=noninteractive apt-get -q=2 update || true
+fi
+pkg_list="python-pip openssl libssl-dev"
+if ! sudo DEBIAN_FRONTEND=noninteractive apt-get -q=2 install -y ${pkg_list}; then
+  echo "INFO: apt install error - try again in a moment"
+  sleep 15
+  sudo DEBIAN_FRONTEND=noninteractive apt-get -q=2 install -y ${pkg_list}
+fi
+
+make ARCH=arm64 ${DEFCONFIG}
+make ARCH=arm64 CROSS_COMPILE=aarch64-linux-android- -j$(nproc) -s Image-dtb
+
+wget -q https://android-git.linaro.org/platform/system/core.git/plain/mkbootimg/mkbootimg.py -O mkbootimg
+wget -q ${REFERENCE_BUILD_URL}/ramdisk.img -O ramdisk.img
+
+mkdir -p out
+case "${DEFCONFIG}" in
+  hikey_defconfig)
+    python mkbootimg \
+      --kernel ${PWD}/arch/arm64/boot/Image-dtb \
+      --cmdline "androidboot.console=ttyFIQ0 androidboot.hardware=hikey firmware_class.path=/vendor/firmware efi=noruntime printk.devkmsg=on buildvariant=userdebug overlay_mgr.overlay_dt_entry=hardware_cfg_enable_android_fstab" \
+      --os_version O \
+      --os_patch_level 2016-11-05 \
+      --ramdisk ramdisk.img \
+      --output out/boot.img
+    ;;
+  hikey960_defconfig)
+    python mkbootimg \
+      --kernel ${PWD}/arch/arm64/boot/Image-dtb \
+      --cmdline "androidboot.hardware=hikey960 console=ttyFIQ0 androidboot.console=ttyFIQ0 firmware_class.path=/vendor/firmware loglevel=15 buildvariant=userdebug overlay_mgr.overlay_dt_entry=hardware_cfg_enable_android_fstab" \
+      --base 0x0 --tags_offset 0x07a00000 --kernel_offset 0x00080000 \
+      --ramdisk_offset 0x07c00000 \
+      --os_version P \
+      --os_patch_level 2016-11-05 \
+      --ramdisk ramdisk.img \
+      --output out/boot.img
+    ;;
+esac
+xz out/boot.img
---
+++ android-hikey-linaro-4.14-pmwg/submit_for_testing.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+set -ex
+
+rm -rf configs
+git clone --depth 1 http://git.linaro.org/ci/job/configs.git
+
+# Install jinja2-cli and ruamel.yaml
+if ! sudo DEBIAN_FRONTEND=noninteractive apt-get -q=2 update; then
+  echo "INFO: apt update error - try again in a moment"
+  sleep 15
+  sudo DEBIAN_FRONTEND=noninteractive apt-get -q=2 update || true
+fi
+pkg_list="virtualenv python-pip"
+if ! sudo DEBIAN_FRONTEND=noninteractive apt-get -q=2 install -y ${pkg_list}; then
+  echo "INFO: apt install error - try again in a moment"
+  sleep 15
+  sudo DEBIAN_FRONTEND=noninteractive apt-get -q=2 install -y ${pkg_list}
+fi
+pip install --user --force-reinstall jinja2-cli ruamel.yaml
+
+[ -z "${DEVICE_TYPE}" ] || \
+python configs/openembedded-lkft/submit_for_testing.py \
+  --device-type ${DEVICE_TYPE} \
+  --build-number ${BUILD_NUMBER} \
+  --lava-server ${LAVA_SERVER} \
+  --qa-server ${QA_SERVER} \
+  --qa-server-team ${QA_SERVER_TEAM} \
+  --qa-server-project ${QA_SERVER_PROJECT} \
+  --git-commit ${GIT_COMMIT:0:12} \
+  --template-path configs/android-hikey-linaro-4.14-pmwg/lava-job-definitions \
+  --template-names vellamo-template.yaml multimedia-template.yaml
