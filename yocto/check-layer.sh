alimon/configs

commit 1f35ea39a636f47035a4d3ffc09b74e178ebb66b

yocto-check-layer: improve build script
* remove -x as it is too noisy
* record commit ID that was checked out for Poky and the Layer to test
* fixup the pkg list , based on YP documentation

Change-Id: I163c742be93e94eeacca6cfdef6c830302c1fd66
Signed-off-by: Nicolas Dechesne <nicolas.dechesne@linaro.org>

yocto/check-layer.sh
@@ -7,7 +7,8 @@ if ! sudo DEBIAN_FRONTEND=noninteractive apt-get -q=2 update; then
   sleep 15
   sudo DEBIAN_FRONTEND=noninteractive apt-get -q=2 update || true
 fi
-pkg_list="python-pip chrpath cpio diffstat gawk libmagickwand-dev libmath-prime-util-perl libsdl1.2-dev libssl-dev python-requests texinfo vim-tiny whiptail"
+pkg_list="gawk diffstat unzip texinfo gcc-multilib build-essential chrpath socat cpio python python3 python3-pip python3-pexpect xz-utils debianutils iputils-ping"
+
 if ! sudo DEBIAN_FRONTEND=noninteractive apt-get -q=2 install -y ${pkg_list}; then
   echo "INFO: apt install error - try again in a moment"
   sleep 15
@@ -17,10 +18,8 @@ fi
 # Install ruamel.yaml
 pip install --user --force-reinstall ruamel.yaml
 
-set -ex
-
-git clone --depth=1 https://git.yoctoproject.org/git/poky -b ${BRANCH}
-git clone --depth=1 ${LAYER_URL} -b ${LAYER_BRANCH:-$BRANCH} layer
+git clone --depth=1 https://git.yoctoproject.org/git/poky -b ${BRANCH} && cd poky && git log -1
+git clone --depth=1 ${LAYER_URL} -b ${LAYER_BRANCH:-$BRANCH} layer && cd layer && git log -1
 
 cd poky
 source oe-init-build-env

commit 4bf1aad38051e71c94262fa425c7947c3e5f4129

yocto/check-layer.sh: fix typo
Fixes: 1f35ea39a636 (yocto-check-layer: improve build script)

Change-Id: Ib99f4bf3b78d30e0001f04e686805674d04e9e5a
Signed-off-by: Nicolas Dechesne <nicolas.dechesne@linaro.org>

yocto/check-layer.sh
@@ -18,8 +18,8 @@ fi
 # Install ruamel.yaml
 pip install --user --force-reinstall ruamel.yaml
 
-git clone --depth=1 https://git.yoctoproject.org/git/poky -b ${BRANCH} && cd poky && git log -1
-git clone --depth=1 ${LAYER_URL} -b ${LAYER_BRANCH:-$BRANCH} layer && cd layer && git log -1
+git clone --depth=1 https://git.yoctoproject.org/git/poky -b ${BRANCH} && (cd poky && git log -1)
+git clone --depth=1 ${LAYER_URL} -b ${LAYER_BRANCH:-$BRANCH} layer && (cd layer && git log -1)
 
 cd poky
 source oe-init-build-env