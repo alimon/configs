60c137f5bee8eb99d1a61d1a253d9c7e6df4f2fe
Riku Voipio <riku.voipio@linaro.org>
2019-03-19 15:11:07 +0200

    ci-terraform-sanity-check: terraform sanity check script
    Modelled after dockerfiles CI. Implements SYS-192
    
    Change-Id: I7b814d50c00321ef1e1c18353b77e1bcdd0a2342
    Signed-off-by: Riku Voipio <riku.voipio@linaro.org>

---
+++ ci-terraform-sanity-check/builders.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+set -e
+
+echo ""
+echo "########################################################################"
+echo "    Gerrit Environment"
+env |grep '^GERRIT'
+echo "########################################################################"
+
+
+cd terraform/
+export GIT_PREVIOUS_COMMIT=$(git rev-parse HEAD~1)
+export GIT_COMMIT=${GERRIT_PATCHSET_REVISION}
+files=$(git diff --name-only ${GIT_PREVIOUS_COMMIT} ${GIT_COMMIT})
+echo Changes in: ${files}
+changed_dirs=$(dirname ${files})
+export AWS_ACCESS_KEY_ID=${TERRAFORM_BOT_ID}
+export AWS_SECRET_ACCESS_KEY=${TERRAFORM_BOT_KEY}
+for dir in ${changed_dirs}; do
+    cd $dir
+    terraform init
+    terraform plan --var-file *.tfvars -out demo.plan
+    cd ..
+done
+
