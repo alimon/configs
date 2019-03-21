d643971838720d54bef96a31390f890420de119e
Julien Duraj <julien.duraj@linaro.org>
2019-03-21 11:40:28 +0000

    ART benchmarks: remove boot.oat from archive
    boot.oat is not built anymore and the build does not pass anymore
    because of that.
    
    Change-Id: Icfd24ec392dd80b6e17fa95f1d739cb2e27c8ab2

--- linaro-art/microbenchmarks/builders.sh
+++ linaro-art/microbenchmarks/builders.sh
@@ -24,8 +24,8 @@ readonly out_krait=out/target/product/arm_krait
 readonly out_armv8=out/target/product/armv8
 readonly out_host=out/host/linux-x86
 
-tar -cJf test-arm-fs.txz ${out_host}/bin/ ${out_host}/framework/ ${out_krait}/data/ ${out_krait}/system/ ${out_krait}/dex_bootjars/system/framework/arm/boot.oat
-tar -cJf test-armv8-fs.txz ${out_host}/bin/ ${out_host}/framework/ ${out_armv8}/data/ ${out_armv8}/system/ ${out_armv8}/dex_bootjars/system/framework/arm64/boot.oat
+tar -cJf test-arm-fs.txz ${out_host}/bin/ ${out_host}/framework/ ${out_krait}/data/ ${out_krait}/system/
+tar -cJf test-armv8-fs.txz ${out_host}/bin/ ${out_host}/framework/ ${out_armv8}/data/ ${out_armv8}/system/
 
 mkdir -p pub
 mv ${WORKSPACE}/*.xml *.txz pub/
