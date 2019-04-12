45fcad983e3eb9eb7c089600ae6408da7b0e32db
Ryan Harkin <ryan.harkin@linaro.org>
2019-04-12 16:03:57 +0100

    warp7-openembedded: added new job for WaRP7 board
    Add a new job definition for building WaRP7 with rpb-openembedded style
    builds.
    
    This was copied from rpb-openembedded-sumo.yaml and related files, then
    customised for WaRP7.
    
    Change-Id: I73a7a1a1bd1c32b4573bfa799f91f5bdc5b77b13
    Signed-off-by: Ryan Harkin <ryan.harkin@linaro.org>

---
+++ warp7-openembedded/submit_for_testing.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+rm -rf configs
+git clone --depth 1 http://git.linaro.org/ci/job/configs.git
+
+# Used by DB410C's template:
+export RESIZE_ROOTFS=${RESIZE_ROOTFS:-}
+
+[ -z "${DEVICE_TYPE}" ] || \
+python configs/openembedded-lkft/submit_for_testing.py \
+  --device-type ${DEVICE_TYPE} \
+  --build-number ${BUILD_NUMBER} \
+  --lava-server ${LAVA_SERVER} \
+  --qa-server ${QA_SERVER} \
+  --qa-server-team ${QA_SERVER_TEAM} \
+  --qa-server-project ${QA_SERVER_PROJECT} \
+  --git-commit ${MANIFEST_COMMIT} \
+  --template-path configs/warp7-openembedded/lava-job-definitions/ \
+  --template-names template-boot.yaml
