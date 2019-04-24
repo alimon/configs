602c262544a129f71f711030d3d23193f055bd98
Arthur <arthur.she@linaro.org>
2019-04-24 16:30:26 -0700

    lhg-oe-build: Add igt test to dragonboard-410c rocko build
    Add igt test to dragonboard-410c rocko build
    
    Change-Id: Ife7ca4d31c6fcedb87f3275d78d9751ee21a4a7d

--- lhg-oe-build/submit_for_testing.sh
+++ lhg-oe-build/submit_for_testing.sh
@@ -19,3 +19,17 @@ python configs/openembedded-lkft/submit_for_testing.py \
   --git-commit ${BUILD_NUMBER} \
   --template-path configs/lhg-oe-build/lava-job-definitions \
   --template-names ${TEST_JOBS}
+
+# DB410c igt test only runs with rocko build and on staging LAVA
+[ "${DEVICE_TYPE}" == "dragonboard-410c" ] && [ "${MANIFEST_BRANCH}" == "rocko" ] && \
+export CHAMELIUM_IP="10.7.0.94" && \
+python configs/openembedded-lkft/submit_for_testing.py \
+  --device-type ${DEVICE_TYPE} \
+  --build-number ${BUILD_NUMBER} \
+  --lava-server https://staging.validation.linaro.org/RPC2/ \
+  --qa-server ${QA_SERVER} \
+  --qa-server-team lhg \
+  --qa-server-project ${QA_SERVER_PROJECT} \
+  --git-commit ${BUILD_NUMBER} \
+  --template-path configs/lhg-oe-build/lava-job-definitions \
+  --template-names template-igt-test.yaml
