77f9218657a2546fddd319abe5e1cc28ebc55e45
Maxim Uvarov <maxim.uvarov@linaro.org>
2019-04-30 10:22:38 +0300

    ledge-oe: add libelf-dev for building x86-64
    Similar to LKFT, workaround following error on kernel compilation:
    error: Cannot generate ORC metadata for
    CONFIG_UNWINDER_ORC=y, please install libelf-dev
    
    For reference, see also:
    https://bugs.linaro.org/show_bug.cgi?id=3459
    https://git.linaro.org/ci/job/configs.git/commit/?id=eadb9c70ea32ce47aa5e802937718640f799550c
    
    Change-Id: Ib0926994dcd62977978dd2224286c79b0f721154
    Signed-off-by: Maxim Uvarov <maxim.uvarov@linaro.org>

--- ledge/oe/builders.sh
+++ ledge/oe/builders.sh
@@ -21,7 +21,7 @@ if ! sudo DEBIAN_FRONTEND=noninteractive apt-get -q=2 update; then
   sudo DEBIAN_FRONTEND=noninteractive apt-get -q=2 update || true
 fi
 
-pkg_list="bc ccache chrpath cpio diffstat gawk git expect pkg-config python-requests python-crypto texinfo wget zlib1g-dev libglib2.0-dev libpixman-1-dev python python3 sudo"
+pkg_list="bc ccache chrpath cpio diffstat gawk git expect pkg-config python-requests python-crypto texinfo wget zlib1g-dev libglib2.0-dev libpixman-1-dev python python3 sudo libelf-dev"
 if ! sudo DEBIAN_FRONTEND=noninteractive apt-get -q=2 install -y ${pkg_list}; then
   echo "INFO: apt install error - try again in a moment"
   sleep 15
