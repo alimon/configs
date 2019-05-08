68b166d3cab7cc8dbbd74e82923bb131cb7f3284
Arthur <arthur.she@linaro.org>
2019-05-08 11:57:07 -0700

    lhg-oe-build: Add igt test to am57xx-evm rocko build
    Change-Id: I257de94f750496bc443414d539e7743d91170850

--- lhg-oe-build/submit_for_testing.sh
+++ lhg-oe-build/submit_for_testing.sh
@@ -5,31 +5,47 @@ set -ex
 rm -rf configs
 git clone --depth 1 http://git.linaro.org/ci/job/configs.git
 
-TEST_JOBS="template-tests-${MANIFEST_BRANCH}.yaml template-wifi-tests.yaml"
-[ "${DEVICE_TYPE}" == "dragonboard-410c" ] && TEST_JOBS="${TEST_JOBS} template-v4l2-compliance-test.yaml"
+PRODUCTION_LAVA_TEST_JOBS="template-tests-${MANIFEST_BRANCH}.yaml template-wifi-tests.yaml"
+STAGING_LAVA_TEST_JOBS=""
 
-[ -z "${DEVICE_TYPE}" ] || \
-python configs/openembedded-lkft/submit_for_testing.py \
-  --device-type ${DEVICE_TYPE} \
-  --build-number ${BUILD_NUMBER} \
-  --lava-server ${LAVA_SERVER} \
-  --qa-server ${QA_SERVER} \
-  --qa-server-team lhg \
-  --qa-server-project ${QA_SERVER_PROJECT} \
-  --git-commit ${BUILD_NUMBER} \
-  --template-path configs/lhg-oe-build/lava-job-definitions \
-  --template-names ${TEST_JOBS}
+case "${DEVICE_TYPE}" in
+  dragonboard-410c)
+    PRODUCTION_LAVA_TEST_JOBS="${PRODUCTION_LAVA_TEST_JOBS} template-v4l2-compliance-test.yaml"
+    if [ "${MANIFEST_BRANCH}" == "rocko" ]; then
+      STAGING_LAVA_TEST_JOBS="template-igt-test.yaml"
+      export CHAMELIUM_IP="10.7.0.94"
+    fi
+    ;;
+  am57xx-evm)
+    if [ "${MANIFEST_BRANCH}" == "rocko" ]; then
+      PRODUCTION_LAVA_TEST_JOBS="template-igt-test.yaml"
+      export CHAMELIUM_IP="10.7.0.93"
+    fi
+    ;;
+esac
 
-# DB410c igt test only runs with rocko build and on staging LAVA
-[ "${DEVICE_TYPE}" == "dragonboard-410c" ] && [ "${MANIFEST_BRANCH}" == "rocko" ] && \
-export CHAMELIUM_IP="10.7.0.94" && \
-python configs/openembedded-lkft/submit_for_testing.py \
-  --device-type ${DEVICE_TYPE} \
-  --build-number ${BUILD_NUMBER} \
-  --lava-server https://staging.validation.linaro.org/RPC2/ \
-  --qa-server ${QA_SERVER} \
-  --qa-server-team lhg \
-  --qa-server-project ${QA_SERVER_PROJECT} \
-  --git-commit ${BUILD_NUMBER} \
-  --template-path configs/lhg-oe-build/lava-job-definitions \
-  --template-names template-igt-test.yaml
+if [ -n "${DEVICE_TYPE}" ]; then
+  [ -z "${PRODUCTION_LAVA_TEST_JOBS}" ] || \
+  python configs/openembedded-lkft/submit_for_testing.py \
+    --device-type ${DEVICE_TYPE} \
+    --build-number ${BUILD_NUMBER} \
+    --lava-server ${LAVA_SERVER} \
+    --qa-server ${QA_SERVER} \
+    --qa-server-team lhg \
+    --qa-server-project ${QA_SERVER_PROJECT} \
+    --git-commit ${BUILD_NUMBER} \
+    --template-path configs/lhg-oe-build/lava-job-definitions \
+    --template-names ${PRODUCTION_LAVA_TEST_JOBS}
+  
+  [ -z "${STAGING_LAVA_TEST_JOBS}" ] || \
+  python configs/openembedded-lkft/submit_for_testing.py \
+    --device-type ${DEVICE_TYPE} \
+    --build-number ${BUILD_NUMBER} \
+    --lava-server https://staging.validation.linaro.org/RPC2/ \
+    --qa-server ${QA_SERVER} \
+    --qa-server-team lhg \
+    --qa-server-project ${QA_SERVER_PROJECT} \
+    --git-commit ${BUILD_NUMBER} \
+    --template-path configs/lhg-oe-build/lava-job-definitions \
+    --template-names ${STAGING_LAVA_TEST_JOBS}
+fi
