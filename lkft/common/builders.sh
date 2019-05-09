71f15b3eb25469815bdf2fc6f95b3dcf9ed28e23
Yongqin Liu <yongqin.liu@linaro.org>
2019-05-09 17:12:54 +0800

    lkft android: change to use lkft build directory
    Change-Id: I8cc8f330f7a4b977f5ad38ba49a9f5a886a0a40d
    Signed-off-by: Yongqin Liu <yongqin.liu@linaro.org>

--- lkft/common/builders.sh
+++ lkft/common/builders.sh
@@ -2,29 +2,34 @@
 
 set -ex
 
-if false; then
-    git config --global user.email "ci_notify@linaro.org"
-    git config --global user.name "Linaro CI"
+git config --global user.email "ci_notify@linaro.org"
+git config --global user.name "Linaro CI"
 
-    if ! sudo DEBIAN_FRONTEND=noninteractive apt-get -q=2 update; then
-      echo "INFO: apt update error - try again in a moment"
-      sleep 15
-      sudo DEBIAN_FRONTEND=noninteractive apt-get -q=2 update || true
-    fi
-    pkg_list="python-pip openssl libssl-dev coreutils"
-    if ! sudo DEBIAN_FRONTEND=noninteractive apt-get -q=2 install -y ${pkg_list}; then
-      echo "INFO: apt install error - try again in a moment"
-      sleep 15
-      sudo DEBIAN_FRONTEND=noninteractive apt-get -q=2 install -y ${pkg_list}
-    fi
-
-    # Install ruamel.yaml
-    pip install --user --force-reinstall ruamel.yaml
-
+if ! sudo DEBIAN_FRONTEND=noninteractive apt-get -q=2 update; then
+  echo "INFO: apt update error - try again in a moment"
+  sleep 15
+  sudo DEBIAN_FRONTEND=noninteractive apt-get -q=2 update || true
+fi
+pkg_list="python-pip openssl libssl-dev coreutils"
+if ! sudo DEBIAN_FRONTEND=noninteractive apt-get -q=2 install -y ${pkg_list}; then
+  echo "INFO: apt install error - try again in a moment"
+  sleep 15
+  sudo DEBIAN_FRONTEND=noninteractive apt-get -q=2 install -y ${pkg_list}
 fi
 
-wget https://android-git.linaro.org/android-build-configs.git/plain/lkft/linaro-lkft.sh?h=lkft -O linaro-lkft.sh
+# Install ruamel.yaml
+pip install --user --force-reinstall ruamel.yaml
 
+
+BUILD_DIR=${BUILD_DIR:-${JOB_NAME}}
+if [ ! -d "/home/buildslave/srv/${BUILD_DIR}" ]; then
+  sudo mkdir -p /home/buildslave/srv/${BUILD_DIR}
+  sudo chmod 777 /home/buildslave/srv/${BUILD_DIR}
+fi
+cd /home/buildslave/srv/${BUILD_DIR}
+rm -fr out/${ANDROID_BUILD_CONFIG}
+
+wget https://android-git.linaro.org/android-build-configs.git/plain/lkft/linaro-lkft.sh?h=lkft -O linaro-lkft.sh
 chmod +x linaro-lkft.sh
 ./linaro-lkft.sh -c "${ANDROID_BUILD_CONFIG}"
 mv out/${ANDROID_BUILD_CONFIG}/pinned-manifest/*-pinned.xml out/${ANDROID_BUILD_CONFIG}/pinned-manifest.xml
