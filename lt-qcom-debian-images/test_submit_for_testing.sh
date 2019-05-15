1481cabd35c19fffa380d299d38c1aa0efff1081
Aníbal Limón <anibal.limon@linaro.org>
2019-05-14 21:34:26 -0500

    lt-qcom-{openembedded, debian-images}: Only test_submit_for_testing db410c
    The other boards (now only db820c) requires a valid URLs and there are
    the same templates generated.
    
    Change-Id: I161e04ec60cd2a9ef8b70d9a66a4bca35389d758
    Signed-off-by: Aníbal Limón <anibal.limon@linaro.org>

--- lt-qcom-debian-images/test_submit_for_testing.sh
+++ lt-qcom-debian-images/test_submit_for_testing.sh
@@ -15,14 +15,12 @@ export QA_REPORTS_TOKEN="secret"
 export LAVA_SERVER=https://validation.linaro.org/RPC2/
 export PMWG_LAVA_SERVER=https://pmwg.validation.linaro.org/RPC2/
 export ARTIFACTORIAL_TOKEN="nosecret"
+export DRY_RUN="--dry-run "
+
+export DEVICE_TYPE=dragonboard-410c
 export PUBLISH_SERVER=https://snapshots.linaro.org/
 export PUB_DEST=96boards/dragonboard410c/${VENDOR}/debian/${BUILD_NUMBER}
-
-export DRY_RUN="--dry-run "
-for device in $(ls ../lt-qcom/lava-job-definitions/devices); do
-    export DEVICE_TYPE=$device
-    bash submit_for_testing.sh
-done
+bash submit_for_testing.sh
 
 # cleanup virtualenv
 deactivate
--- lt-qcom-openembedded/test_submit_for_testing.sh
+++ lt-qcom-openembedded/test_submit_for_testing.sh
@@ -9,17 +9,15 @@ pip install Jinja2 requests urllib3 ruamel.yaml
 export BUILD_NUMBER=125
 export DISTRO=rpb
 export MANIFEST_BRANCH=thud
-export BOOT_URL=https://snapshots.linaro.org/96boards/dragonboard410c/linaro/openembedded/thud/125/rpb/boot--4.14-r0-dragonboard-410c-20190409213001-125.img
-export ROOTFS_SPARSE_BUILD_URL=https://snapshots.linaro.org/96boards/dragonboard410c/linaro/openembedded/thud/125/rpb/rpb-console-image-test-dragonboard-410c-20190409213001-125.rootfs.img.gz
 export QA_SERVER="http://localhost:8000"
 export QA_REPORTS_TOKEN="secret"
 export LAVA_SERVER=https://validation.linaro.org/RPC2/
-
 export DRY_RUN="--dry-run "
-for device in $(ls ../lt-qcom/lava-job-definitions/devices); do
-    export MACHINE=$device
-    bash submit_for_testing.sh
-done
+
+export MACHINE=dragonboard-410c
+export BOOT_URL=https://snapshots.linaro.org/96boards/dragonboard410c/linaro/openembedded/thud/125/rpb/boot--4.14-r0-dragonboard-410c-20190409213001-125.img
+export ROOTFS_SPARSE_BUILD_URL=https://snapshots.linaro.org/96boards/dragonboard410c/linaro/openembedded/thud/125/rpb/rpb-console-image-test-dragonboard-410c-20190409213001-125.rootfs.img.gz
+bash submit_for_testing.sh
 
 # cleanup virtualenv
 deactivate
