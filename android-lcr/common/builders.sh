alimon/configs

commit 067c71857397daeee18dfda4c50b004fc81555ff

android lcr common builders: rm kmgk dir before..
..starting a build, to prevent errors seen for e.g. in job below:
LINK: https://ci.linaro.org/job/android-hikey-optee-master/85/consoleText

Change-Id: I29a8d44e7920c8dea1de4a7a63389c167e864a51
Signed-off-by: Victor Chong <victor.chong@linaro.org>

android-lcr/common/builders.sh
@@ -35,7 +35,7 @@ export PATH=${HOME}/bin:${PATH}
 
 # Install helper packages
 rm -rf build-tools jenkins-tools build-configs build/bionic build/frameworks/base build/system/core build/device build/out build/android-patchsets build/kernel/ti/x15 build/kernel/hikey-linaro build/hardware/interfaces
-rm -fr build/external/optee_test build/external/optee_client build/optee/optee_os build/optee/edk2
+rm -fr build/external/optee_test build/external/optee_client build/optee/optee_os build/optee/edk2 device/linaro/kmgk
 git clone --depth 1 https://git.linaro.org/infrastructure/linaro-android-build-tools.git build-tools
 git clone --depth 1 https://git.linaro.org/infrastructure/linaro-jenkins-tools.git jenkins-tools
 git clone --depth 1 http://android-git.linaro.org/git/android-build-configs.git build-configs

commit e93d7f2082ab35ccc98e0e3e651c7b3e561d0791

android lcr common builders: fix kmgk dir path..
..in 'rm' command

Fixes: 067c7185 ("android lcr common builders: rm kmgk dir before..")
Change-Id: Ic57faac1ad697fd903854010789d4bd7d39984c6
Signed-off-by: Victor Chong <victor.chong@linaro.org>

android-lcr/common/builders.sh
@@ -35,7 +35,7 @@ export PATH=${HOME}/bin:${PATH}
 
 # Install helper packages
 rm -rf build-tools jenkins-tools build-configs build/bionic build/frameworks/base build/system/core build/device build/out build/android-patchsets build/kernel/ti/x15 build/kernel/hikey-linaro build/hardware/interfaces
-rm -fr build/external/optee_test build/external/optee_client build/optee/optee_os build/optee/edk2 device/linaro/kmgk
+rm -fr build/external/optee_test build/external/optee_client build/optee/optee_os build/optee/edk2 build/device/linaro/kmgk
 git clone --depth 1 https://git.linaro.org/infrastructure/linaro-android-build-tools.git build-tools
 git clone --depth 1 https://git.linaro.org/infrastructure/linaro-jenkins-tools.git jenkins-tools
 git clone --depth 1 http://android-git.linaro.org/git/android-build-configs.git build-configs