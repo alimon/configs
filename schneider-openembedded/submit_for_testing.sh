7dcdea671abda22a6103989dd452e797dcd1231d
Ryan Harkin <ryan.harkin@linaro.org>
2019-05-20 16:21:27 +0100

    schneider-openembedded: only run jobs on rzn1d
    Rename the rzn1 template directory to rzn1d and make sure the jobs only
    run on rzn1d, not on other boards such as soca9, which are not present
    in LAVA yet.
    
    And when soca9 comes online, only a subset of the tests will be
    relevant. For example, soca9 doesn't boot a FIT image.
    
    Change-Id: Ib22d77182dc48a68375bc462f170a86539a21cb4
    Signed-off-by: Ryan Harkin <ryan.harkin@linaro.org>

--- schneider-openembedded/submit_for_testing.sh
+++ schneider-openembedded/submit_for_testing.sh
@@ -6,50 +6,51 @@ git clone --depth 1 http://git.linaro.org/ci/job/configs.git
 # Used by DB410C's template:
 export RESIZE_ROOTFS=${RESIZE_ROOTFS:-}
 
-[ -z "${DEVICE_TYPE}" ] || \
-python configs/openembedded-lkft/submit_for_testing.py \
-  --device-type ${DEVICE_TYPE} \
-  --build-number ${BUILD_NUMBER} \
-  --lava-server ${LAVA_SERVER} \
-  --qa-server ${QA_SERVER} \
-  --qa-server-team ${QA_SERVER_TEAM} \
-  --qa-server-project ${QA_SERVER_PROJECT} \
-  --git-commit ${MANIFEST_COMMIT} \
-  --template-path configs/schneider-openembedded/lava-job-definitions/ \
-  --template-names template-uboot.yaml
-
-[ -z "${DEVICE_TYPE}" ] || \
-python configs/openembedded-lkft/submit_for_testing.py \
-  --device-type ${DEVICE_TYPE} \
-  --build-number ${BUILD_NUMBER} \
-  --lava-server ${LAVA_SERVER} \
-  --qa-server ${QA_SERVER} \
-  --qa-server-team ${QA_SERVER_TEAM} \
-  --qa-server-project ${QA_SERVER_PROJECT} \
-  --git-commit ${MANIFEST_COMMIT} \
-  --template-path configs/schneider-openembedded/lava-job-definitions/ \
-  --template-names template-fit.yaml
-
-[ -z "${DEVICE_TYPE}" ] || \
-python configs/openembedded-lkft/submit_for_testing.py \
-  --device-type ${DEVICE_TYPE} \
-  --build-number ${BUILD_NUMBER} \
-  --lava-server ${LAVA_SERVER} \
-  --qa-server ${QA_SERVER} \
-  --qa-server-team ${QA_SERVER_TEAM} \
-  --qa-server-project ${QA_SERVER_PROJECT} \
-  --git-commit ${MANIFEST_COMMIT} \
-  --template-path configs/schneider-openembedded/lava-job-definitions/ \
-  --template-names template-zimage.yaml
-
-[ -z "${DEVICE_TYPE}" ] || \
-python configs/openembedded-lkft/submit_for_testing.py \
-  --device-type ${DEVICE_TYPE} \
-  --build-number ${BUILD_NUMBER} \
-  --lava-server ${LAVA_SERVER} \
-  --qa-server ${QA_SERVER} \
-  --qa-server-team ${QA_SERVER_TEAM} \
-  --qa-server-project ${QA_SERVER_PROJECT} \
-  --git-commit ${MANIFEST_COMMIT} \
-  --template-path configs/schneider-openembedded/lava-job-definitions/ \
-  --template-names template-zimage-dev.yaml
+if [ "${DEVICE_TYPE}" == "rzn1d" ]; then
+	python configs/openembedded-lkft/submit_for_testing.py \
+	  --device-type ${DEVICE_TYPE} \
+	  --build-number ${BUILD_NUMBER} \
+	  --lava-server ${LAVA_SERVER} \
+	  --qa-server ${QA_SERVER} \
+	  --qa-server-team ${QA_SERVER_TEAM} \
+	  --qa-server-project ${QA_SERVER_PROJECT} \
+	  --git-commit ${MANIFEST_COMMIT} \
+	  --template-path configs/schneider-openembedded/lava-job-definitions/ \
+	  --template-names template-uboot.yaml
+fi
+if [ "${DEVICE_TYPE}" == "rzn1d" ]; then
+	python configs/openembedded-lkft/submit_for_testing.py \
+	  --device-type ${DEVICE_TYPE} \
+	  --build-number ${BUILD_NUMBER} \
+	  --lava-server ${LAVA_SERVER} \
+	  --qa-server ${QA_SERVER} \
+	  --qa-server-team ${QA_SERVER_TEAM} \
+	  --qa-server-project ${QA_SERVER_PROJECT} \
+	  --git-commit ${MANIFEST_COMMIT} \
+	  --template-path configs/schneider-openembedded/lava-job-definitions/ \
+	  --template-names template-fit.yaml
+fi
+if [ "${DEVICE_TYPE}" == "rzn1d" ]; then
+	python configs/openembedded-lkft/submit_for_testing.py \
+	  --device-type ${DEVICE_TYPE} \
+	  --build-number ${BUILD_NUMBER} \
+	  --lava-server ${LAVA_SERVER} \
+	  --qa-server ${QA_SERVER} \
+	  --qa-server-team ${QA_SERVER_TEAM} \
+	  --qa-server-project ${QA_SERVER_PROJECT} \
+	  --git-commit ${MANIFEST_COMMIT} \
+	  --template-path configs/schneider-openembedded/lava-job-definitions/ \
+	  --template-names template-zimage.yaml
+fi
+if [ "${DEVICE_TYPE}" == "rzn1d" ]; then
+	python configs/openembedded-lkft/submit_for_testing.py \
+	  --device-type ${DEVICE_TYPE} \
+	  --build-number ${BUILD_NUMBER} \
+	  --lava-server ${LAVA_SERVER} \
+	  --qa-server ${QA_SERVER} \
+	  --qa-server-team ${QA_SERVER_TEAM} \
+	  --qa-server-project ${QA_SERVER_PROJECT} \
+	  --git-commit ${MANIFEST_COMMIT} \
+	  --template-path configs/schneider-openembedded/lava-job-definitions/ \
+	  --template-names template-zimage-dev.yaml
+fi
