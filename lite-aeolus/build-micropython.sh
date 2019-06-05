fb76d7b7a82aad8b8eccc702313c4d150a69cca9
Paul Sokolovsky <paul.sokolovsky@linaro.org>
2019-06-05 23:56:31 +0300

    lite-aeolus-micropython: qemu_x86: Disable running testsuite via piping
    Go ahead and make a switchover, it just failed nwo again.
    
    Change-Id: Id99adde6845a456b3b1417a065165d0b6a033283

--- lite-aeolus/build-micropython.sh
+++ lite-aeolus/build-micropython.sh
@@ -30,13 +30,14 @@ else
     make BOARD=${PLATFORM}
 fi
 
-if [ ${PLATFORM} = "qemu_x86" ]; then
-    # Run testsuite via piping scripts to REPL - doesn't work reliably
-    # with QEMU, will likely be removed
-    rm -f /tmp/slip.sock
-    (socat PTY,link=/tmp/slip.dev UNIX-LISTEN:/tmp/slip.sock &)
-    make BOARD=${PLATFORM} test
-fi
+# Disabled - too unreliable with current QEMUs
+#if [ ${PLATFORM} = "qemu_x86" ]; then
+#    # Run testsuite via piping scripts to REPL - doesn't work reliably
+#    # with QEMU, will likely be removed
+#    rm -f /tmp/slip.sock
+#    (socat PTY,link=/tmp/slip.dev UNIX-LISTEN:/tmp/slip.sock &)
+#    make BOARD=${PLATFORM} test
+#fi
 
 cd ${WORKSPACE}
 mkdir -p out/${PLATFORM}
