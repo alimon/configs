9425fcb9e72d12b3799ac8a3f077656c689b9c76
Ryan Harkin <ryan.harkin@linaro.org>
2019-06-28 13:55:27 +0100

    schneider-openembedded: test dip-image-edge on rzn1d
    Change-Id: I15c490226df1f1b215934be07b540baef64d4d26
    Signed-off-by: Ryan Harkin <ryan.harkin@linaro.org>

--- schneider-openembedded/submit_for_testing.sh
+++ schneider-openembedded/submit_for_testing.sh
@@ -54,3 +54,15 @@ if [ "${DEVICE_TYPE}" == "rzn1d" ] || [ "${DEVICE_TYPE}" == "soca9" ]; then
 	  --template-path configs/schneider-openembedded/lava-job-definitions/ \
 	  --template-names template-zimage-dev.yaml
 fi
+if [ "${DEVICE_TYPE}" == "rzn1d" ]; then
+	python configs/openembedded-lkft/submit_for_testing.py \
+	  --device-type ${DEVICE_TYPE} \
+	  --build-number ${BUILD_NUMBER} \
+	  --lava-server ${LAVA_SERVER} \
+	  --qa-server ${QA_SERVER} \
+	  --qa-server-team ${QA_SERVER_TEAM} \
+	  --qa-server-project ${QA_SERVER_PROJECT} \
+	  --git-commit ${MANIFEST_COMMIT} \
+	  --template-path configs/schneider-openembedded/lava-job-definitions/ \
+	  --template-names template-zimage-edge.yaml
+fi
