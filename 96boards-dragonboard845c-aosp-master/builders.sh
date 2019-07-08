c1bbf1119543c9b901f75b26e1843d453e143abf
Vishal Bhoj <vishal.bhoj@linaro.org>
2019-07-08 14:18:18 +0530

    96boards-dragonboard845c-aosp-master: fix local manifest location
    Change-Id: Iafa659a484fea7f86493c05d1fe827f92f020482
    Signed-off-by: Vishal Bhoj <vishal.bhoj@linaro.org>

--- 96boards-dragonboard845c-aosp-master/builders.sh
+++ 96boards-dragonboard845c-aosp-master/builders.sh
@@ -36,7 +36,7 @@ export PATH=${HOME}/bin:${PATH}
 repo init -u https://android.googlesource.com/platform/manifest 
 cd .repo
 rm -rf local_manifests
-git clone https://android-git.linaro.org/git/platform/manifest.git .repo/local_manifests -b db845c
+git clone https://android-git.linaro.org/git/platform/manifest.git local_manifests -b db845c
 cd -
 
 repo sync -j$(nproc) -c -f
