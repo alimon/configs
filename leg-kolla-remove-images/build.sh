8fe8790bab9622b2001a4f6bd4822a4ac717123e
Marcin Juszkiewicz <marcin.juszkiewicz@linaro.org>
2019-07-16 12:06:24 +0200

    leg-kolla-remove-images: you are allowed to fail
    Change-Id: I5966eaa7cbaf430413de007dafcddbfac6a71457

--- leg-kolla-remove-images/build.sh
+++ leg-kolla-remove-images/build.sh
@@ -13,6 +13,6 @@ mkdir -p ${HOME}/.docker
 sed -e "s|\${DOCKER_AUTH}|${DOCKER_AUTH}|" < ${WORKSPACE}/config.json > ${HOME}/.docker/config.json
 chmod 0600 ${HOME}/.docker/config.json
 
-docker images | grep ${kolla_tag} | awk "{print $3}" | xargs docker rmi -f
-docker images | grep ${kolla_tag} | awk "{print $3}" | xargs docker rmi -f
-docker images | grep ${kolla_tag} | awk "{print $3}" | xargs docker rmi -f
+docker images | grep ${kolla_tag} | awk "{print $3}" | xargs docker rmi -f || true
+docker images | grep ${kolla_tag} | awk "{print $3}" | xargs docker rmi -f || true
+docker images | grep ${kolla_tag} | awk "{print $3}" | xargs docker rmi -f || true
