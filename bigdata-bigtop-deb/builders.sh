12b67e15c7933f79419a3002cf31fe24ca2b0791
Guodong Xu <guodong.xu@linaro.org>
2019-08-19 14:29:54 +0800

    bigtop: update to v1.4.0-rc1 and include tarball packaging
    Upgrade to v1.4.0-rc1 and update build script.
    Branch working-tar-gz-packaging is based off bigtop V1.4.0-RC1. It
    also includes tarball packaging support.
    
    Signed-off-by: Guodong Xu <guodong.xu@linaro.org>
    Change-Id: I33f95844afbc0710555c32f16f9d2b1b51c57ad2

--- bigdata-bigtop-deb/builders.sh
+++ bigdata-bigtop-deb/builders.sh
@@ -7,19 +7,37 @@ export CPU_CORES=$(getconf _NPROCESSORS_ONLN)
 
 # get source
 rm -rf bigtop-trunk
-git clone --depth 1 --branch erp18.06 https://git.linaro.org/leg/bigdata/bigtop-trunk.git
+git clone --depth 1 --branch working-tar-gz-packaging https://git.linaro.org/leg/bigdata/bigtop-trunk.git
+
+# Before starting the container, give other users `w` access to `bigtop`
+# home directory. It is required for gradle installation as 'jenkins' users.
+# Otherwise, you will see this error when run 'gradlew tasks'.
+#   FAILED: Could not create service of type CrossBuildFileHashCache
+#    using BuildSessionScopeServices.createCrossBuildFileHashCache().
+chmod a+w bigtop-trunk
 
 # now build bigtop slaves
 cd bigtop-trunk
-# build docker images locally - only needed once
+
+# optionally, build docker images locally - only needed once
 #./gradlew -POS=debian-9 -Pprefix=erp18.06 bigtop-puppet
 #./gradlew -POS=debian-9 -Pprefix=erp18.06 bigtop-slaves
 
-# build bigdata bigtop components using locally built docker image. This will take a while.
+# build bigdata bigtop components. This will take a while.
 # Artifacts will be stored under individual component folder inside output folder.
-# components to be built: ambari bigtop-groovy bigtop-jsvc bigtop-tomcat bigtop-utils hadoop hbase hive spark zookeeper
-# docker run -v ${PWD}:/ws bigtop/slaves:erp18.06-debian-9-aarch64 bash -l -c 'cd /ws ; ./gradlew <comp>-deb'
-docker run -v ${PWD}:/ws bigtop/slaves:erp18.06-debian-9-aarch64 bash -l -c 'cd /ws ; ./gradlew bigtop-groovy-deb bigtop-jsvc-deb bigtop-tomcat-deb bigtop-utils-deb hadoop-deb zookeeper-deb spark-deb hive-deb hbase-deb ambari-deb'
+#
+# Example command line:
+#   docker run -it --rm -u jenkins --workdir /ws -v ${PWD}:/ws \
+#          bigtop/slaves:1.4.0-debian-9-aarch64 bash -l -c '. /etc/profile.d/bigtop.sh; ./gradlew deb repo'
+#
+# Note:
+#   - User 'jenkins' is employed. It exists by default in the root docker image of bigtop/slaves.
+#   - It's not allowed using 'root' to build bigtop. Some component refuses to be built in root.
+#   - Image "bigtop/slaves:*-aarch64" will be retrieved from docker hub on live.
+#   - bigtop.sh sets environment variables such as: JAVA_HOME, MAVEN_HOME, ANT_HOME, GRADLE_HOME, etc.
+
+docker run -it --rm -u jenkins --workdir /ws -v ${PWD}:/ws \
+          bigtop/slaves:1.4.0-debian-9-aarch64 bash -l -c '. /etc/profile.d/bigtop.sh; ./gradlew deb repo'
 
 # cleanup
 #docker prune -fa
