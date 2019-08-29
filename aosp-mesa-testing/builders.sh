0ef2ea0bdb2aed4ab78c38ceb48cd32b7b4e34e7
Vishal Bhoj <vishal.bhoj@linaro.org>
2019-08-30 00:11:08 +0530

    aosp-mesa-testing: add new job
    This job is used to test upstream mesa updates
    
    Change-Id: Iddff8f4278b2271bc93ecc8b395c689273bf7089
    Signed-off-by: Vishal Bhoj <vishal.bhoj@linaro.org>

---
+++ aosp-mesa-testing/builders.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+
+# Install needed packages
+sudo apt-get update
+sudo apt-get install -y bison git gperf libxml2-utils python-mako zip time python-requests genisoimage patch mtools python-wand rsync linaro-image-tools liblz4-tool lzop libssl-dev libdrm-intel1 python-pip
+
+wget -q \
+  http://repo.linaro.org/ubuntu/linaro-overlay/pool/main/a/android-tools/android-tools-fsutils_4.2.2+git20130218-3ubuntu41+linaro1_amd64.deb \
+  http://old-releases.ubuntu.com/ubuntu/pool/universe/o/openjdk-8/openjdk-8-jre-headless_8u45-b14-1_amd64.deb \
+  http://old-releases.ubuntu.com/ubuntu/pool/universe/o/openjdk-8/openjdk-8-jre_8u45-b14-1_amd64.deb \
+  http://old-releases.ubuntu.com/ubuntu/pool/universe/o/openjdk-8/openjdk-8-jdk_8u45-b14-1_amd64.deb
+sudo dpkg -i --force-all *.deb
+rm -f *.deb
+
+# Install jinja2-cli and ruamel.yaml
+pip install --user --force-reinstall jinja2-cli ruamel.yaml
+
+# Set local configuration
+git config --global user.email "ci_notify@linaro.org"
+git config --global user.name "Linaro CI"
+java -version
+
+BUILD_DIR=aosp-master/build
+if [ ! -d "/home/buildslave/srv/${BUILD_DIR}" ]; then
+  sudo mkdir -p /home/buildslave/srv/${BUILD_DIR}
+  sudo chmod 777 /home/buildslave/srv/${BUILD_DIR}
+fi
+cd /home/buildslave/srv/${BUILD_DIR}
+
+# Download helper scripts (repo)
+mkdir -p ${HOME}/bin
+curl https://storage.googleapis.com/git-repo-downloads/repo > ${HOME}/bin/repo
+chmod a+x ${HOME}/bin/*
+export PATH=${HOME}/bin:${PATH}
+
+repo init -u https://android.googlesource.com/platform/manifest 
+cd .repo
+rm -rf local_manifests
+git clone https://android-git.linaro.org/git/platform/manifest.git local_manifests -b db845c
+cd -
+
+export AOSP_MASTER_BUILD=fail
+repo sync -j$(nproc) -c -f
+rm -rf build-info
+source build/envsetup.sh
+lunch linaro_db845c-userdebug
+mma -j$(nproc)
+
+export AOSP_MASTER_BUILD=pass
+
+pushd external/mesa3d
+git remote rm upstream
+git remote add upstream git://anongit.freedesktop.org/mesa/mesa
+git pull
+git checkout ${GIT_COMMIT}
+AUTHOR_EMAIL_ADDRESS=$(git log --pretty=format:"%ae" HEAD -1)
+PATCH_SUBJECT=$(git log --pretty=format:"%s" HEAD -1)
+popd
+
+set -ex
+ln -sf /usr/bin/python prebuilts/build-tools/path/linux-x86/python
+mma -j$(nproc) libGLES_mesa hwcomposer.drm gralloc.gbm TEMPORARY_DISABLE_PATH_RESTRICTIONS=true
