5d7c0b57d8fbd3a24a8384756dec8dbcea858157
Paul Sokolovsky <paul.sokolovsky@linaro.org>
2019-09-11 11:04:04 +0300

    zephyr-upstream: Install python3-serial for build.
    The builds started to fail on "import serial" line in the "sanitycheck"
    script. I'm not sure what happened, as that line was there (unconditional)
    for a long time. Anyway, just explicitly install the dependency.
    
    Change-Id: I0470e59904b0076b271ef0f9dddec4809b33bc8e

--- zephyr-upstream/builders.sh
+++ zephyr-upstream/builders.sh
@@ -3,7 +3,7 @@
 sudo apt-get -q=2 update
 sudo apt-get -q=2 -y install git ninja-build g++ g++-multilib gperf python3-ply \
     python3-yaml gcc-arm-none-eabi python-requests rsync device-tree-compiler \
-    python3-pip python3-setuptools python3-wheel
+    python3-pip python3-serial python3-setuptools python3-wheel
 
 set -ex
 
