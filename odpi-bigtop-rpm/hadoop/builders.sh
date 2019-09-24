6c2443b06d705f9d5f7a1cce1b25f61d25088b9e
Riku Voipio <riku.voipio@linaro.org>
2019-09-25 00:17:39 +0100

    remove deleted jobs from git
    These jobs were originally attemted to be deleted by Vishal in
    
    https://review.linaro.org/c/ci/job/configs/+/31725
    
    The job failed due to renaming issues. It was fixed and the jobs
    were removed for real in
    
    https://ci.linaro.org/job/trigger-ci-job-configs/9687/console
    
    Now that the jobs are really gone, remove them from git too.
    
    Signed-off-by: Riku Voipio <riku.voipio@linaro.org>
    Change-Id: I069575498773a94ee5033f27feefea59fa00d8b6

--- odpi-bigtop-rpm/hadoop/builders.sh
+++
@@ -1,7 +0,0 @@
-# okay everything is in place, this build will take a while
-./gradlew clean hadoop-rpm \
-  -Pdist,native-win \
-  -DskipTests \
-  -Dtar \
-  -Dmaven.javadoc.skip=true \
-  --debug
--- odpi-bigtop-rpm/hive/builders.sh
+++
@@ -1,9 +0,0 @@
-# okay everything is in place, this build will take a while
-./gradlew clean hive-rpm \
-  -Pdist,native-win \
-  -DskipTests \
-  -Dtar \
-  -Dmaven.javadoc.skip=true \
-  -PHadoop-2.7 \
-  -Phadoop.version=2.7.3 \
-  --debug
