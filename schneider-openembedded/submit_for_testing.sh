6805530146c0490fbc9e75b3923cba284088784f
Ryan Harkin <ryan.harkin@linaro.org>
2019-09-26 08:57:59 +0100

    schneider-openembedded: add LTP and ptest defs
    Change-Id: Idc6289b42e74983c2acfecc1d0ba4d2cb53c8173
    Signed-off-by: Ryan Harkin <ryan.harkin@linaro.org>

--- schneider-openembedded/submit_for_testing.sh
+++ schneider-openembedded/submit_for_testing.sh
@@ -54,6 +54,31 @@ if [ "${DEVICE_TYPE}" == "rzn1d" ] || [ "${DEVICE_TYPE}" == "soca9" ]; then
 	  --template-path configs/schneider-openembedded/lava-job-definitions/ \
 	  --template-names template-zimage-dev.yaml
 fi
+if [ "${DEVICE_TYPE}" == "rzn1d" ] || [ "${DEVICE_TYPE}" == "soca9" ]; then
+	python configs/openembedded-lkft/submit_for_testing.py \
+	  --device-type ${DEVICE_TYPE} \
+	  --build-number ${BUILD_NUMBER} \
+	  --lava-server ${LAVA_SERVER} \
+	  --qa-server ${QA_SERVER} \
+	  --qa-server-team ${QA_SERVER_TEAM} \
+	  --qa-server-project ${QA_SERVER_PROJECT} \
+	  --git-commit ${MANIFEST_COMMIT} \
+	  --template-path configs/schneider-openembedded/lava-job-definitions/ \
+	  --template-names template-zimage-dev-ltp.yaml
+fi
+if [ "${DEVICE_TYPE}" == "rzn1d" ] || [ "${DEVICE_TYPE}" == "soca9" ]; then
+	python configs/openembedded-lkft/submit_for_testing.py \
+	  --device-type ${DEVICE_TYPE} \
+	  --build-number ${BUILD_NUMBER} \
+	  --lava-server ${LAVA_SERVER} \
+	  --qa-server ${QA_SERVER} \
+	  --qa-server-team ${QA_SERVER_TEAM} \
+	  --qa-server-project ${QA_SERVER_PROJECT} \
+	  --git-commit ${MANIFEST_COMMIT} \
+	  --template-path configs/schneider-openembedded/lava-job-definitions/ \
+	  --template-names template-zimage-dev-ptest.yaml
+fi
+
 if [ "${DEVICE_TYPE}" == "rzn1d" ]; then
 	python configs/openembedded-lkft/submit_for_testing.py \
 	  --device-type ${DEVICE_TYPE} \
@@ -89,4 +114,26 @@ if [ "${DEVICE_TYPE}" == "soca9" ]; then
 	  --git-commit ${MANIFEST_COMMIT} \
 	  --template-path configs/schneider-openembedded/lava-job-definitions/ \
 	  --template-names template-wic-dev.yaml
+fiif [ "${DEVICE_TYPE}" == "soca9" ]; then
+	python configs/openembedded-lkft/submit_for_testing.py \
+	  --device-type ${DEVICE_TYPE} \
+	  --build-number ${BUILD_NUMBER} \
+	  --lava-server ${LAVA_SERVER} \
+	  --qa-server ${QA_SERVER} \
+	  --qa-server-team ${QA_SERVER_TEAM} \
+	  --qa-server-project ${QA_SERVER_PROJECT} \
+	  --git-commit ${MANIFEST_COMMIT} \
+	  --template-path configs/schneider-openembedded/lava-job-definitions/ \
+	  --template-names template-wic-dev-ltp.yaml
+fiif [ "${DEVICE_TYPE}" == "soca9" ]; then
+	python configs/openembedded-lkft/submit_for_testing.py \
+	  --device-type ${DEVICE_TYPE} \
+	  --build-number ${BUILD_NUMBER} \
+	  --lava-server ${LAVA_SERVER} \
+	  --qa-server ${QA_SERVER} \
+	  --qa-server-team ${QA_SERVER_TEAM} \
+	  --qa-server-project ${QA_SERVER_PROJECT} \
+	  --git-commit ${MANIFEST_COMMIT} \
+	  --template-path configs/schneider-openembedded/lava-job-definitions/ \
+	  --template-names template-wic-dev-ptest.yaml
 fi
