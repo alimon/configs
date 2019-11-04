a3c22a5757a6df92f0576e69d3d81e754d71bf81
Riku Voipio <riku.voipio@linaro.org>
2019-11-04 14:20:01 +0200

    ci-dockerslaves-cleanup: Delete images removed from dockerhub
    Delete images no longer in dockerhub
    
    Change-Id: I1be22a052dd81bc33d58f9f8918f57dbf2150221

---
+++ ci-dockerfiles-deployment/cleanup-update.sh
@@ -0,0 +1,20 @@
+#!/bin/bash -e
+
+for image in $(docker images|grep tcwg|grep -v none|awk '{ print $1":"$2}');
+do
+    echo delete: $image;
+    docker rmi $image;
+done
+
+for image in $(docker images|grep linaro|grep -v none|awk '{ print $1":"$2}');
+do
+    echo update: $image:
+    if ! docker pull $image
+    then
+        echo could not fetch image from dockerhub, delete
+        docker rmi $image||true
+    fi
+done
+
+echo cleaning up
+docker system prune -f
