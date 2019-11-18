2d3920986567c4a60b145c0ac258cada8552aaca
Riku Voipio <riku.voipio@linaro.org>
2019-11-18 11:00:59 +0200

    ldcg-bigdata-apache-arrow: move to generic invocation of llp
    The current OE version of the script creates:
    
    https://snapshots.linaro.org/reference-platform/components/bigdata/latest/
    
    While what we actually want is:
    
    https://snapshots.linaro.org/reference-platform/components/bigdata/apache-arrow/latest/
    
    This is because the rpb publisher expects PUB_DEST to include a distro
    parameter. So lets not use that, and instead make a generic llp/publisher.sh we can use
    on all simple usecases
    
    Change-Id: I1acf094967b8f8489bdc9fe90be12ba521f400ad
    Signed-off-by: Riku Voipio <riku.voipio@linaro.org>

---
+++ llp/publisher.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+if [ -f ${WORKSPACE}/BUILD-INFO.txt ];then
+    BUILD_INFO="--build-info ${WORKSPACE}/BUILD-INFO.txt"
+else
+    BUILD_INFO=""
+fi
+
+if [ -z "${DEPLOY_DIR_IMAGE}" ] || [ -z "${PUB_DEST}" ] || [ -z "${PUBLISH_SERVER}" ]
+then
+    echo "== missing publishing variables =="
+    echo "DEPLOY_DIR_IMAGE = ${DEPLOY_DIR_IMAGE}"
+    echo "PUB_DEST         = ${PUB_DEST}"
+    echo "PUBLISH_SERVER   = ${PUBLISH_SERVER}"
+    exit 1
+fi
+
+# Publish
+test -d ${HOME}/bin || mkdir ${HOME}/bin
+wget -q https://git.linaro.org/ci/publishing-api.git/blob_plain/HEAD:/linaro-cp.py -O ${HOME}/bin/linaro-cp.py
+time python3 ${HOME}/bin/linaro-cp.py \
+  --api_version 3 \
+  --link-latest \
+  --server ${PUBLISH_SERVER} \
+  ${BUILD_INFO} \
+  ${DEPLOY_DIR_IMAGE}/ ${PUB_DEST}
