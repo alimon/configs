5e83397fc69f23c92e5a978247126329330d7001
Victor Chong <victor.chong@linaro.org>
2019-12-01 02:42:45 +0000

    lcr: common: builders: use pip to install pycryptodome
    On Ubuntu 16.04 and probably earlier, need to use pip rather than apt
    directly.
    
    Fixes: 65ecefd8756e (lcr: common: builders: install python Cryptodome)
    Change-Id: I81b46d4d262234e82b5cf688d9f5b6aa161d0008
    Signed-off-by: Victor Chong <victor.chong@linaro.org>

--- android-lcr/common/builders.sh
+++ android-lcr/common/builders.sh
@@ -2,7 +2,9 @@
 
 # Install needed packages
 sudo apt-get update
-sudo apt-get install -y bison git gperf libxml2-utils python-mako zip time python-requests genisoimage patch mtools python-wand rsync linaro-image-tools liblz4-tool lzop libssl-dev libdrm-intel1 python-pip python-pyelftools python3-pyelftools python-crypto python3-crypto python-pycryptodome python3-pycryptodome
+sudo apt-get install -y bison git gperf libxml2-utils python-mako zip time python-requests genisoimage patch mtools python-wand rsync linaro-image-tools liblz4-tool lzop libssl-dev libdrm-intel1 python-pip python3-pip python-pyelftools python3-pyelftools python-crypto python3-crypto
+sudo python -m pip install --user python-pycryptodome
+sudo python3 -m pip install --user python3-pycryptodome
 
 wget -q \
   http://repo.linaro.org/ubuntu/linaro-overlay/pool/main/a/android-tools/android-tools-fsutils_4.2.2+git20130218-3ubuntu41+linaro1_amd64.deb \
