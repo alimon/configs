alimon/configs

commit 766b5c88a511d6550ffbfad2a43631a526449b42

lcr: common: builders: upgrade pip for python3
Change-Id: I5659572f66ec9c1b5819380b4521e88e3ec20309
Signed-off-by: Victor Chong <victor.chong@linaro.org>

android-lcr/common/builders.sh
@@ -3,7 +3,8 @@
 # Install needed packages
 sudo apt-get update
 sudo apt-get install -y bison git gperf libxml2-utils python-mako zip time python-requests genisoimage patch mtools python-wand rsync linaro-image-tools liblz4-tool lzop libssl-dev libdrm-intel1 python-pip python3-pip python-pyelftools python3-pyelftools python-crypto python3-crypto
-sudo pip install --upgrade pip
+sudo python pip install --upgrade pip
+sudo python3 pip install --upgrade pip
 sudo python -m pip install --user python-pycryptodome
 sudo python3 -m pip install --user python3-pycryptodome
 

commit 0de4a6aaff0968d2e87791cf96ff085dc72ba05c

android: lcr: build: fix pip upgrade
Missing -m switch.

Fixes: 766b5c88a511 (lcr: common: builders: upgrade pip for python3)
Change-Id: Id93d65af496e3a1f263f71d916a252efea5fabb2
Signed-off-by: Victor Chong <victor.chong@linaro.org>

android-lcr/common/builders.sh
@@ -3,8 +3,8 @@
 # Install needed packages
 sudo apt-get update
 sudo apt-get install -y bison git gperf libxml2-utils python-mako zip time python-requests genisoimage patch mtools python-wand rsync linaro-image-tools liblz4-tool lzop libssl-dev libdrm-intel1 python-pip python3-pip python-pyelftools python3-pyelftools python-crypto python3-crypto
-sudo python pip install --upgrade pip
-sudo python3 pip install --upgrade pip
+sudo python -m pip install --upgrade pip
+sudo python3 -m pip install --upgrade pip
 sudo python -m pip install --user python-pycryptodome
 sudo python3 -m pip install --user python3-pycryptodome
 