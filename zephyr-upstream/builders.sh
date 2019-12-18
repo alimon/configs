fb39cebd7f84e73fd97b3509aa6fb1ad70d97ca4
Paul Sokolovsky <paul.sokolovsky@linaro.org>
2019-12-18 19:06:17 +0300

    zephyr-upstream: Install Python3.6 and use it for Zephyr builds.
    As of today, Zephyr master requires Python3.6.
    
    Change-Id: I553ff6c3064a672faa04e02d8a51aef67783dc27
    Signed-off-by: Paul Sokolovsky <paul.sokolovsky@linaro.org>

--- zephyr-upstream/builders.sh
+++ zephyr-upstream/builders.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
+# Zephyr 2.2+ requires Python3.6. As it's not available in official distro
+# packages for Ubuntu Xenial (16.04) which we use, install it from PPA.
+sudo add-apt-repository -y ppa:deadsnakes/ppa
 sudo apt-get -q=2 update
+sudo apt-get install -y python3.6
+sudo ln -sf python3.6 /usr/bin/python3
+
 sudo apt-get -q=2 -y install git ninja-build g++ g++-multilib gperf python3-ply \
     python3-yaml gcc-arm-none-eabi python-requests rsync device-tree-compiler \
     python3-pip python3-serial python3-setuptools python3-wheel \
