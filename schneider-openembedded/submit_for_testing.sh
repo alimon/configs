990fc6e0296047891868afc5df7508174ccc0d66
Ryan Harkin <ryan.harkin@linaro.org>
2019-12-27 12:35:16 +0000

    se: improve job submission script
    Remove duplication of submission calls, and simplify the script by
    providing lists of jobs to run.
    
    Change-Id: I7dae45ba6cb411d7490c17ae3612c15d5fbdbbc5
    Signed-off-by: Ryan Harkin <ryan.harkin@linaro.org>

--- schneider-openembedded/submit_for_testing.sh
+++ schneider-openembedded/submit_for_testing.sh
@@ -6,7 +6,36 @@ git clone --depth 1 http://git.linaro.org/ci/job/configs.git
 # Used by DB410C's template:
 export RESIZE_ROOTFS=${RESIZE_ROOTFS:-}
 
-if [ "${DEVICE_TYPE}" == "rzn1d" ] || [ "${DEVICE_TYPE}" == "soca9" ]; then
+templates_common=(\
+	uboot.yaml \
+	tftp-nfs.yaml \
+	tftp-nfs-dev.yaml \
+	wic.yaml \
+	wic-dev.yaml \
+)
+
+templates_soca9=(\
+	wic-dev-ltp-1.yaml \
+	wic-dev-ltp-2.yaml \
+	wic-dev-ptest.yaml \
+)
+
+templates_rzn1d=(\
+	tftp-nfs-dev-ltp.yaml \
+	tftp-nfs-dev-ptest.yaml \
+	ubi.yaml \
+	ubi-edge.yaml \
+	wic-edge.yaml \
+)
+
+if [ "${DEVICE_TYPE}" == "rzn1d" ]; then
+	templates=( ${templates_common[@]} ${templates_rzn1d[@]} )
+else
+	templates=( ${templates_common[@]} ${templates_soca9[@]} )
+fi
+
+for template in ${templates[@]};
+do
 	python configs/openembedded-lkft/submit_for_testing.py \
 	  --device-type ${DEVICE_TYPE} \
 	  --build-number ${BUILD_NUMBER} \
@@ -17,185 +46,4 @@ if [ "${DEVICE_TYPE}" == "rzn1d" ] || [ "${DEVICE_TYPE}" == "soca9" ]; then
 	  --git-commit ${MANIFEST_COMMIT} \
 	  --template-path configs/schneider-openembedded/lava-job-definitions/ \
 	  --template-names uboot.yaml
-fi
-
-if [ "${DEVICE_TYPE}" == "rzn1d" ] || [ "${DEVICE_TYPE}" == "soca9" ]; then
-	python configs/openembedded-lkft/submit_for_testing.py \
-	  --device-type ${DEVICE_TYPE} \
-	  --build-number ${BUILD_NUMBER} \
-	  --lava-server ${LAVA_SERVER} \
-	  --qa-server ${QA_SERVER} \
-	  --qa-server-team ${QA_SERVER_TEAM} \
-	  --qa-server-project ${QA_SERVER_PROJECT} \
-	  --git-commit ${MANIFEST_COMMIT} \
-	  --template-path configs/schneider-openembedded/lava-job-definitions/ \
-	  --template-names tftp-nfs.yaml
-fi
-
-if [ "${DEVICE_TYPE}" == "rzn1d" ] || [ "${DEVICE_TYPE}" == "soca9" ]; then
-	python configs/openembedded-lkft/submit_for_testing.py \
-	  --device-type ${DEVICE_TYPE} \
-	  --build-number ${BUILD_NUMBER} \
-	  --lava-server ${LAVA_SERVER} \
-	  --qa-server ${QA_SERVER} \
-	  --qa-server-team ${QA_SERVER_TEAM} \
-	  --qa-server-project ${QA_SERVER_PROJECT} \
-	  --git-commit ${MANIFEST_COMMIT} \
-	  --template-path configs/schneider-openembedded/lava-job-definitions/ \
-	  --template-names tftp-nfs-dev.yaml
-fi
-
-if [ "${DEVICE_TYPE}" == "rzn1d" ]; then
-	python configs/openembedded-lkft/submit_for_testing.py \
-	  --device-type ${DEVICE_TYPE} \
-	  --build-number ${BUILD_NUMBER} \
-	  --lava-server ${LAVA_SERVER} \
-	  --qa-server ${QA_SERVER} \
-	  --qa-server-team ${QA_SERVER_TEAM} \
-	  --qa-server-project ${QA_SERVER_PROJECT} \
-	  --git-commit ${MANIFEST_COMMIT} \
-	  --template-path configs/schneider-openembedded/lava-job-definitions/ \
-	  --template-names ubi.yaml
-fi
-
-if [ "${DEVICE_TYPE}" == "rzn1d" ]; then
-	python configs/openembedded-lkft/submit_for_testing.py \
-	  --device-type ${DEVICE_TYPE} \
-	  --build-number ${BUILD_NUMBER} \
-	  --lava-server ${LAVA_SERVER} \
-	  --qa-server ${QA_SERVER} \
-	  --qa-server-team ${QA_SERVER_TEAM} \
-	  --qa-server-project ${QA_SERVER_PROJECT} \
-	  --git-commit ${MANIFEST_COMMIT} \
-	  --template-path configs/schneider-openembedded/lava-job-definitions/ \
-	  --template-names ubi-edge.yaml
-fi
-
-
-if [ "${DEVICE_TYPE}" == "rzn1d" ]; then
-	python configs/openembedded-lkft/submit_for_testing.py \
-	  --device-type ${DEVICE_TYPE} \
-	  --build-number ${BUILD_NUMBER} \
-	  --lava-server ${LAVA_SERVER} \
-	  --qa-server ${QA_SERVER} \
-	  --qa-server-team ${QA_SERVER_TEAM} \
-	  --qa-server-project ${QA_SERVER_PROJECT} \
-	  --git-commit ${MANIFEST_COMMIT} \
-	  --template-path configs/schneider-openembedded/lava-job-definitions/ \
-	  --template-names wic.yaml
-fi
-
-if [ "${DEVICE_TYPE}" == "rzn1d" ]; then
-	python configs/openembedded-lkft/submit_for_testing.py \
-	  --device-type ${DEVICE_TYPE} \
-	  --build-number ${BUILD_NUMBER} \
-	  --lava-server ${LAVA_SERVER} \
-	  --qa-server ${QA_SERVER} \
-	  --qa-server-team ${QA_SERVER_TEAM} \
-	  --qa-server-project ${QA_SERVER_PROJECT} \
-	  --git-commit ${MANIFEST_COMMIT} \
-	  --template-path configs/schneider-openembedded/lava-job-definitions/ \
-	  --template-names wic-dev.yaml
-fi
-
-if [ "${DEVICE_TYPE}" == "rzn1d" ]; then
-	python configs/openembedded-lkft/submit_for_testing.py \
-	  --device-type ${DEVICE_TYPE} \
-	  --build-number ${BUILD_NUMBER} \
-	  --lava-server ${LAVA_SERVER} \
-	  --qa-server ${QA_SERVER} \
-	  --qa-server-team ${QA_SERVER_TEAM} \
-	  --qa-server-project ${QA_SERVER_PROJECT} \
-	  --git-commit ${MANIFEST_COMMIT} \
-	  --template-path configs/schneider-openembedded/lava-job-definitions/ \
-	  --template-names wic-edge.yaml
-fi
-
-if [ "${DEVICE_TYPE}" == "soca9" ]; then
-	python configs/openembedded-lkft/submit_for_testing.py \
-	  --device-type ${DEVICE_TYPE} \
-	  --build-number ${BUILD_NUMBER} \
-	  --lava-server ${LAVA_SERVER} \
-	  --qa-server ${QA_SERVER} \
-	  --qa-server-team ${QA_SERVER_TEAM} \
-	  --qa-server-project ${QA_SERVER_PROJECT} \
-	  --git-commit ${MANIFEST_COMMIT} \
-	  --template-path configs/schneider-openembedded/lava-job-definitions/ \
-	  --template-names wic.yaml
-fi
-
-if [ "${DEVICE_TYPE}" == "soca9" ]; then
-	python configs/openembedded-lkft/submit_for_testing.py \
-	  --device-type ${DEVICE_TYPE} \
-	  --build-number ${BUILD_NUMBER} \
-	  --lava-server ${LAVA_SERVER} \
-	  --qa-server ${QA_SERVER} \
-	  --qa-server-team ${QA_SERVER_TEAM} \
-	  --qa-server-project ${QA_SERVER_PROJECT} \
-	  --git-commit ${MANIFEST_COMMIT} \
-	  --template-path configs/schneider-openembedded/lava-job-definitions/ \
-	  --template-names wic-dev.yaml
-fi
-
-if [ "${DEVICE_TYPE}" == "rzn1d" ]; then
-	python configs/openembedded-lkft/submit_for_testing.py \
-	  --device-type ${DEVICE_TYPE} \
-	  --build-number ${BUILD_NUMBER} \
-	  --lava-server ${LAVA_SERVER} \
-	  --qa-server ${QA_SERVER} \
-	  --qa-server-team ${QA_SERVER_TEAM} \
-	  --qa-server-project ${QA_SERVER_PROJECT} \
-	  --git-commit ${MANIFEST_COMMIT} \
-	  --template-path configs/schneider-openembedded/lava-job-definitions/ \
-	  --template-names tftp-nfs-dev-ltp.yaml
-fi
-
-if [ "${DEVICE_TYPE}" == "soca9" ]; then
-	python configs/openembedded-lkft/submit_for_testing.py \
-	  --device-type ${DEVICE_TYPE} \
-	  --build-number ${BUILD_NUMBER} \
-	  --lava-server ${LAVA_SERVER} \
-	  --qa-server ${QA_SERVER} \
-	  --qa-server-team ${QA_SERVER_TEAM} \
-	  --qa-server-project ${QA_SERVER_PROJECT} \
-	  --git-commit ${MANIFEST_COMMIT} \
-	  --template-path configs/schneider-openembedded/lava-job-definitions/ \
-	  --template-names wic-dev-ltp-1.yaml
-
-	python configs/openembedded-lkft/submit_for_testing.py \
-	  --device-type ${DEVICE_TYPE} \
-	  --build-number ${BUILD_NUMBER} \
-	  --lava-server ${LAVA_SERVER} \
-	  --qa-server ${QA_SERVER} \
-	  --qa-server-team ${QA_SERVER_TEAM} \
-	  --qa-server-project ${QA_SERVER_PROJECT} \
-	  --git-commit ${MANIFEST_COMMIT} \
-	  --template-path configs/schneider-openembedded/lava-job-definitions/ \
-	  --template-names wic-dev-ltp-2.yaml
-fi
-
-if [ "${DEVICE_TYPE}" == "rzn1d" ] ; then
-	python configs/openembedded-lkft/submit_for_testing.py \
-	  --device-type ${DEVICE_TYPE} \
-	  --build-number ${BUILD_NUMBER} \
-	  --lava-server ${LAVA_SERVER} \
-	  --qa-server ${QA_SERVER} \
-	  --qa-server-team ${QA_SERVER_TEAM} \
-	  --qa-server-project ${QA_SERVER_PROJECT} \
-	  --git-commit ${MANIFEST_COMMIT} \
-	  --template-path configs/schneider-openembedded/lava-job-definitions/ \
-	  --template-names tftp-nfs-dev-ptest.yaml
-fi
-
-if [ "${DEVICE_TYPE}" == "soca9" ]; then
-	python configs/openembedded-lkft/submit_for_testing.py \
-	  --device-type ${DEVICE_TYPE} \
-	  --build-number ${BUILD_NUMBER} \
-	  --lava-server ${LAVA_SERVER} \
-	  --qa-server ${QA_SERVER} \
-	  --qa-server-team ${QA_SERVER_TEAM} \
-	  --qa-server-project ${QA_SERVER_PROJECT} \
-	  --git-commit ${MANIFEST_COMMIT} \
-	  --template-path configs/schneider-openembedded/lava-job-definitions/ \
-	  --template-names wic-dev-ptest.yaml
-fi
+done
