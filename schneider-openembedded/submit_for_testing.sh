3fbb4b9d7ec8c40f84a613480ec8ae415ec8c62f
Ryan Harkin <ryan.harkin@linaro.org>
2019-12-30 10:19:21 +0000

    se: git log inside the repo
    A previous commit added a "git log -1" command, but didn't run it from
    inside the repo.
    
    Change-Id: I2949e29d678ed14fddb79c73d32ba7e0d62c5d8b
    Signed-off-by: Ryan Harkin <ryan.harkin@linaro.org>

--- schneider-openembedded/submit_for_testing.sh
+++ schneider-openembedded/submit_for_testing.sh
@@ -2,7 +2,9 @@
 
 rm -rf configs
 git clone --depth 1 http://git.linaro.org/ci/job/configs.git
+pushd configs
 git log -1
+popd
 
 # Used by DB410C's template:
 export RESIZE_ROOTFS=${RESIZE_ROOTFS:-}
