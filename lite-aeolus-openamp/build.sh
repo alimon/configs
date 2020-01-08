2dd5164812e289f6b0e4cfa9355e9245b5713d1b
Paul Sokolovsky <paul.sokolovsky@linaro.org>
2020-01-08 13:53:29 +0300

    lite-aeolus-openamp: Quiet download, mock git revision.
    Change-Id: I1b4d5d9ad03599aeef166fc7dec51681b584def6
    Signed-off-by: Paul Sokolovsky <paul.sokolovsky@linaro.org>

--- lite-aeolus-openamp/build.sh
+++ lite-aeolus-openamp/build.sh
@@ -2,4 +2,7 @@
 set -ex
 
 # We don't build anything so far, just downloading pre-built.
-wget https://people.linaro.org/~ed.mooring/Images/openamp-image-minimal-zcu102-zynqmp.wic.qemu-sd
+wget -q https://people.linaro.org/~ed.mooring/Images/openamp-image-minimal-zcu102-zynqmp.wic.qemu-sd
+
+#echo "GIT_COMMIT=$(git rev-parse --short=8 HEAD)" > env_var_parameters
+echo "GIT_COMMIT=mock" > env_var_parameters
