5b7f7ce0a627793c44981f4381891271c8e5b627
Paul Sokolovsky <paul.sokolovsky@linaro.org>
2020-01-09 17:13:00 +0300

    lite-aeolus-openamp/install-deps.sh: Try to cut down on apt spam.
    Change-Id: Iab1ea4fac5aae5dbc044692903c78d9a4a1508e9
    Signed-off-by: Paul Sokolovsky <paul.sokolovsky@linaro.org>

--- lite-aeolus-openamp/install-deps.sh
+++ lite-aeolus-openamp/install-deps.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 set -ex
 
-sudo apt update
-sudo apt -q=2 -y install jq python3-pip
+export DEBIAN_FRONTEND=noninteractive
+sudo apt-get -qq update
+sudo apt-get -qq -y install jq python3-pip
 pip3 install yq
