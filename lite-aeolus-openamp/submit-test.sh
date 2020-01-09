99b69dcbce79278d67d58831c11deef3595eacd0
Paul Sokolovsky <paul.sokolovsky@linaro.org>
2020-01-09 13:23:35 +0300

    lite-aeolus-openamp: Pass the actual published artifact to LAVA.
    Previously, a sample image hosted on people.linaro.org was passed to LAVA.
    We still use that image, but we use the URL of that image published as a
    build artifact, so that gets us closer to the "real" CI job. Literally,
    once that is working, the image build phase is the only missing part.
    
    Change-Id: Ibeef69ea9290e1e4c5b7d0c6c6c2243a314fb6f4
    Signed-off-by: Paul Sokolovsky <paul.sokolovsky@linaro.org>

--- lite-aeolus-openamp/submit-test.sh
+++ lite-aeolus-openamp/submit-test.sh
@@ -7,4 +7,11 @@ if [ ! -d lite-lava-docker-compose ]; then
     git clone https://github.com/Linaro/lite-lava-docker-compose
 fi
 
-python3 $dir/lava-submit.py lite-lava-docker-compose/example/docker-xilinx-qemu-openamp-echo_test.job
+IMAGE_URL="http://snapshots.linaro.org/components/kernel/aeolus-2/openamp/${BRANCH}/${PLATFORM}/${BUILD_NUMBER}/openamp-image-minimal-zcu102-zynqmp.wic.qemu-sd"
+
+# Replace image url (passed as docker image command-line arg) in the job
+# template. "yq" tools is used, https://github.com/kislyuk/yq (note:
+# there're different yq's around, we used Python wrapper around jq).
+yq -y ".actions[1].boot.command=\"$IMAGE_URL\"" lite-lava-docker-compose/example/docker-xilinx-qemu-openamp-echo_test.job > lava.job
+
+python3 $dir/lava-submit.py lava.job
