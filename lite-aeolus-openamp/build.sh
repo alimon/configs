9fbaaf7263b9767e9874620147f90fac297723de
Paul Sokolovsky <paul.sokolovsky@linaro.org>
2020-01-09 16:53:16 +0300

    lite-aeolus-openamp/build.sh: See if we can use docker for builds.
    Change-Id: I7a11d98b9edbfaf249b8661354e6912a6dd7875d
    Signed-off-by: Paul Sokolovsky <paul.sokolovsky@linaro.org>

--- lite-aeolus-openamp/build.sh
+++ lite-aeolus-openamp/build.sh
@@ -6,3 +6,6 @@ wget -q https://people.linaro.org/~ed.mooring/Images/openamp-image-minimal-zcu10
 
 #echo "GIT_COMMIT=$(git rev-parse --short=8 HEAD)" > env_var_parameters
 echo "GIT_COMMIT=mock" > env_var_parameters
+
+# See if we can run docker.
+docker run hello-world
