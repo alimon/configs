1611982bd88747c0aadd20b09cf75031adfcbee8
Theodore Grey <theodore.grey@linaro.org>
2020-01-16 10:08:16 +0000

    armnn-ci-build: adding vim-common because builds are failing due to: xxd: command not found.
    Change-Id: I6164691b3d98d6636e7f372e44ac8ee03c66f2f6

--- armnn-ci-build/builders.sh
+++ armnn-ci-build/builders.sh
@@ -4,7 +4,7 @@ set -ex
 
 sudo apt -q=2 update
 sudo apt -q=2 install -y --no-install-recommends build-essential scons cmake git autoconf curl libtool libpthread-stubs0-dev
-
+sudo apt -q=2 install -y --no-install-recommends vim-common
 # Set local configuration
 git config --global user.email "ci_notify@linaro.org"
 git config --global user.name "Linaro CI"
