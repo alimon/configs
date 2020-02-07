47ef5b16108e9086ebef14f30c3cd321bdc75d90
Paul Sokolovsky <paul.sokolovsky@linaro.org>
2020-02-07 18:12:33 +0700

    lite-aeolus-openamp: Run actual build image, just mock command.
    Change-Id: I7d4d136167cc5751f661fd0363b2621ed0d68712
    Signed-off-by: Paul Sokolovsky <paul.sokolovsky@linaro.org>

--- lite-aeolus-openamp/build.sh
+++ lite-aeolus-openamp/build.sh
@@ -7,5 +7,6 @@ wget -q https://people.linaro.org/~ed.mooring/Images/openamp-image-minimal-zcu10
 #echo "GIT_COMMIT=$(git rev-parse --short=8 HEAD)" > env_var_parameters
 echo "GIT_COMMIT=mock" > env_var_parameters
 
-# See if we can run docker.
-#docker run hello-world
+mkdir -p /home/buildslave/srv/lite-aeolus-openamp/downloads
+
+docker run pfalcon/xilinx-openamp-build:v1 echo done
