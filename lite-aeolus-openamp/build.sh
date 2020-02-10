b1a3140ce8bb5143a05541bc510ab72cbbd08c5e
Paul Sokolovsky <paul.sokolovsky@linaro.org>
2020-02-10 22:19:14 +0700

    lite-aeolus-openamp: Trying to get file publish to work.
    Not exactly trivial as we run w/o root access, so can't install
    arbitrary dependencies.
    
    Change-Id: I50dc825c8260b1371f91da157746ea04be0b7394
    Signed-off-by: Paul Sokolovsky <paul.sokolovsky@linaro.org>

--- lite-aeolus-openamp/build.sh
+++ lite-aeolus-openamp/build.sh
@@ -4,7 +4,7 @@ set -ex
 id
 
 ## We don't build anything so far, just downloading pre-built.
-#wget -q https://people.linaro.org/~ed.mooring/Images/openamp-image-minimal-zcu102-zynqmp.wic.qemu-sd
+wget -q https://people.linaro.org/~ed.mooring/Images/openamp-image-minimal-zcu102-zynqmp.wic.qemu-sd
 
 #echo "GIT_COMMIT=$(git rev-parse --short=8 HEAD)" > env_var_parameters
 echo "GIT_COMMIT=mock" > env_var_parameters
@@ -12,10 +12,10 @@ echo "GIT_COMMIT=mock" > env_var_parameters
 mkdir -p /home/buildslave/srv/lite-aeolus-openamp/downloads
 
 rm -f xilinx-openamp-build.cid
-docker run --cidfile xilinx-openamp-build.cid \
-    -v /home/buildslave/srv/lite-aeolus-openamp/downloads:/home/build/prj/build/downloads \
-    pfalcon/xilinx-openamp-build:v2 \
-    /bin/bash -c "cd ~/prj; source setupsdk; MACHINE=zcu102-zynqmp bitbake openamp-image-minimal"
+#docker run --cidfile xilinx-openamp-build.cid \
+#    -v /home/buildslave/srv/lite-aeolus-openamp/downloads:/home/build/prj/build/downloads \
+#    pfalcon/xilinx-openamp-build:v2 \
+#    /bin/bash -c "cd ~/prj; source setupsdk; MACHINE=zcu102-zynqmp bitbake openamp-image-minimal"
 
-cid=$(cat xilinx-openamp-build.cid)
-docker cp -L $cid:/home/build/prj/build/tmp/deploy/images/zcu102-zynqmp/openamp-image-minimal-zcu102-zynqmp.wic.qemu-sd .
+#cid=$(cat xilinx-openamp-build.cid)
+#docker cp -L $cid:/home/build/prj/build/tmp/deploy/images/zcu102-zynqmp/openamp-image-minimal-zcu102-zynqmp.wic.qemu-sd .
---
+++ lite-aeolus-openamp/install-deps-nonroot.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+set -ex
+
+pip install --user requests
