9dc0174361b40d2472edee4bbb681a40eb39f8f8
Paul Sokolovsky <paul.sokolovsky@linaro.org>
2020-02-10 11:06:53 +0700

    lite-aeolus-openamp: Actually build test SD card image.
    Change-Id: I34571f04dd35ce8a22904ededa4211b658ad8c4c
    Signed-off-by: Paul Sokolovsky <paul.sokolovsky@linaro.org>

--- lite-aeolus-openamp/build.sh
+++ lite-aeolus-openamp/build.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
 set -ex
 
-# We don't build anything so far, just downloading pre-built.
-wget -q https://people.linaro.org/~ed.mooring/Images/openamp-image-minimal-zcu102-zynqmp.wic.qemu-sd
+## We don't build anything so far, just downloading pre-built.
+#wget -q https://people.linaro.org/~ed.mooring/Images/openamp-image-minimal-zcu102-zynqmp.wic.qemu-sd
 
 #echo "GIT_COMMIT=$(git rev-parse --short=8 HEAD)" > env_var_parameters
 echo "GIT_COMMIT=mock" > env_var_parameters
 
 mkdir -p /home/buildslave/srv/lite-aeolus-openamp/downloads
 
-docker run pfalcon/xilinx-openamp-build:v1 echo done
+docker run --cidfile xilinx-openamp-build.cid \
+    -v /home/buildslave/srv/lite-aeolus-openamp/downloads:/home/build/prj/build/downloads \
+    pfalcon/xilinx-openamp-build:v1 \
+    /bin/bash -c "cd ~/prj; source setupsdk; MACHINE=zcu102-zynqmp bitbake openamp-image-minimal"
+
+cid=$(cat xilinx-openamp-build.cid)
+docker cp -L $cid:/home/build/prj/build/tmp/deploy/images/zcu102-zynqmp/openamp-image-minimal-zcu102-zynqmp.wic.qemu-sd .
