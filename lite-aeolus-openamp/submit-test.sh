483432c61793546a7e1d8ba191ed32289834b49f
Paul Sokolovsky <paul.sokolovsky@linaro.org>
2020-02-10 23:53:52 +0700

    lite-aeolus-openamp: submit-test.sh: Use "yq" standalone Go version.
    Because Python version previously used requires "jq" tool, and we can't
    install anything via apt due to lack of root.
    
    Change-Id: I05ae0511933a5a8c9b7ced2e8226f0a288d19a1b
    Signed-off-by: Paul Sokolovsky <paul.sokolovsky@linaro.org>

--- lite-aeolus-openamp/submit-test.sh
+++ lite-aeolus-openamp/submit-test.sh
@@ -11,8 +11,14 @@ fi
 IMAGE_URL="http://snapshots.linaro.org/components/kernel/aeolus-2/openamp/${BRANCH}/${PLATFORM}/${BUILD_NUMBER}/openamp-image-minimal-zcu102-zynqmp.wic.qemu-sd"
 
 # Replace image url (passed as docker image command-line arg) in the job
-# template. "yq" tools is used, https://github.com/kislyuk/yq (note:
-# there're different yq's around, we used Python wrapper around jq).
-yq -y ".actions[1].boot.command=\"$IMAGE_URL\"" lite-lava-docker-compose/example/docker-xilinx-qemu-openamp-echo_test.job > lava.job
+# template.
+
+# "yq" the Python version, https://github.com/kislyuk/yq, requires jq
+#yq -y ".actions[1].boot.command=\"$IMAGE_URL\"" lite-lava-docker-compose/example/docker-xilinx-qemu-openamp-echo_test.job > lava.job
+
+# "yq" the Go version, https://github.com/mikefarah/yq
+wget -q https://github.com/mikefarah/yq/releases/download/3.1.0/yq_linux_amd64
+chmod +x yq_linux_amd64
+./yq_linux_amd64 w lite-lava-docker-compose/example/docker-xilinx-qemu-openamp-echo_test.job actions[1].boot.command $IMAGE_URL > lava.job
 
 python3 $dir/lava-submit.py lava.job
