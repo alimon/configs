2652f3292c69e50b58394be7d62d88044e43e1a6
Paul Sokolovsky <paul.sokolovsky@linaro.org>
2020-02-10 12:24:13 +0700

    lite-aeolus-openamp: Show build user id.
    Change-Id: Ibc619d39eccc5f599c3a8b5921330a3fdcb75c77
    Signed-off-by: Paul Sokolovsky <paul.sokolovsky@linaro.org>

--- lite-aeolus-openamp/build.sh
+++ lite-aeolus-openamp/build.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 set -ex
 
+id
+
 ## We don't build anything so far, just downloading pre-built.
 #wget -q https://people.linaro.org/~ed.mooring/Images/openamp-image-minimal-zcu102-zynqmp.wic.qemu-sd
 
