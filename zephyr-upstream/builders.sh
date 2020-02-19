a783f2970fad259eece22ddb0311859461bea0c1
Paul Sokolovsky <paul.sokolovsky@linaro.org>
2020-02-19 20:00:22 +0700

    zephyr-upstream: Update to Zephyr SDK 0.11.2.
    Change-Id: I660c13127d3fe7c1b3a1d8eaabee74378054e923
    Signed-off-by: Paul Sokolovsky <paul.sokolovsky@linaro.org>

--- zephyr-upstream/builders.sh
+++ zephyr-upstream/builders.sh
@@ -51,8 +51,8 @@ head -5 Makefile
 
 # Note that Zephyr SDK is needed even when building with the gnuarmemb
 # toolchain, ZEPHYR_SDK_INSTALL_DIR is needed to find things like conf
-ZEPHYR_SDK_URL="https://github.com/zephyrproject-rtos/sdk-ng/releases/download/v0.11.1/zephyr-sdk-0.11.1-setup.run"
-export ZEPHYR_SDK_INSTALL_DIR="${HOME}/srv/toolchain/zephyr-sdk-0.11.1"
+ZEPHYR_SDK_URL="https://github.com/zephyrproject-rtos/sdk-ng/releases/download/v0.11.2/zephyr-sdk-0.11.2-setup.run"
+export ZEPHYR_SDK_INSTALL_DIR="${HOME}/srv/toolchain/zephyr-sdk-0.11.2"
 
 # GNU ARM Embedded is downloaded once (per release) and cached in a persistent
 # docker volume under ${HOME}/srv/toolchain/.
