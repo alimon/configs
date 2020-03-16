3255e838447716c69cc9b2ef849d0aa384ac6827
Vishal Bhoj <vishal.bhoj@linaro.org>
2020-03-16 10:55:53 +0530

    lkft-android: handle multiple andorid-build-configs
    Change-Id: I4d57d6045c85677e5e92444db4c4c587ac28126a
    Signed-off-by: Vishal Bhoj <vishal.bhoj@linaro.org>

--- lkft/common/postbuildscript-v2.sh
+++ lkft/common/postbuildscript-v2.sh
@@ -1,17 +1,19 @@
 #!/bin/bash -e
-config_url="https://android-git.linaro.org/android-build-configs.git/plain/lkft/${build_config}?h=lkft"
-wget ${config_url} -O ${build_config}
-source ${build_config}
-KERNEL_COMMIT=${SRCREV_kernel}
-if [ -n "${MAKE_KERNELVERSION}" ] && echo "X${USE_KERNELVERSION_FOR_QA_BUILD_VERSION}" | grep -i "Xtrue"; then
-    QA_BUILD_VERSION=${MAKE_KERNELVERSION}-${KERNEL_COMMIT:0:12}
-elif [ ! -z "${KERNEL_DESCRIBE}" ]; then
-    QA_BUILD_VERSION=${KERNEL_DESCRIBE}
-else
-    QA_BUILD_VERSION=${KERNEL_COMMIT:0:12}
-fi
+for build_config in ${ANDROID_BUILD_CONFIG}; do
+    config_url="https://android-git.linaro.org/android-build-configs.git/plain/lkft/${build_config}?h=lkft"
+    wget ${config_url} -O ${build_config}
+    source ${build_config}
+    KERNEL_COMMIT=${SRCREV_kernel}
+    if [ -n "${MAKE_KERNELVERSION}" ] && echo "X${USE_KERNELVERSION_FOR_QA_BUILD_VERSION}" | grep -i "Xtrue"; then
+        QA_BUILD_VERSION=${MAKE_KERNELVERSION}-${KERNEL_COMMIT:0:12}
+    elif [ ! -z "${KERNEL_DESCRIBE}" ]; then
+        QA_BUILD_VERSION=${KERNEL_DESCRIBE}
+    else
+        QA_BUILD_VERSION=${KERNEL_COMMIT:0:12}
+    fi
 
-if [ -z "${TEST_QA_SERVER_TEAM}" ]; then
-    TEST_QA_SERVER_TEAM="android-lkft"
-fi
-curl --header "Auth-Token: ${QA_REPORTS_TOKEN}" --form tests='{"build_process/build": "fail"}'  ${qa_server}/api/submit/${qa_server_team}/${qa_server_project}/${QA_BUILD_VERSION}/${TEST_DEVICE_TYPE}
+    if [ -z "${TEST_QA_SERVER_TEAM}" ]; then
+        TEST_QA_SERVER_TEAM="android-lkft"
+    fi
+    curl --header "Auth-Token: ${QA_REPORTS_TOKEN}" --form tests='{"build_process/build": "fail"}'  ${qa_server}/api/submit/${qa_server_team}/${qa_server_project}/${QA_BUILD_VERSION}/${TEST_DEVICE_TYPE}
+done
