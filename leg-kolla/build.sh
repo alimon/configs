c11220d253086a43c22524bc4017c19115656365
Marcin Juszkiewicz <marcin.juszkiewicz@linaro.org>
2020-03-20 18:07:59 +0100

    leg-kolla: remove Docker credentials from jobs
    only leg-kolla-push-images job needs Docker hub access
    
    other jobs are 100% local
    
    Change-Id: I688d8c45184f799a24b2eb666c01abc6d2e16016

--- leg-kolla/build.sh
+++ leg-kolla/build.sh
@@ -50,10 +50,6 @@ cleanup_exit()
     rm -rf ${HOME}/.docker
 }
 
-mkdir -p ${HOME}/.docker
-sed -e "s|\${DOCKER_AUTH}|${DOCKER_AUTH}|" < ${WORKSPACE}/config.json > ${HOME}/.docker/config.json
-chmod 0600 ${HOME}/.docker/config.json
-
 rm -rf ${WORKSPACE}/*
 
 wget -q http://git.linaro.org/ci/job/configs.git/plain/leg-kolla/linaro.conf -O ${WORKSPACE}/linaro.conf
