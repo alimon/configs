166dc04ca295aba0c52db421500bcac89a2313d5
Paul Sokolovsky <paul.sokolovsky@linaro.org>
2020-03-23 16:10:26 +0200

    lite-common/lava-submit.py: Render correct URL for multinode jobs.
    When a multinode job is submitted, a list of per-node subjobs is
    returned by LAVA API. Use the first id in the list to create a
    URL in this case (other subjobs are reachable via LAVA UI from
    this first subjob).
    
    Change-Id: I64744b33bb14d21ee9795fe251794ee24ea88a34
    Signed-off-by: Paul Sokolovsky <paul.sokolovsky@linaro.org>

--- lite-common/lava-submit.py
+++ lite-common/lava-submit.py
@@ -21,4 +21,9 @@ if SUBMIT:
     server = xmlrpc.client.ServerProxy("https://%s:%s@%s" % (username, token, uri))
 
     job_id = server.scheduler.submit_job(jobdef)
+    # Multinode job submission returns a list of per-node id's. We output
+    # just the first id, as the rest of related jobs are reachable from
+    # LAVA UI.
+    if isinstance(job_id, list):
+        job_id = job_id[0]
     print("LAVA: https://%s../scheduler/job/%s" % (uri, job_id))
