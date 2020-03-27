7ec521ee71b7e444f57c1a2c9eebaa594cc92576
Paul Sokolovsky <paul.sokolovsky@linaro.org>
2020-03-27 15:20:15 +0200

    pfalcon-zephyr-net/submit-test.sh: Add test for dumb_http_server_mt.
    Also, refactor existing code to make regular patterns more apparent.
    
    Change-Id: Idb5f8611f61c2103a22d06fcf8e621b17255fc3b
    Signed-off-by: Paul Sokolovsky <paul.sokolovsky@linaro.org>

--- pfalcon-zephyr-net/submit-test.sh
+++ pfalcon-zephyr-net/submit-test.sh
@@ -5,7 +5,14 @@ export PATH=$HOME/.local/bin:$PATH
 dir=$(dirname $0)
 
 sudo apt-get -qq -y install jq python3-pip
+# "yq" the Python version, https://github.com/kislyuk/yq, requires jq
+# Used to replace image url in the job template.
 pip3 install yq
+
+# "yq" the Go version, https://github.com/mikefarah/yq
+#wget -q https://github.com/mikefarah/yq/releases/download/3.1.0/yq_linux_amd64
+#chmod +x yq_linux_amd64
+#./yq_linux_amd64 w lite-lava-docker-compose/example/docker-xilinx-qemu-openamp-echo_test.job actions[1].boot.command $IMAGE_URL > lava.job
 
 # For now, always check out latest version
 rm -rf lite-lava-docker-compose
@@ -13,23 +20,21 @@ if [ ! -d lite-lava-docker-compose ]; then
     git clone --depth 1 https://github.com/Linaro/lite-lava-docker-compose
 fi
 
-IMAGE_URL="http://snapshots.linaro.org/components/kernel/pfalcon-zephyr-net/${BRANCH}/${ZEPHYR_TOOLCHAIN_VARIANT}/${PLATFORM}/${BUILD_NUMBER}/samples/net/sockets/dumb_http_server/sample.net.sockets.dumb_http_server/zephyr/zephyr.bin"
+ARTIFACT_URL="http://snapshots.linaro.org/components/kernel/pfalcon-zephyr-net/${BRANCH}/${ZEPHYR_TOOLCHAIN_VARIANT}/${PLATFORM}/${BUILD_NUMBER}"
+
+BASE="${ARTIFACT_URL}/samples/net/sockets"
+
+IMAGE_URL="${BASE}/dumb_http_server/sample.net.sockets.dumb_http_server/zephyr/zephyr.bin"
 JOB_TEMPLATE="lite-lava-docker-compose/example/zephyr-net-ping-frdm_k64f.job"
-
-# Replace image url (passed as docker image command-line arg) in the job
-# template.
-
-# "yq" the Python version, https://github.com/kislyuk/yq, requires jq
 yq -y ".actions[0].deploy.images.zephyr.url=\"$IMAGE_URL\"" $JOB_TEMPLATE > lava.job
-
-# "yq" the Go version, https://github.com/mikefarah/yq
-#wget -q https://github.com/mikefarah/yq/releases/download/3.1.0/yq_linux_amd64
-#chmod +x yq_linux_amd64
-#./yq_linux_amd64 w lite-lava-docker-compose/example/docker-xilinx-qemu-openamp-echo_test.job actions[1].boot.command $IMAGE_URL > lava.job
-
 python3 $dir/../lite-common/lava-submit.py lava.job
 
+IMAGE_URL="${BASE}/dumb_http_server/sample.net.sockets.dumb_http_server/zephyr/zephyr.bin"
+JOB_TEMPLATE="lite-lava-docker-compose/example/zephyr-net-http-ab-frdm_k64f.job"
+yq -y ".actions[0].deploy.images.zephyr.url=\"$IMAGE_URL\"" $JOB_TEMPLATE > lava.job
+python3 $dir/../lite-common/lava-submit.py lava.job
 
+IMAGE_URL="${BASE}/dumb_http_server_mt/sample.net.sockets.dumb_http_server_mt/zephyr/zephyr.bin"
 JOB_TEMPLATE="lite-lava-docker-compose/example/zephyr-net-http-ab-frdm_k64f.job"
 yq -y ".actions[0].deploy.images.zephyr.url=\"$IMAGE_URL\"" $JOB_TEMPLATE > lava.job
 python3 $dir/../lite-common/lava-submit.py lava.job
