fe9e4b624881944ef3dbebcc836fa81c978785e4
Ralph Siemsen <ralph.siemsen@linaro.org>
2020-04-09 14:04:05 -0400

    se: trials with groovy continue
    Change-Id: I59d0b550fe7526469f309e48d25444172e172c6f

--- schneider-openembedded/postbuild.groovy
+++ schneider-openembedded/postbuild.groovy
@@ -1,20 +1,26 @@
 if (manager.build.result == hudson.model.Result.SUCCESS) {
+  def desc = manager.build.getDescription()
+  if (desc == null) {
+    desc = ""
+  }
   pattern = ~/CVE_NEW:\t([^\t]+\t[^\t]+)\t([^t]+)/
-  def map = [:]
+//  def map = [:]
   manager.build.logFile.eachLine { line ->
-      matcher = pattern.matcher(line)
-      if(matcher.matches()) {
-          def pkg = matcher.group(1)
-          def cve = matcher.group(2)
-          map[pkg] = cve
-      }
+    matcher = pattern.matcher(line)
+    if(matcher.matches()) {
+      def pkg = matcher.group(1)
+      def cve = matcher.group(2)
+//    map[pkg] = cve
+      desc += "&nbsp;NEW CVE in ${pkg} <a href='https://nvd.nist.gov/vuln/detail/${cve}'>${cve}</a><br/>"
+    }
   }
-  if(map.size() > 0) {
-      def summary = manager.createSummary("warning.gif")
-      summary.appendText("New CVEs:<ul>", false)
-      map.each {
-          summary.appendText("<li><b>$it.pkg</b> - <a href=\"https://nvd.nist.gov/vuln/detail/CVE-2020-6096/$it.value\">$it.value</a></li>", false)
-      }
-      summary.appendText("</ul>", false)
-  }
+  manager.build.setDescription(desc)
+//  if(map.size() > 0) {
+//      def summary = manager.createSummary("warning.gif")
+//      summary.appendText("New CVEs:<ul>", false)
+//      map.each {
+//          summary.appendText("<li><b>$it.pkg</b> - <a href=\"https://nvd.nist.gov/vuln/detail/$it.value\">$it.value</a></li>", false)
+//      }
+//      summary.appendText("</ul>", false)
+//  }
 }
