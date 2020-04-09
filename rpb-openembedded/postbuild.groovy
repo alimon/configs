e1f05ec9b664eed832b6d97ca564f25960f73813
Ralph Siemsen <ralph.siemsen@linaro.org>
2020-04-09 18:49:25 -0400

    fix closing anchor tag
    The closing </a> tag seems to have an extra character, presumably a typo.
    
    Any plain text that follows ends up becoming part of the hyperlink, at
    least in Chromium.
    
    An example of this in action can be seen at
    https://ci.linaro.org/job/schneider-openembedded-warrior-4.19-rfs/14/DISTRO=dip,MACHINE=rzn1d,label=docker-stretch-amd64/
    
    Change-Id: I6c60a9a087d8a896fb3460173f54a49de2f6093a

--- rpb-openembedded/postbuild.groovy
+++ rpb-openembedded/postbuild.groovy
@@ -2,7 +2,7 @@ if (manager.build.result == hudson.model.Result.SUCCESS) {
   def publish_server = manager.envVars["PUBLISH_SERVER"]
   def pub_dest = manager.envVars["PUB_DEST"]
 
-  def desc = "&nbsp;<a href='${publish_server}${pub_dest}'>Build location<t/a><br />"
+  def desc = "&nbsp;<a href='${publish_server}${pub_dest}'>Build location</a><br />"
 
   manager.build.setDescription(desc)
 }
