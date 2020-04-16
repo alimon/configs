82ad9d035fda6bbfee85a7487ac206f49217edca
Julien Duraj <julien.duraj@linaro.org>
2020-04-16 14:23:39 +0100

    VIXL: script in external file & upgrade tools
    clang-format-4.0 and clang-tidy-4.0 are now required for the presubmit
    test to work.
    
    Change-Id: Ib38dee54f437a77c55c07c443551967de79c1378

---
+++ linaro-art/vixl/presubmit.sh
@@ -0,0 +1,21 @@
+#!/bin/bash -xe
+
+vixl_repo="https://git.linaro.org/arm/vixl.git"
+
+mkdir -p "${HOME}/bin"
+export PATH="${HOME}/bin:${PATH}"
+
+sudo apt-get update
+sudo apt-get install -y clang scons clang-format-4.0 clang-tidy-4.0
+
+wget --no-verbose --output-document "${HOME}/bin/cpplint.py" \
+  https://raw.githubusercontent.com/google/styleguide/gh-pages/cpplint/cpplint.py
+chmod +x "${HOME}/bin/cpplint.py"
+
+git config --global user.name "vixl-build-bot"
+git config --global user.email "vixl-build-bot@fake-email.invalid"
+
+git clone "${vixl_repo}" vixl
+cd vixl
+git pull "${vixl_repo}" "${GERRIT_REFSPEC}"
+./tools/test.py ${VIXL_TEST_ARGS}
