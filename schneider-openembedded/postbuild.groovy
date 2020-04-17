a7209cbb8a5bc1fa3bfdc584fb1cd7d7884345f2
Ralph Siemsen <ralph.siemsen@linaro.org>
2020-04-16 20:32:21 -0400

    se: fix groovy postbuild
    Change-Id: I93d6b4c53ace2c3a972f295f32550eb4a8e96211

--- schneider-openembedded/postbuild.groovy
+++ schneider-openembedded/postbuild.groovy
@@ -3,23 +3,23 @@ if (manager.build.result == hudson.model.Result.SUCCESS) {
   if (desc == null) {
     desc = ""
   }
-  pattern = ~/CVE_NEW:\t([^\t]+\t[^\t]+)\t([^t]+)/
+  pattern = ~/CVE_NEW\s+(\S+)\s+(\S+)\s+(\S+)\s+(\S+)/
   def map = [:]
   manager.build.logFile.eachLine { line ->
     matcher = pattern.matcher(line)
     if(matcher.matches()) {
-      def pkg = matcher.group(1)
-      def cve = matcher.group(2)
-      map["${pkg}"] = "${cve}"
-      //desc += "&nbsp;NEW CVE in ${pkg} <a href='https://nvd.nist.gov/vuln/detail/${cve}'>${cve}</a><br/>"
+      def pkgver = matcher.group(1)
+      def cssv2 = matcher.group(2)
+      def cssv3 = matcher.group(3)
+      def cve = matcher.group(4)
+      map[$pkgver] = "<a href='https://nvd.nist.gov/vuln/detail/${cve}'>${cve}</a> score ${cssv3} in ${pkgver}"
     }
   }
-  //manager.build.setDescription(desc)
   if(map.size() > 0) {
       def summary = manager.createSummary("warning.gif")
       summary.appendText("New CVEs:<ul>", false)
       map.each {
-          summary.appendText("<li><a href=\"https://nvd.nist.gov/vuln/detail/$it.value\">$it.value</a> in $it.pkg</li>", false)
+          summary.appendText("<li>$it.value</li>", false)
       }
       summary.appendText("</ul>", false)
   }
