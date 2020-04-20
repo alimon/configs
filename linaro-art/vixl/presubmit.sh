e554411a6752f4278f382183ece27a7681bd8263
Julien Duraj <julien.duraj@linaro.org>
2020-04-20 11:46:46 +0100

    VIXL: fixup previous commit
    We are not root user so we need to use sudo to change the source list.
    
    Change-Id: Id75ac908474b8980777556d3e82660f44fbac998

--- linaro-art/vixl/presubmit.sh
+++ linaro-art/vixl/presubmit.sh
@@ -6,7 +6,7 @@ mkdir -p "${HOME}/bin"
 export PATH="${HOME}/bin:${PATH}"
 
 echo 'deb http://ports.ubuntu.com/ubuntu-ports xenial main universe' \
-  > /etc/apt/sources.list
+  | sudo tee /etc/apt/sources.list
 sudo apt-get update
 sudo apt-get install -y clang scons clang-format-4.0 clang-tidy-4.0
 
