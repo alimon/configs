fbf1aff381ec3257b2303f435712b8a83ecf789c
Julien Duraj <julien.duraj@linaro.org>
2020-04-20 09:04:36 +0100

    VIXL: add universe repositories for clang-tools-x.y
    Change-Id: Ib49188755ef27a5ac0ac9a422949e1ab5f7c72cd

--- linaro-art/vixl/presubmit.sh
+++ linaro-art/vixl/presubmit.sh
@@ -5,6 +5,8 @@ vixl_repo="https://git.linaro.org/arm/vixl.git"
 mkdir -p "${HOME}/bin"
 export PATH="${HOME}/bin:${PATH}"
 
+echo 'deb http://ports.ubuntu.com/ubuntu-ports xenial main universe' \
+  > /etc/apt/sources.list
 sudo apt-get update
 sudo apt-get install -y clang scons clang-format-4.0 clang-tidy-4.0
 
