16140a7ad2bcec7924767745c60bc98f8b6bb1e5
Julien Duraj <julien.duraj@linaro.org>
2020-04-28 15:11:37 +0100

    Change VIXL x86 img & install clang-tools
    Clang tools have been installed in arm64 base images only.
    
    Change-Id: I07ac94f29870cd4047f0f0f254010c7e5a3228d8

--- linaro-art/vixl/presubmit.sh
+++ linaro-art/vixl/presubmit.sh
@@ -5,10 +5,11 @@ vixl_repo="https://git.linaro.org/arm/vixl.git"
 mkdir -p "${HOME}/bin"
 export PATH="${HOME}/bin:${PATH}"
 
-echo 'deb http://ports.ubuntu.com/ubuntu-ports xenial main universe' \
-  | sudo tee /etc/apt/sources.list
-sudo apt-get update
-sudo apt-get install -y clang-4.0 scons clang-format-4.0 clang-tidy-4.0
+# Only x86 image contains no clang tools.
+if [[ "$(uname -p)" == "x86_64" ]]; then
+  sudo apt-get update
+  sudo apt-get install -y clang-4.0 scons clang-format-4.0 clang-tidy-4.0
+fi
 
 wget --no-verbose --output-document "${HOME}/bin/cpplint.py" \
   https://raw.githubusercontent.com/google/styleguide/gh-pages/cpplint/cpplint.py
