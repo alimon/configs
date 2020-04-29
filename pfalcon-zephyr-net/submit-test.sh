496f124f5e9962d10856067bbe9c88c264bed9df
Paul Sokolovsky <paul.sokolovsky@linaro.org>
2020-04-29 15:38:52 +0300

    pfalcon-zephyr-net: Switch to use dedicate repo for build/test tools
    Will allow for quicker turnaround while developing/testing and less
    noise in a common repo.
    
    Change-Id: Ib34aafda7f37d9d78279f5ce6934431db273e6f8
    Signed-off-by: Paul Sokolovsky <paul.sokolovsky@linaro.org>

--- pfalcon-zephyr-net/submit-test.sh
+++ pfalcon-zephyr-net/submit-test.sh
@@ -37,4 +37,4 @@ python3 $dir/../lite-common/lava-submit.py lava.job
 IMAGE_URL="${BASE}/dumb_http_server_mt/sample.net.sockets.dumb_http_server_mt/zephyr/zephyr.bin"
 JOB_TEMPLATE="lite-lava-docker-compose/example/zephyr-net-http-ab-frdm_k64f.job"
 yq -y ".actions[0].deploy.images.zephyr.url=\"$IMAGE_URL\"" $JOB_TEMPLATE > lava.job
-python3 $dir/../lite-common/lava-submit.py lava.job
+python3 $dir/../../lite-build-tools/lava_submit.py lava.job
