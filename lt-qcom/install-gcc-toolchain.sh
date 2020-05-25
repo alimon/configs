dbfbf80b3da7d51048cb88e82a4dce1d64893700
Aníbal Limón <anibal.limon@linaro.org>
2020-05-25 15:07:00 -0500

    lt-qcom: Add install-gcc-toolchain.sh
    Add common script for use across lt-qcom jobs to install toolchain,
    currently Linaro GCC6.
    
    Signed-off-by: Aníbal Limón <anibal.limon@linaro.org>
    Change-Id: I3f2e8b62a1d0a6e71a5f92b1f24695ff38986a7c

---
+++ lt-qcom/install-gcc-toolchain.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+toolchain_url_arm=http://releases.linaro.org/components/toolchain/binaries/6.3-2017.02/arm-linux-gnueabihf/gcc-linaro-6.3.1-2017.02-x86_64_arm-linux-gnueabihf.tar.xz
+toolchain_url_arm64=http://releases.linaro.org/components/toolchain/binaries/6.3-2017.02/aarch64-linux-gnu/gcc-linaro-6.3.1-2017.02-x86_64_aarch64-linux-gnu.tar.xz
+toolchain_url=toolchain_url_$ARCH
+toolchain_url=${!toolchain_url}
+
+tcdir=${HOME}/srv/toolchain
+tcbindir="${tcdir}/$(basename $toolchain_url .tar.xz)/bin"
+if [ ! -d "${tcbindir}" ]; then
+	wget -q "${toolchain_url}"
+	mkdir -p "${tcdir}"
+	tar -xf "$(basename ${toolchain_url})" -C "${tcdir}"
+fi
+
+export PATH=${tcbindir}:$PATH
+export CROSS_COMPILE="ccache $(basename $(ls -1 ${tcbindir}/*-gcc) gcc)"
+
+echo PATH="${PATH}" > gcc_toolchain_env
+echo CROSS_COMPILE="${CROSS_COMPILE}" >> gcc_toolchain_env
