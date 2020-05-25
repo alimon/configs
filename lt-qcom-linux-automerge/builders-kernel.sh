7e5e0ecc72531b628997925f4b0d9588bc6fcd98
Aníbal Limón <anibal.limon@linaro.org>
2020-05-25 18:15:46 -0500

    lt-qcom-linux: Use install-gcc-toolchain.sh
    Remove duplicate code across lt-qcom-linux-{aosp, automerge, build} to
    install the toolchain.
    
    Signed-off-by: Aníbal Limón <anibal.limon@linaro.org>
    Change-Id: I448944adc0f673833f98566f04da34d359cd3e77

--- lt-qcom-linux-automerge/builders-kernel.sh
+++ lt-qcom-linux-automerge/builders-kernel.sh
@@ -7,19 +7,7 @@ function build_integration_kernel()
 	export ARCH=$1
 	export KERNEL_CONFIGS=$2
 
-	toolchain_url_arm=http://releases.linaro.org/components/toolchain/binaries/6.3-2017.02/arm-linux-gnueabihf/gcc-linaro-6.3.1-2017.02-x86_64_arm-linux-gnueabihf.tar.xz
-	toolchain_url_arm64=http://releases.linaro.org/components/toolchain/binaries/6.3-2017.02/aarch64-linux-gnu/gcc-linaro-6.3.1-2017.02-x86_64_aarch64-linux-gnu.tar.xz
-	toolchain_url=toolchain_url_$ARCH
-	toolchain_url=${!toolchain_url}
-
-	tcdir=${HOME}/srv/toolchain
-	tcbindir="${tcdir}/$(basename $toolchain_url .tar.xz)/bin"
-	if [ ! -d "${tcbindir}" ]; then
-		wget -q "${toolchain_url}"
-		mkdir -p "${tcdir}"
-		tar -xf "$(basename ${toolchain_url})" -C "${tcdir}"
-	fi
-
+	# tcbindir from install-gcc-toolchain.sh
 	export CROSS_COMPILE="ccache $(basename $(ls -1 ${tcbindir}/*-gcc) gcc)"
 	export PATH=${tcbindir}:$PATH
 
--- lt-qcom-linux/build-kernel.sh
+++ lt-qcom-linux/build-kernel.sh
@@ -14,19 +14,7 @@ if [ -z "${KERNEL_CONFIGS}" ]; then
     export KERNEL_CONFIGS="defconfig"
 fi
 
-toolchain_url_arm=http://releases.linaro.org/components/toolchain/binaries/6.3-2017.02/arm-linux-gnueabihf/gcc-linaro-6.3.1-2017.02-x86_64_arm-linux-gnueabihf.tar.xz
-toolchain_url_arm64=http://releases.linaro.org/components/toolchain/binaries/6.3-2017.02/aarch64-linux-gnu/gcc-linaro-6.3.1-2017.02-x86_64_aarch64-linux-gnu.tar.xz
-toolchain_url=toolchain_url_$ARCH
-toolchain_url=${!toolchain_url}
-
-tcdir="${HOME}/srv/toolchain"
-tcbindir="${tcdir}/$(basename $toolchain_url .tar.xz)/bin"
-if [ ! -d "${tcbindir}" ]; then
-	wget -q "${toolchain_url}"
-	mkdir -p "${tcdir}"
-	tar -xf "$(basename ${toolchain_url})" -C "${tcdir}"
-fi
-
+# tcbindir from install-gcc-toolchain.sh
 export CROSS_COMPILE="ccache $(basename $(ls -1 ${tcbindir}/*-gcc) gcc)"
 export PATH=${tcbindir}:$PATH
 
