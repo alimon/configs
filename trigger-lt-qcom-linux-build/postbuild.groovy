ad91883291bdd859037d1b01fac31200270ceef5
Aníbal Limón <anibal.limon@linaro.org>
2020-05-25 22:14:25 -0500

    trigger-lt-qcom-linux-build: Fix postbuild.groovy configs
    Signed-off-by: Aníbal Limón <anibal.limon@linaro.org>
    Change-Id: I3fcc6c555cdd2b07fbacabd71bf862303de7a0d2

--- trigger-lt-qcom-linux-build/postbuild.groovy
+++ trigger-lt-qcom-linux-build/postbuild.groovy
@@ -2,15 +2,13 @@ if (manager.build.result == hudson.model.Result.SUCCESS) {
   def repo = manager.envVars["KERNEL_REPO_URL"]
   def branch = manager.envVars["KERNEL_BRANCH"]
   def revision = manager.envVars["KERNEL_REVISION"]
-  def configs = manager.envVars["KERNEL_CONFIGS"]
-  def arch = manager.envVars["ARCH"]
+  def configs = manager.envVars["KERNEL_CONFIGS_arm64"]
 
   def desc = "&nbsp;<h2>Trigger settings</h2><br />"
   desc += "&nbsp;<b>Repository:</b> ${repo}<br />"
   desc += "&nbsp;<b>Branch:</b> ${branch}<br />"
   desc += "&nbsp;<b>Revision:</b> ${revision}<br />"
   desc += "&nbsp;<b>Configs:</b> ${configs}<br />"
-  desc += "&nbsp;<b>Arch:</b> ${arch}<br />"
 
   manager.build.setDescription(desc)
 }
