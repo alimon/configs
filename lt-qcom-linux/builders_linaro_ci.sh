cd5a4ea2b2e73d91a13249ff4fe854f812395e35
Aníbal Limón <anibal.limon@linaro.org>
2020-05-26 08:13:31 -0500

    lt-qcom-linux/builders_linaro_ci.sh: Add information for publish
    Signed-off-by: Aníbal Limón <anibal.limon@linaro.org>
    Change-Id: I9a40ddb07f38b96a27ede8b568d89696c0f715df
    Signed-off-by: Aníbal Limón <anibal.limon@linaro.org>

--- lt-qcom-linux/builders_linaro_ci.sh
+++ lt-qcom-linux/builders_linaro_ci.sh
@@ -19,5 +19,21 @@ cp linux/{System.map,vmlinux} ${WORKSPACE}/out/
 cp linux/arch/$ARCH/boot/Image* ${WORKSPACE}/out/
 (mkdir -p out/dtbs && cd linux/arch/$ARCH/boot/dts && cp -a --parents $(find . -name *.dtb) ${WORKSPACE}/out/dtbs)
 
+
+cat > ${WORKSPACE}/out/HEADER.textile << EOF
+h4. QC LT kernel build
+
+Build description:
+* Build URL: "$BUILD_URL":$BUILD_URL
+* KERNEL_REPO_URL: "$KERNEL_REPO_URL":$KERNEL_REPO_URL
+* KERNEL_BRANCH: $KERNEL_BRANCH
+* KERNEL_COMMIT: $GIT_COMMIT
+* ARCH: $ARCH
+* KERNEL_VERSION: $KERNEL_VERSION
+* KERNEL_DESCRIBE: $KERNEL_DESCRIBE
+* KERNEL_CONFIGS: $KERNEL_CONFIGS
+EOF
+
+
 BRANCH_NAME_URL=$(echo ${KERNEL_BRANCH} | sed -e 's/[^A-Za-z0-9._-]/_/g')
 echo "PUB_DEST=member-builds/qcomlt/kernel/${BRANCH_NAME_URL}/${BUILD_NUMBER}" > pub_dest_parameters
