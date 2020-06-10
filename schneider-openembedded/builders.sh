e79909babc152a02ff435cf9aa0d67ab8770a6e7
Ryan Harkin <ryan.harkin@linaro.org>
2020-06-10 10:32:14 +0100

    se: add KERNEL_VERSION to yocto env
    Adding KERNEL_VERSION into postfile means it will appear inside the
    Yocto build environment and can be used to make choices, eg. over
    the location of the CVE database directory.
    
    Change-Id: I38953c068e65c676389d1e133a24c8f73e3114bc
    Signed-off-by: Ryan Harkin <ryan.harkin@linaro.org>

--- schneider-openembedded/builders.sh
+++ schneider-openembedded/builders.sh
@@ -188,7 +188,8 @@ case "${MACHINE}" in
 esac
 
 postfile=$(mktemp /tmp/postfile.XXXXX.conf)
-echo PREFERRED_VERSION_linux-rzn1 = \"${KERNEL_VERSION}.%\" > ${postfile}
+echo KERNEL_VERSION = \"${KERNEL_VERSION}\" > ${postfile}
+echo PREFERRED_VERSION_linux-rzn1 = \"${KERNEL_VERSION}.%\" >> ${postfile}
 echo PREFERRED_VERSION_linux-socfpga = \"${KERNEL_VERSION}.%\" >> ${postfile}
 cat ${postfile}
 bbopt="-R ${postfile}"
