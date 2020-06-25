2b88a3fad4be43db0188aa98416f3434e44c3032
Aníbal Limón <anibal.limon@linaro.org>
2020-06-25 13:10:29 -0500

    lt-qcom-u-boot: Use WORKSPACE variable to build
    Signed-off-by: Aníbal Limón <anibal.limon@linaro.org>
    Change-Id: I6baf5d0120339b3c78f844d136c88762f85ec6ac

--- lt-qcom-u-boot/builders.sh
+++ lt-qcom-u-boot/builders.sh
@@ -3,6 +3,8 @@
 set -ex
 
 export PATH=${tcbindir}:$PATH
+UBOOT_DIR="${WORKSPACE}/uboot"
+OUT_DIR="${WORKSPACE}/out"
 
 CONFIG=""
 case "${MACHINE}" in
@@ -15,18 +17,17 @@ case "${MACHINE}" in
  	;;
 esac
 
-make distclean
-pwd
-make "$CONFIG"
-make ARCH=arm CROSS_COMPILE=aarch64-linux-gnu- -j$(nproc)
+mkdir -p ${OUT_DIR}
+make -C ${UBOOT_DIR} distclean
+make -j$(nproc) \
+     -C ${UBOOT_DIR} \
+     O=${OUT_DIR} \
+     ARCH=arm \
+     CROSS_COMPILE=aarch64-linux-gnu- \
+     ${CONFIG}
 
+cd ${OUT_DIR}
 gzip -k u-boot.bin
 touch fake_rd.img
 skales-mkbootimg --kernel=u-boot.bin.gz --output=u-boot.img --dt=u-boot.dtb \
   --pagesize 2048 --base 0x80000000 --ramdisk=fake_rd.img --cmdline=""
-
-mkdir -p out/
-
-cp u-boot.bin.gz out/
-cp u-boot.dtb out/
-cp u-boot.img out/
