alimon/configs

commit 17385900cfda11ac70843990496a227566b2ce81

lt-qcom-linux-test: add configuration data for RB5
There is no firmware available for now, skipp that.

Change-Id: Ie79d35c984b42b29e861ca424cc5715285f24414
Signed-off-by: Nicolas Dechesne <nicolas.dechesne@linaro.org>

lt-qcom-linux-test/builders.sh
@@ -179,6 +179,11 @@ case "${MACHINE}" in
 		ROOTFS_PARTITION=/dev/disk/by-partlabel/userdata
 		;;
 	*)
+	qrb5165-rb5)
+		# Use userdata for now.
+		ROOTFS_PARTITION=/dev/disk/by-partlabel/userdata
+		;;
+	*)
 		echo "Currently MACHINE: ${MACHINE} isn't supported"
 		exit 1
 		;;

commit f2669d1ffa980fcae451dba5624b8ee369d833a8

lt-qcom-linux-test: fix typo
Fix typo introduced in
17385900cfda (lt-qcom-linux-test: add configuration data for RB5)

Change-Id: Ida02e3510c3424d17f3ae119b0d38f2fe08e538d
Signed-off-by: Nicolas Dechesne <nicolas.dechesne@linaro.org>

lt-qcom-linux-test/builders.sh
@@ -178,7 +178,6 @@ case "${MACHINE}" in
 		# Use userdata for now.
 		ROOTFS_PARTITION=/dev/disk/by-partlabel/userdata
 		;;
-	*)
 	qrb5165-rb5)
 		# Use userdata for now.
 		ROOTFS_PARTITION=/dev/disk/by-partlabel/userdata