0944f4b57d86a6045895149714f26bc6dca62f4b
Aníbal Limón <anibal.limon@linaro.org>
2020-07-15 12:15:56 -0500

    lt-qcom-u-boot: Fix u-boot images build number and revision names
    Signed-off-by: Aníbal Limón <anibal.limon@linaro.org>
    Change-Id: Ie06c8fba132f9cd86ca4dfd81f05d9bbcf2675d1

--- lt-qcom-u-boot/builders.sh
+++ lt-qcom-u-boot/builders.sh
@@ -38,14 +38,14 @@ Build description:
 * Build URL: "$BUILD_URL":$BUILD_URL
 * Uboot Repository: "$UBOOT_REPO_URL":$UBOOT_REPO_URL
 * Uboot Branch: $UBOOT_BRANCH
-* Uboot Revision: $GIT_REVISION
+* Uboot Revision: $GIT_COMMIT
 EOF
 
-git_short_rev="$(echo $GIT_REVISION | cut -c1-8)"
+git_short_rev="$(echo $GIT_COMMIT | cut -c1-8)"
 
 gzip -k uboot/u-boot.bin
 touch fake_rd.img
-skales-mkbootimg --kernel=uboot/u-boot.bin.gz --output=u-boot-$git_short_rev.img --dt=uboot/u-boot.dtb \
+skales-mkbootimg --kernel=uboot/u-boot.bin.gz --output=out/u-boot-$BUILD_NUMBER-$git_short_rev.img --dt=uboot/u-boot.dtb \
   --pagesize 2048 --base 0x80000000 --ramdisk=fake_rd.img --cmdline=""
-cp uboot/u-boot.bin.gz ${OUT_DIR}/u-boot-$git_short_rev.bin.gz
-cp uboot/u-boot.dtb ${OUT_DIR}/u-boot-$git_short_rev.dtb
+cp uboot/u-boot.bin.gz ${OUT_DIR}/u-boot-$BUILD_NUMBER-$git_short_rev.bin.gz
+cp uboot/u-boot.dtb ${OUT_DIR}/u-boot-$BUILD_NUMBER-$git_short_rev.dtb
