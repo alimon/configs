713b795c3a70c74e5e03f442224b175fa10537e1
Marcin Juszkiewicz <marcin.juszkiewicz@linaro.org>
2020-08-04 15:26:06 +0200

    ldcg-sbsa-firmware: also check ACPI tables against SBBR v1.2
    Change-Id: I97e54858dac72d9429305bc2a7bb3cb491308544

--- ldcg-sbsa-firmware/build.sh
+++ ldcg-sbsa-firmware/build.sh
@@ -45,6 +45,9 @@ make -C edk2/BaseTools
 source edk2/edksetup.sh
 build -b RELEASE -a AARCH64 -t GCC5 -p edk2-platforms/Platform/Qemu/SbsaQemu/SbsaQemu.dsc -n 0
 
+# build AcpiViewApp
+build -a AARCH64 -t GCC5 -p edk2/ShellPkg/ShellPkg.dsc -m edk2/ShellPkg/Application/AcpiViewApp/AcpiViewApp.inf  -n 0
+
 # copy resulting firmware and resize to 256MB images
 
 cp Build/SbsaQemu/RELEASE_GCC5/FV/SBSA_FLASH[01].fd .
@@ -62,15 +65,20 @@ truncate -s 16M sda.raw
 echo 'label:gpt' | /sbin/sfdisk sda.raw
 echo ',,U;' |/sbin/sfdisk sda.raw
 
-# We want SBSA ACS to autostart on boot
-# so we run UEFI Shell as bootloader and then it will run SBSA ACS via startup.nsh script
+# We want to do some checks to autostart on boot
+# first check ACPI tables against SBBR 1.2
+# then run SBSA ACS
+# so we run UEFI Shell as bootloader and then it will run tests via startup.nsh script
+echo "fs0:\AcpiViewApp.efi -r 2" > startup.nsh
+echo "fs0:\Sbsa.efi" >> startup.nsh
 
 device=$(sudo kpartx -avs sda.raw | cut -d' ' -f3)
 mkdir 1
 sudo mkfs.vfat /dev/mapper/$device
 sudo mount /dev/mapper/$device 1
 sudo cp Sbsa.efi 1/
-echo "fs0:\Sbsa.efi" |sudo tee 1/startup.nsh
+sudo cp Build/Shell/DEBUG_GCC5/AARCH64/AcpiViewApp.efi 1/
+sudo cp startup.nsh 1/
 sudo mkdir -p 1/efi/boot/
 sudo cp Build/SbsaQemu/RELEASE_GCC5/AARCH64/Shell.efi 1/efi/boot/bootaa64.efi
 sudo umount 1
