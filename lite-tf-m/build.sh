0a9dbe84156d0a3cdd7c4522a7ca57b5c5fc1c81
Paul Sokolovsky <paul.sokolovsky@linaro.org>
2020-08-12 16:12:00 +0300

    lite-tf-m: Make sure to set GIT_COMMIT_ID/EXTERNAL_BUILD_ID.
    Dummy values are used so far.
    
    Change-Id: I4532d8b6540ff8d0af883a070176b2a41e229e97
    Signed-off-by: Paul Sokolovsky <paul.sokolovsky@linaro.org>

--- lite-tf-m/build.sh
+++ lite-tf-m/build.sh
@@ -3,3 +3,9 @@ set -ex
 
 # We don't build anything so far, just downloading pre-built.
 wget https://people.linaro.org/~kevin.townsend/lava/an521_tfm_full.hex -O tfm_full.hex
+
+#echo "GIT_COMMIT_ID=$(git rev-parse --short=8 HEAD)" > ${WORKSPACE}/env_var_parameters
+#echo "EXTERNAL_BUILD_ID=$(git rev-parse --short=8 HEAD)-${BUILD_NUMBER}" >> ${WORKSPACE}/env_var_parameters
+
+echo "GIT_COMMIT_ID=unk" > ${WORKSPACE}/env_var_parameters
+echo "EXTERNAL_BUILD_ID=unk-${BUILD_NUMBER}" >> ${WORKSPACE}/env_var_parameters
