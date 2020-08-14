6debf797e361ac5caa1f9d7c5f1d9d6734c33016
Yongqin Liu <yongqin.liu@linaro.org>
2020-08-14 14:33:25 +0800

    lkft android: change to call api callbacks in the trigger build
    to make sure that the api callbacks called before the normal
    triggered builds
    
    Change-Id: I780a17a4bb29ee6d5de492c674abc7f8664a2e57
    Signed-off-by: Yongqin Liu <yongqin.liu@linaro.org>

---
+++ lkft/common/api-callbacks.sh
@@ -0,0 +1,14 @@
+#!/bin/bash -x
+
+if [ -n "${1}" ] && [ -f "${1}" ]; then
+    source ${1}
+fi
+# call api of android.linaro.org for lkft report check scheduling
+if [ -n "${KERNEL_BRANCH}" ] && \
+    [ -n "${QA_BUILD_VERSION}" ] && \
+    [ -n "${CALLER_JOB_NAME}" ] && \
+    [ -n "${CALLER_BUILD_NUMBER}" ]; then
+    curl -L https://android.linaro.org/lkft/newchanges/${KERNEL_BRANCH}/${QA_BUILD_VERSION}/${CALLER_JOB_NAME}/${CALLER_BUILD_NUMBER} || true
+    curl -L http://213.146.155.43/lkft/newchanges/${KERNEL_BRANCH}/${QA_BUILD_VERSION}/${CALLER_JOB_NAME}/${CALLER_BUILD_NUMBER} || true
+fi
+
