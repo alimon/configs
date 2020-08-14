8204d16eac9983768a86eea4a3569be26f63ae75
Paul Sokolovsky <paul.sokolovsky@linaro.org>
2020-08-14 17:34:03 +0300

    lite-tf-m: Add TF-M build script.
    Based on
    https://github.com/Linaro/lite-lava-docker-compose/blob/lite/test-images/qemu_mps2_an521/tfm/README.md
    This script can be reused for local building, so kept separate.
    
    Change-Id: Id428a43bb6234b73a0b600503e5578c1f59b9d8d
    Signed-off-by: Paul Sokolovsky <paul.sokolovsky@linaro.org>

---
+++ lite-tf-m/tfm-build.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+# Copyright (c) 2020, Linaro. All rights reserved.
+# SPDX-License-Identifier: BSD-3-Clause
+
+# Exit on error
+set -e
+
+# Cleanup previous build artifacts
+rm -rf CMakeCache.txt CMakeFiles cmake_install.cmake install bl2 secure_fw app unit_test test
+
+# Set the readlink binary name:
+if [ "$(uname)" == "Darwin" ]; then
+    # For OS X this should be be 'greadlink' (brew install coreutils)
+    readlink=greadlink
+else
+    # For Linux this should be 'readlink'
+    readlink=readlink
+fi
+
+# Set the config file to use
+configfile=ConfigRegression
+
+target=AN521
+
+# Generate the S and NS makefiles
+cmake -G"Unix Makefiles" \
+        -DPROJ_CONFIG=`$readlink -f ../configs/$configfile.cmake` \
+        -DTARGET_PLATFORM=$target \
+        -DCMAKE_BUILD_TYPE=Debug \
+        -DBL2=False \
+        -DCOMPILER=GNUARM \
+        ../
+
+# Build the binaries
+make install
+
+# Convert S and NS binaries to .hex file
+arm-none-eabi-objcopy -S --gap-fill 0xff -O ihex \
+        install/outputs/$target/tfm_s.axf tfm_s.hex
+arm-none-eabi-objcopy -S --gap-fill 0xff -O ihex \
+        install/outputs/$target/tfm_ns.axf tfm_ns.hex
+
+# Generate a single hex file for convenience/QEMU sake
+srec_cat tfm_s.hex -Intel tfm_ns.hex -Intel -o tfm_full.hex -Intel
