7d05827d967156c50af5b6fda9a931c75a75167c
Paul Sokolovsky <paul.sokolovsky@linaro.org>
2020-08-18 16:36:14 +0300

    lite-tf-m: No git-lfs in xenial, install from packagecloud.io instead.
    Change-Id: I51c1e0f7cdd522e18dc6a74b9daa483120d1df2b
    Signed-off-by: Paul Sokolovsky <paul.sokolovsky@linaro.org>

--- lite-tf-m/install-deps.sh
+++ lite-tf-m/install-deps.sh
@@ -3,7 +3,11 @@ set -ex
 
 export DEBIAN_FRONTEND=noninteractive
 sudo apt-get -qq update
-sudo apt-get -qq -y install git-lfs python3 python3-pip srecord
+sudo apt-get -qq -y install python3 python3-pip srecord
+
+# No git-lfs package in Ununtu Xenial, install manually from packagecloud.io
+wget https://packagecloud.io/github/git-lfs/packages/ubuntu/xenial/git-lfs_2.11.0_amd64.deb/download.deb -O git-lfs_2.11.0_amd64.deb
+sudo dpkg -i git-lfs_2.11.0_amd64.deb
 
 sudo pip3 install cmake
 pip3 install --user cryptography pyasn1 pyyaml jinja2 cbor
