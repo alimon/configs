3161f971cf436288f70fc08758c1c348a713b585
Paul Sokolovsky <paul.sokolovsky@linaro.org>
2020-08-18 16:20:36 +0300

    lite-tf-m: Install git-lfs (required by CMSIS_5 repo).
    Change-Id: Icac52c3efb07a9fafd4d3d784a1c994dfdbfda3f
    Signed-off-by: Paul Sokolovsky <paul.sokolovsky@linaro.org>

--- lite-tf-m/install-deps.sh
+++ lite-tf-m/install-deps.sh
@@ -3,7 +3,7 @@ set -ex
 
 export DEBIAN_FRONTEND=noninteractive
 sudo apt-get -qq update
-sudo apt-get -qq -y install python3 python3-pip srecord
+sudo apt-get -qq -y install git-lfs python3 python3-pip srecord
 
 sudo pip3 install cmake
 pip3 install --user cryptography pyasn1 pyyaml jinja2 cbor
@@ -13,6 +13,8 @@ df -h
 
 # List available toolchains
 ls -l ${HOME}/srv/toolchain/
+
+git lfs install
 
 # Preclude spammy "advices"
 git config --global advice.detachedHead false
