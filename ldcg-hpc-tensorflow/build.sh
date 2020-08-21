660381b323f595c55a1f1ca605dae70c0007d936
Marcin Juszkiewicz <marcin.juszkiewicz@linaro.org>
2020-08-21 11:10:30 +0200

    ldcg-hpc-tensorflow: fetch repo to get ansible playbooks
    Change-Id: I7d3b3c349f4e44ec9826a32b77157ba2b8bd69ad

--- ldcg-hpc-tensorflow/build.sh
+++ ldcg-hpc-tensorflow/build.sh
@@ -3,7 +3,13 @@
 # first let update system
 sudo dnf -y distrosync
 sudo dnf -y install centos-release-ansible-29
-sudo dnf -y install ansible
+sudo dnf -y install ansible git
 
-cd tensorflow
+git clone --depth 1 https://git.linaro.org/ci/job/configs.git
+
+cd configs/ldcg-hpc-tensorflow/tensorflow
+
+echo "[all]" > inventory
+echo "localhost ansible_connection=local" >> inventory
+
 ansible-playbook -i inventory playbooks/run.yml
