ed785ab84dbad73aa480fb798bb97b8a58265e8b
Marcin Juszkiewicz <marcin.juszkiewicz@linaro.org>
2020-09-02 13:07:56 +0200

    ldcg-hpc-tensorflow: publish to snapshots
    Change-Id: Id0c248a72bead2b57f129277da6240cd647426bd

---
+++ ldcg-hpc-tensorflow/publishers.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+set -ex
+
+# Publish wheel files
+test -d ${HOME}/bin || mkdir ${HOME}/bin
+wget -q https://git.linaro.org/ci/publishing-api.git/blob_plain/HEAD:/linaro-cp.py -O ${HOME}/bin/linaro-cp.py
+time python3 ${HOME}/bin/linaro-cp.py \
+  --server ${PUBLISH_SERVER} \
+  --link-latest \
+  ${WORKSPACE}/out \
+  hpc/python/tensorflow/${BUILD_NUMBER}
+
+set +x
+
+echo "Python wheels: https://snapshots.linaro.org/hpc/python/tensorflow/${BUILD_NUMBER}/"
