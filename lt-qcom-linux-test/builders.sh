8941d65c2a52c52c946e89ba40e17f3b926a1d9d
Aníbal Limón <anibal.limon@linaro.org>
2020-09-03 15:30:52 -0500

    lt-qcom-linux-test: Remove BT QCA firmware on db{410,820}c
    Signed-off-by: Aníbal Limón <anibal.limon@linaro.org>
    Change-Id: I9ad0a2b327da146d20447556873df11cee1a6c04

--- lt-qcom-linux-test/builders.sh
+++ lt-qcom-linux-test/builders.sh
@@ -68,13 +68,13 @@ function remove_unused_firmware() {
 			cpio -idv -H newc < ../../$target_file
 
 			if [ "${MACHINE}" = "apq8016-sbc" ]; then
-				rm -rf lib/firmware/ar* lib/firmware/htc* lib/firmware/wil*
+				rm -rf lib/firmware/ar* lib/firmware/htc* lib/firmware/wil* lib/firmware/qca*
 				rm -rf lib/firmware/ath* lib/firmware/LICENSE.QualcommAtheros_ath10k
 				rm -rf lib/firmware/qcom/a530* lib/firmware/qcom/a630*
 				rm -rf lib/firmware/qcom/msm8996 lib/firmware/qcom/sdm845 lib/firmware/K2026090.mem
 				rm -rf lib/firmware/qcom/venus-4.2 lib/firmware/qcom/venus-5.2 lib/firmware/qcom/venus-5.4
 			elif [ "${MACHINE}" = "apq8096-db820c" ]; then
-				rm -rf lib/firmware/ar* lib/firmware/htc* lib/firmware/wil*
+				rm -rf lib/firmware/ar* lib/firmware/htc* lib/firmware/wil* lib/firmware/qca*
 				rm -rf lib/firmware/ath3* lib/firmware/ath6* lib/firmware/ath9* 
 				rm -rf lib/firmware/ath10k/QCA4* lib/firmware/ath10k/QCA9* lib/firmware/ath10k/WCN*
 				rm -rf lib/firmware/a300* lib/firmware/qcom/a300* lib/firmware/qcom/a630*
