86945184b2103f91926abbea7da71dec68ce6fe6
Vishal Bhoj <vishal.bhoj@linaro.org>
2020-09-14 15:49:29 +0530

    test-definitions-validation: add igt test
    Signed-off-by: Vishal Bhoj <vishal.bhoj@linaro.org>
    Change-Id: I724356eeb1c6fa68a6a85620451f2b293fff4a8f

--- test-definitions-validation/builders.sh
+++ test-definitions-validation/builders.sh
@@ -5,7 +5,7 @@ sudo DEBIAN_FRONTEND=noninteractive apt-get install -y python-pip
 pip install ruamel.yaml jinja2
 git clone git://git.linaro.org/ci/job/configs
 python2 configs/openembedded-lkft/submit_for_testing.py \
-        --device-type db410c \
+        --device-type db845c \
         --build-number ${BUILD_NUMBER} \
         --lava-server https://validation.linaro.org \
         --qa-server https://qa-reports.linaro.org \
@@ -14,3 +14,14 @@ python2 configs/openembedded-lkft/submit_for_testing.py \
 	--git-commit ${BUILD_NUMBER} \
         --testplan-path configs/test-definitions-validation/ \
         --test-plan test-db845c-1.yaml test-db845c-2.yaml test-db845c-3.yaml
+
+python2 configs/openembedded-lkft/submit_for_testing.py \
+        --device-type db410c \
+        --build-number ${BUILD_NUMBER} \
+        --lava-server https://lkft.validation.linaro.org \
+        --qa-server https://qa-reports.linaro.org \
+        --qa-server-team qa \
+        --qa-server-project test-definitions-validation \
+	--git-commit ${BUILD_NUMBER} \
+        --testplan-path configs/test-definitions-validation/ \
+        --test-plan test-db410c-1.yaml
