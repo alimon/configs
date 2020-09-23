fa466556aebe27ff8e05f9ab679bb41476783fc6
Theodore Grey <theodore.grey@linaro.org>
2020-09-23 19:35:33 +0100

    armnn-ci-build : builders.sh fixing error caused due to missing swig during compilation.
    Signed-off-by: Theodore Grey <theodore.grey@linaro.org>
    Change-Id: Ic01c698212986dc5221baf9890261fb9719c7f50

--- armnn-ci-build/builders.sh
+++ armnn-ci-build/builders.sh
@@ -4,7 +4,7 @@ set -ex
 
 sudo apt -q=2 update
 sudo apt -q=2 install -y --no-install-recommends build-essential scons cmake git autoconf curl libtool libpthread-stubs0-dev
-sudo apt -q=2 install -y --no-install-recommends vim-common
+sudo apt -q=2 install -y --no-install-recommends vim-common swig
 sudo apt -q=2 install -y --no-install-recommends python-pip python3-pip virtualenv python-dev python3-dev
 # Set local configuration
 git config --global user.email "ci_notify@linaro.org"
