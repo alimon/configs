dca35ac16bd1c5260fa4e0e939867f573bec8911
Marcin Juszkiewicz <marcin.juszkiewicz@linaro.org>
2020-10-13 08:58:47 +0000

    ldcg-python-pytorch-vision: build also Glow
    We will build Pytorch Glow too but this job does not use it.
    
    Ansible playbooks will be used to build pytorch images for HPCAI-488
    
    Change-Id: I9fe50078e090c104ba2473b9794a207204c99d5d

--- ldcg-python-pytorch-vision/publishers.sh
+++ ldcg-python-pytorch-vision/publishers.sh
@@ -3,21 +3,22 @@
 set -ex
 
 if [ -e /etc/debian_version ]; then
-  BUILD_NUMBER="${BUILD_NUMBER}-debian"
+    BUILD_NUMBER="${BUILD_NUMBER}-debian"
 else
-   sudo dnf install -y python3-requests wget
+    BUILD_NUMBER="${BUILD_NUMBER}-centos"
+    sudo dnf install -y python3-requests wget
 fi
 
-ls -alR /var/tmp/workspace/out
+ls -alR /home/buildslave/wheels
 
 # Publish wheel files
 test -d ${HOME}/bin || mkdir ${HOME}/bin
 wget -q https://git.linaro.org/ci/publishing-api.git/blob_plain/HEAD:/linaro-cp.py -O ${HOME}/bin/linaro-cp.py
 time python3 ${HOME}/bin/linaro-cp.py \
-  --server ${PUBLISH_SERVER} \
-  --link-latest \
-  /var/tmp/workspace/out \
-  hpc/python/pytorch/${BUILD_NUMBER}
+    --server ${PUBLISH_SERVER} \
+    --link-latest \
+    /home/buildslave/wheels
+    hpc/python/pytorch/${BUILD_NUMBER}
 
 set +x
 
