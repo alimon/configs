47eea0bd537e517933f05c2f0d9c742253da6af1
Marcin Juszkiewicz <marcin.juszkiewicz@linaro.org>
2020-10-14 10:29:10 +0200

    ldcg-container-pytorch: TAG not LABEL
    Change-Id: Ib710612491129c94c094081f10424fa15c272615

--- ldcg-python-pytorch-vision/build-containers.sh
+++ ldcg-python-pytorch-vision/build-containers.sh
@@ -6,8 +6,8 @@ git clone --depth 1 https://git.linaro.org/ci/job/configs.git
 
 cd configs/ldcg-python-pytorch-vision/
 
-docker build -f Dockerfile-debian --pull --label linaro/debian-pytorch:${BUILD_NUMBER} .
-docker build -f Dockerfile-centos --pull --label linaro/centos-pytorch:${BUILD_NUMBER} .
+docker build -f Dockerfile-debian --pull --tag linaro/debian-pytorch:${BUILD_NUMBER} .
+docker build -f Dockerfile-centos --pull --tag linaro/centos-pytorch:${BUILD_NUMBER} .
 
 docker push linaro/debian-pytorch:${BUILD_NUMBER}
 docker push linaro/centos-pytorch:${BUILD_NUMBER}
