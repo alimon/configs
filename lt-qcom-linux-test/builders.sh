99aebe4581524c5f2418fc22c8f3c632d4b6a14b
Aníbal Limón <anibal.limon@linaro.org>
2020-10-14 18:05:57 -0500

    lt-qcom-linux-test: Use readlink to validate if link exists
    When copy links as files using e2cp to ext4 fs use readlink
    to get the actual file pointing to.
    
    Signed-off-by: Aníbal Limón <anibal.limon@linaro.org>
    Change-Id: Ia00b6bc39a00121ecff0fe8d36384ffbc585f706

--- lt-qcom-linux-test/builders.sh
+++ lt-qcom-linux-test/builders.sh
@@ -60,10 +60,12 @@ function copy_archive_to_rootfs() {
 		do
 			e2cp -a -p -G 0 -O 0 -v $f $cdir/$target_file:/
 		done
-		for l in $(find . -type l -xtype f)
+		for l in $(find . -type l)
 		do
-			f=$(realpath $l)
-			e2cp -p -G 0 -O 0 -v $f $cdir/$target_file:/$l
+			f=$(readlink $l)
+			if [ -f "./$f" ]; then
+				e2cp -p -G 0 -O 0 -v ./$f $cdir/$target_file:/$l
+			fi
 		done
 		popd
 		sudo rm -rf $archive_tmpd
