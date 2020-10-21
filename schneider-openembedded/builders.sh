746074e20d90666db3017ed86f516ae0b25152b8
Ralph Siemsen <ralph.siemsen@linaro.org>
2020-10-21 06:28:34 -0400

    se: update CVE baseline
    Change-Id: I59c7668f3143f8b8772739af29861a7aba4c3fc8

--- schneider-openembedded/builders.sh
+++ schneider-openembedded/builders.sh
@@ -284,10 +284,10 @@ if [ -e cve-${MACHINE}.old ]; then
 	# Same thing, but against arbitrary (but fixed) baseline
 	case "${MACHINE}" in
 	    *rzn1*)
-		wget -nv -O cve-${MACHINE}.base https://releases.linaro.org/members/schneider/openembedded/2019.09-warrior.3/rzn1d-4.19/dip-image-rzn1-snarc.rootfs.cve
+		wget -nv -O cve-${MACHINE}.base https://releases.linaro.org/members/schneider/openembedded/2020.09-dunfell/rzn1d-5.4/dip-image-rzn1-snarc.rootfs.cve
 		;;
 	    *soca9*)
-		wget -nv -O cve-${MACHINE}.base https://releases.linaro.org/members/schneider/openembedded/2019.09-warrior.3/soca9-4.19/dip-image-snarc-soca9.rootfs.cve
+		wget -nv -O cve-${MACHINE}.base https://releases.linaro.org/members/schneider/openembedded/2020.09-dunfell/soca9-5.4/dip-image-snarc-soca9.rootfs.cve
 		;;
 	esac
 	gawk -f diff-cve cve-${MACHINE}.base cve-${MACHINE}.new > ${WORKSPACE}/base-cve-${MACHINE}.txt
