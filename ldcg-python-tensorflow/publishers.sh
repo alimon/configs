f59696040f17d2b6f0abad76067f318a527db3bc
Marcin Juszkiewicz <marcin.juszkiewicz@linaro.org>
2020-12-08 12:29:01 +0100

    ldcg-python-tensorflow: publish from proper directory
    Change-Id: Iad4eb15d2c5cc47240a15e35a78e45fb4b2c89f0

--- ldcg-python-tensorflow/publishers.sh
+++ ldcg-python-tensorflow/publishers.sh
@@ -22,7 +22,7 @@ wget -q https://git.linaro.org/ci/publishing-api.git/blob_plain/HEAD:/linaro-cp.
 time python3 ${HOME}/bin/linaro-cp.py \
   --server ${PUBLISH_SERVER} \
   --link-latest \
-  /var/tmp/workspace/out \
+  /home/buildslave/wheels \
   $OUTPUT_PATH
 
 set +x
