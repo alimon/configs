043f4d9cbfe84acb940fadda3c33a4cbb4c45c09
Maxim Uvarov <maxim.uvarov@linaro.org>
2020-12-09 18:01:27 +0300

    ledge: use docker-buster-amd64 to set up env
    Signed-off-by: Maxim Uvarov <maxim.uvarov@linaro.org>
    Change-Id: Id348443ffcf3e6bf2d3c20747a066f6a018af4a1

--- ledge/oe/builders.sh
+++ ledge/oe/builders.sh
@@ -45,10 +45,10 @@ if [ -z "${WORKSPACE}" ]; then
   export WORKSPACE=${PWD}
 fi
 
-mkdir -p ${HOME}/bin
-curl https://storage.googleapis.com/git-repo-downloads/repo > ${HOME}/bin/repo
-chmod a+x ${HOME}/bin/repo
-export PATH=${HOME}/bin:${PATH}
+#mkdir -p ${HOME}/bin
+#curl https://storage.googleapis.com/git-repo-downloads/repo > ${HOME}/bin/repo
+#chmod a+x ${HOME}/bin/repo
+#export PATH=${HOME}/bin:${PATH}
 
 # initialize repo if not done already
 export MANIFEST_URL=${MANIFEST_URL:-https://github.com/Linaro/ledge-oe-manifest.git}
