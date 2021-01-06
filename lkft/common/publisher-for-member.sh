cb2004cc2184f77ca42f591f1442a23ff6cdc187
Yongqin Liu <yongqin.liu@linaro.org>
2021-01-06 20:05:09 +0800

    lkft: add support for build non public lkft jobs
    Change-Id: I5b09e90c9783812f47825fa2febae0a6d1938780
    Signed-off-by: Yongqin Liu <yongqin.liu@linaro.org>

---
+++ lkft/common/publisher-for-member.sh
@@ -0,0 +1,34 @@
+#!/bin/bash -ex
+
+# LKFT_WORK_DIR set in lkft/common/builders-v2.sh does not work here
+# the value is empty, so needs to set it again
+export LKFT_WORK_DIR=/home/buildslave/srv/${BUILD_DIR}/workspace
+cd ${LKFT_WORK_DIR}
+
+JOB_OUT_PUBLISH=out/publish
+rm -fr ${JOB_OUT_PUBLISH} && mkdir -p ${JOB_OUT_PUBLISH}
+url_build_info="https://git.linaro.org/ci/job/configs.git/blob_plain/HEAD:/lkft/common/build-info/member.txt"
+wget ${url_build_info} -O ${JOB_OUT_PUBLISH}/BUILD-INFO.txt
+
+PUBLISH_COMMON_FILES="pinned-manifest.xml defconfig gki_defconfig upstream_gki_defconfig SHA256SUMS.txt"
+for build_config in ${ANDROID_BUILD_CONFIG}; do
+    config_url="https://android-git.linaro.org/android-build-configs.git/plain/lkft/${build_config}?h=lkft"
+    wget ${config_url} -O ${build_config}
+    source ${build_config}
+
+    for f in ${PUBLISH_COMMON_FILES} ${PUBLISH_FILES}; do
+        if [ -f out/${build_config}/${f} ]; then
+            mv -v out/${build_config}/${f} ${JOB_OUT_PUBLISH}/${build_config}-${f}
+        fi
+    done
+done
+
+# Publish
+PUB_DEST=android/lkft/${JOB_NAME}/${BUILD_NUMBER}
+HOST_BIN=out/host/bin
+mkdir -p ${HOST_BIN}
+wget -q https://git.linaro.org/ci/publishing-api.git/blob_plain/HEAD:/linaro-cp.py -O ${HOST_BIN}/linaro-cp.py
+time python3 ${HOST_BIN}/linaro-cp.py \
+  --server ${PUBLISH_SERVER} \
+  --link-latest \
+  ${JOB_OUT_PUBLISH} ${PUB_DEST}
