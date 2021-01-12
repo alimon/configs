a2f578da4e6dbc2d556b904d51565d318bcfaf6e
Aníbal Limón <anibal.limon@linaro.org>
2021-01-12 11:55:09 -0600

    lt-qcom/install-gcc-toolchain.sh: Update to ARM GCC 10.2
    This toolchain is used to build,
    
    - Automerge integration branch.
    - Debian Linux packages.
    - Dragonboard410c U-boot .
    - Dragonboard845c bootloader.
    - Linux testing images.
    - Linux AOSP kernel.
    
    Signed-off-by: Aníbal Limón <anibal.limon@linaro.org>
    Change-Id: If3441897824e213c43a8b7ce1b8682462a90b23b

--- lt-qcom/install-gcc-toolchain.sh
+++ lt-qcom/install-gcc-toolchain.sh
@@ -7,8 +7,8 @@ if [ "${TOOLCHAIN_ARCH}" ]; then
 	ARCH="${TOOLCHAIN_ARCH}"
 fi
 
-toolchain_url_arm=http://releases.linaro.org/components/toolchain/binaries/6.3-2017.02/arm-linux-gnueabihf/gcc-linaro-6.3.1-2017.02-x86_64_arm-linux-gnueabihf.tar.xz
-toolchain_url_arm64=http://releases.linaro.org/components/toolchain/binaries/6.3-2017.02/aarch64-linux-gnu/gcc-linaro-6.3.1-2017.02-x86_64_aarch64-linux-gnu.tar.xz
+toolchain_url_arm=https://armkeil.blob.core.windows.net/developer/Files/downloads/gnu-a/10.2-2020.11/binrel/gcc-arm-10.2-2020.11-x86_64-arm-none-eabi.tar.xz
+toolchain_url_arm64=https://armkeil.blob.core.windows.net/developer/Files/downloads/gnu-a/10.2-2020.11/binrel/gcc-arm-10.2-2020.11-x86_64-aarch64-none-linux-gnu.tar.xz
 toolchain_url=toolchain_url_$ARCH
 toolchain_url=${!toolchain_url}
 
