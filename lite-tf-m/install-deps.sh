a7c53af74176e2da52dd1ed7305eed3dba49ddb1
Paul Sokolovsky <paul.sokolovsky@linaro.org>
2021-01-25 13:40:21 +0300

    lite-tf-m: install-deps.sh: Install python3-setuptools.
    Build started to fail recently due to lack of them.
    
    Change-Id: I65e33863f135a8055cca289060ebe038e3b41c07
    Signed-off-by: Paul Sokolovsky <paul.sokolovsky@linaro.org>

--- lite-tf-m/install-deps.sh
+++ lite-tf-m/install-deps.sh
@@ -3,7 +3,7 @@ set -ex
 
 export DEBIAN_FRONTEND=noninteractive
 sudo apt-get -qq update
-sudo apt-get -qq -y install python3 python3-pip srecord libffi-dev libssl-dev
+sudo apt-get -qq -y install python3 python3-pip python3-setuptools srecord libffi-dev libssl-dev
 
 # No git-lfs package in Ununtu Xenial, install manually from packagecloud.io
 wget https://packagecloud.io/github/git-lfs/packages/ubuntu/xenial/git-lfs_2.11.0_amd64.deb/download.deb -O git-lfs_2.11.0_amd64.deb
