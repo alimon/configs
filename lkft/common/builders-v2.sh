7d637ea6528ad743f4e29294d4ac0e7a5f865059
Yongqin Liu <yongqin.liu@linaro.org>
2021-02-04 09:57:06 +0800

    lkft: remove the call for android.ctt.linaro.org
    Change-Id: I69f469e16d97fe31aea345118222bec36cbf0d5e
    Signed-off-by: Yongqin Liu <yongqin.liu@linaro.org>

--- lkft/common/builders-v2.sh
+++ lkft/common/builders-v2.sh
@@ -16,7 +16,6 @@ if [ -n "${KERNEL_BRANCH}" ] && [ -n "${KERNEL_DESCRIBE}" ] && [ -n "${JOB_NAME}
     fi
 
     curl -L https://android.linaro.org/lkft/newbuild/${KERNEL_BRANCH}/${QA_BUILD_VERSION}/${JOB_NAME}/${BUILD_NUMBER} || true
-    curl -L https://android.ctt.linaro.org/lkft/newbuild/${KERNEL_BRANCH}/${QA_BUILD_VERSION}/${JOB_NAME}/${BUILD_NUMBER} || true
 fi
 
 git config --global user.email "ci_notify@linaro.org"
